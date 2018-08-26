43ebfd9a62e976cb4b47e985b1124f0577ad52e6
Kai Chen <chenkaidev@gmail.com>
2018-08-27 02:38:05 +0800

    move some unit tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -79,3 +79,25 @@ def test_exception():
 
     with pytest.raises(TypeError):
         mmcv.dump(test_obj, 'tmp.txt')
+
+
+def test_list_from_file():
+    filename = osp.join(osp.dirname(__file__), 'data/filelist.txt')
+    filelist = mmcv.list_from_file(filename)
+    assert filelist == ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
+    filelist = mmcv.list_from_file(filename, prefix='a/')
+    assert filelist == ['a/1.jpg', 'a/2.jpg', 'a/3.jpg', 'a/4.jpg', 'a/5.jpg']
+    filelist = mmcv.list_from_file(filename, offset=2)
+    assert filelist == ['3.jpg', '4.jpg', '5.jpg']
+    filelist = mmcv.list_from_file(filename, max_num=2)
+    assert filelist == ['1.jpg', '2.jpg']
+    filelist = mmcv.list_from_file(filename, offset=3, max_num=3)
+    assert filelist == ['4.jpg', '5.jpg']
+
+
+def test_dict_from_file():
+    filename = osp.join(osp.dirname(__file__), 'data/mapping.txt')
+    mapping = mmcv.dict_from_file(filename)
+    assert mapping == {'1': 'cat', '2': ['dog', 'cow'], '3': 'panda'}
+    mapping = mmcv.dict_from_file(filename, key_type=int)
+    assert mapping == {1: 'cat', 2: ['dog', 'cow'], 3: 'panda'}
--- tests/test_path.py
+++ tests/test_path.py
@@ -5,28 +5,6 @@ import mmcv
 import pytest
 
 
-def test_list_from_file():
-    filename = osp.join(osp.dirname(__file__), 'data/filelist.txt')
-    filelist = mmcv.list_from_file(filename)
-    assert filelist == ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
-    filelist = mmcv.list_from_file(filename, prefix='a/')
-    assert filelist == ['a/1.jpg', 'a/2.jpg', 'a/3.jpg', 'a/4.jpg', 'a/5.jpg']
-    filelist = mmcv.list_from_file(filename, offset=2)
-    assert filelist == ['3.jpg', '4.jpg', '5.jpg']
-    filelist = mmcv.list_from_file(filename, max_num=2)
-    assert filelist == ['1.jpg', '2.jpg']
-    filelist = mmcv.list_from_file(filename, offset=3, max_num=3)
-    assert filelist == ['4.jpg', '5.jpg']
-
-
-def test_dict_from_file():
-    filename = osp.join(osp.dirname(__file__), 'data/mapping.txt')
-    mapping = mmcv.dict_from_file(filename)
-    assert mapping == {'1': 'cat', '2': ['dog', 'cow'], '3': 'panda'}
-    mapping = mmcv.dict_from_file(filename, key_type=int)
-    assert mapping == {1: 'cat', 2: ['dog', 'cow'], 3: 'panda'}
-
-
 def test_check_file_exist():
     mmcv.check_file_exist(__file__)
     if sys.version_info > (3, 3):
