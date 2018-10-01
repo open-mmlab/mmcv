c3f87f48d1c845fb3c462a2b877de0f9c89e8137
Kai Chen <chenkaidev@gmail.com>
2018-10-01 22:26:18 +0800

    bug fix

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -3,7 +3,7 @@ from .misc import (is_str, iter_cast, list_cast, tuple_cast, is_seq_of,
                    is_list_of, is_tuple_of, slice_list, concat_list,
                    check_prerequisites, requires_package, requires_executable)
 from .path import (is_filepath, fopen, check_file_exist, mkdir_or_exist,
-                   symlink, scandir)
+                   symlink, scandir, FileNotFoundError)
 from .progressbar import ProgressBar, track_progress, track_parallel_progress
 from .timer import Timer, TimerError, check_time
 
@@ -12,6 +12,6 @@ __all__ = [
     'is_seq_of', 'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
     'check_prerequisites', 'requires_package', 'requires_executable',
     'is_filepath', 'fopen', 'check_file_exist', 'mkdir_or_exist', 'symlink',
-    'scandir', 'ProgressBar', 'track_progress', 'track_parallel_progress',
-    'Timer', 'TimerError', 'check_time'
+    'scandir', 'FileNotFoundError', 'ProgressBar', 'track_progress',
+    'track_parallel_progress', 'Timer', 'TimerError', 'check_time'
 ]
