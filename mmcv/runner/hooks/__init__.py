ecfce392fada280fda233b5cb4434257a6a976df
Kai Chen <chenkaidev@gmail.com>
2018-10-07 17:30:00 +0800

    add EmptyCacheHook and rename some modules

--- mmcv/runner/hooks/__init__.py
+++ mmcv/runner/hooks/__init__.py
@@ -1,15 +1,16 @@
 from .hook import Hook
-from .checkpoint_saver import CheckpointHook
+from .checkpoint import CheckpointHook
 from .closure import ClosureHook
 from .lr_updater import LrUpdaterHook
-from .optimizer_stepper import OptimizerHook
+from .optimizer import OptimizerHook
 from .iter_timer import IterTimerHook
 from .sampler_seed import DistSamplerSeedHook
+from .memory import EmptyCacheHook
 from .logger import (LoggerHook, TextLoggerHook, PaviLoggerHook,
                      TensorboardLoggerHook)
 
 __all__ = [
     'Hook', 'CheckpointHook', 'ClosureHook', 'LrUpdaterHook', 'OptimizerHook',
-    'IterTimerHook', 'DistSamplerSeedHook', 'LoggerHook', 'TextLoggerHook',
-    'PaviLoggerHook', 'TensorboardLoggerHook'
+    'IterTimerHook', 'DistSamplerSeedHook', 'EmptyCacheHook', 'LoggerHook',
+    'TextLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook'
 ]
