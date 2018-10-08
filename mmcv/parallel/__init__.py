b9fa5c90947e5d8704c2d798eb1eaab51bc37e69
Kai Chen <chenkaidev@gmail.com>
2018-10-08 20:34:49 +0800

    add collate function

--- mmcv/parallel/__init__.py
+++ mmcv/parallel/__init__.py
@@ -1,9 +1,10 @@
+from .collate import collate
 from .data_container import DataContainer
 from .data_parallel import MMDataParallel
 from .distributed import MMDistributedDataParallel
 from .scatter_gather import scatter, scatter_kwargs
 
 __all__ = [
-    'DataContainer', 'MMDataParallel', 'MMDistributedDataParallel', 'scatter',
-    'scatter_kwargs'
+    'collate', 'DataContainer', 'MMDataParallel', 'MMDistributedDataParallel',
+    'scatter', 'scatter_kwargs'
 ]
