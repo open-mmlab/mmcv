64959bd7725b91e137a9a3712ab2e8661f5ae01e
yl-1993 <yl93528@gmail.com>
2018-10-09 21:14:09 +0800

    add alexnet & vgg

--- mmcv/cnn/__init__.py
+++ mmcv/cnn/__init__.py
@@ -1,3 +1,7 @@
+from .alexnet import AlexNet
+from .vgg import VGG, make_vgg_layer
 from .resnet import ResNet, make_res_layer
 
-__all__ = ['ResNet', 'make_res_layer']
+__all__ = ['AlexNet',
+           'VGG', 'make_vgg_layer',
+           'ResNet', 'make_res_layer']
