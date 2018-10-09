9af1a2b025a6f8a32aad55dcde97a914b821326a
Kai Chen <chenkaidev@gmail.com>
2018-10-09 22:11:11 +0800

    add weight initialization methods

--- mmcv/cnn/__init__.py
+++ mmcv/cnn/__init__.py
@@ -1,3 +1,7 @@
 from .resnet import ResNet, make_res_layer
+from .weight_init import xavier_init, normal_init, uniform_init, kaiming_init
 
-__all__ = ['ResNet', 'make_res_layer']
+__all__ = [
+    'ResNet', 'make_res_layer', 'xavier_init', 'normal_init', 'uniform_init',
+    'kaiming_init'
+]
