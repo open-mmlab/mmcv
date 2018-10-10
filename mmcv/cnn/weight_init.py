9c38b35fb6437d4723550250dbfd1b2e97f580ec
Kai Chen <chenkaidev@gmail.com>
2018-10-10 20:30:36 +0800

    bug fix for checking bias

--- mmcv/cnn/weight_init.py
+++ mmcv/cnn/weight_init.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 def constant_init(module, val, bias=0):
     nn.init.constant_(module.weight, val)
-    if hasattr(module, 'bias'):
+    if hasattr(module, 'bias') and module.bias is not None:
         nn.init.constant_(module.bias, bias)
 
 
@@ -13,19 +13,19 @@ def xavier_init(module, gain=1, bias=0, distribution='normal'):
         nn.init.xavier_uniform_(module.weight, gain=gain)
     else:
         nn.init.xavier_normal_(module.weight, gain=gain)
-    if hasattr(module, 'bias'):
+    if hasattr(module, 'bias') and module.bias is not None:
         nn.init.constant_(module.bias, bias)
 
 
 def normal_init(module, mean=0, std=1, bias=0):
     nn.init.normal_(module.weight, mean, std)
-    if hasattr(module, 'bias'):
+    if hasattr(module, 'bias') and module.bias is not None:
         nn.init.constant_(module.bias, bias)
 
 
 def uniform_init(module, a=0, b=1, bias=0):
     nn.init.uniform_(module.weight, a, b)
-    if hasattr(module, 'bias'):
+    if hasattr(module, 'bias') and module.bias is not None:
         nn.init.constant_(module.bias, bias)
 
 
@@ -41,5 +41,5 @@ def kaiming_init(module,
     else:
         nn.init.kaiming_normal_(
             module.weight, mode=mode, nonlinearity=nonlinearity)
-    if hasattr(module, 'bias'):
+    if hasattr(module, 'bias') and module.bias is not None:
         nn.init.constant_(module.bias, bias)
