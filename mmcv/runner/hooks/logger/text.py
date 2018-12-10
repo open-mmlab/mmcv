9db49e5610024797008b73531adf5ece0a0890dd
thangvu <thangvubk@gmail.com>
2018-12-10 17:28:47 +0900

    minor fix syntax

--- mmcv/runner/hooks/logger/text.py
+++ mmcv/runner/hooks/logger/text.py
@@ -20,7 +20,7 @@ class TextLoggerHook(LoggerHook):
             log_str = 'Epoch({}) [{}][{}]\t'.format(runner.mode, runner.epoch,
                                                     runner.inner_iter + 1)
         if 'time' in runner.log_buffer.output:
-            self.time_sec_tot += (runner.log_buffer.output['time'] * 
+            self.time_sec_tot += (runner.log_buffer.output['time'] *
                                   self.interval)
             time_sec_avg = self.time_sec_tot / (runner.iter + 1)
             eta_sec = time_sec_avg * (runner.max_iters - runner.iter - 1)
