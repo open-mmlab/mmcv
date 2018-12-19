4486b34e3cf9da7c9216a59df95410a733dcc11e
pangjm <pjmzju@gmail.com>
2018-12-19 17:00:43 +0800

    further reformat

--- mmcv/runner/checkpoint.py
+++ mmcv/runner/checkpoint.py
@@ -15,7 +15,7 @@ open_mmlab_model_urls = {
     'resnet101_caffe': 'https://s3.ap-northeast-2.amazonaws.com/open-mmlab/pretrain/third_party/resnet101_caffe-3ad79236.pth',
     'resnext101_32x4d': 'https://s3.ap-northeast-2.amazonaws.com/open-mmlab/pretrain/third_party/resnext101_32x4d-a5af3160.pth',
     'resnext101_64x4d': 'https://s3.ap-northeast-2.amazonaws.com/open-mmlab/pretrain/third_party/resnext101_64x4d-ee2c6f71.pth'
-} # noqa: E501
+}  # noqa: E501
 
 
 def load_state_dict(module, state_dict, strict=False, logger=None):
