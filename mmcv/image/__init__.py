41ce0bc12750f6ced87d3112af838ee45f00da12
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2018-12-20 23:52:52 +0800

    add iminvert

--- mmcv/image/__init__.py
+++ mmcv/image/__init__.py
@@ -2,11 +2,11 @@ from .io import imread, imwrite, imfrombytes
 from .transforms import (bgr2gray, gray2bgr, bgr2rgb, rgb2bgr, bgr2hsv,
                          hsv2bgr, imflip, imrotate, imcrop, impad,
                          impad_to_multiple, imnormalize, imdenormalize,
-                         imresize, imresize_like, imrescale)
+                         imresize, imresize_like, imrescale, iminvert)
 
 __all__ = [
     'imread', 'imwrite', 'imfrombytes', 'bgr2gray', 'gray2bgr', 'bgr2rgb',
     'rgb2bgr', 'bgr2hsv', 'hsv2bgr', 'imflip', 'imrotate', 'imcrop', 'impad',
     'impad_to_multiple', 'imnormalize', 'imdenormalize', 'imresize',
-    'imresize_like', 'imrescale'
+    'imresize_like', 'imrescale', 'iminvert'
 ]
--- mmcv/image/transforms/__init__.py
+++ mmcv/image/transforms/__init__.py
@@ -1,4 +1,5 @@
 from .colorspace import bgr2gray, gray2bgr, bgr2rgb, rgb2bgr, bgr2hsv, hsv2bgr
+from .colorspace import iminvert
 from .geometry import imflip, imrotate, imcrop, impad, impad_to_multiple
 from .normalize import imnormalize, imdenormalize
 from .resize import imresize, imresize_like, imrescale
@@ -6,5 +7,6 @@ from .resize import imresize, imresize_like, imrescale
 __all__ = [
     'bgr2gray', 'gray2bgr', 'bgr2rgb', 'rgb2bgr', 'bgr2hsv', 'hsv2bgr',
     'imflip', 'imrotate', 'imcrop', 'impad', 'impad_to_multiple',
-    'imnormalize', 'imdenormalize', 'imresize', 'imresize_like', 'imrescale'
+    'imnormalize', 'imdenormalize', 'imresize', 'imresize_like', 'imrescale',
+    'iminvert'
 ]
