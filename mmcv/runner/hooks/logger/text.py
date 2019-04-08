0dbeab6e40840f55f5fe6d56498ff311d875044e
Cao Yuhang <yhcao6@gmail.com>
2019-04-08 16:34:38 +0800

    add json log

--- mmcv/runner/hooks/logger/text.py
+++ mmcv/runner/hooks/logger/text.py
@@ -1,7 +1,9 @@
 import datetime
 
 import torch
+import torch.distributed as dist
 
+import mmcv
 from .base import LoggerHook
 
 
@@ -14,8 +16,12 @@ class TextLoggerHook(LoggerHook):
     def before_run(self, runner):
         super(TextLoggerHook, self).before_run(runner)
         self.start_iter = runner.iter
+        self.dump_log_path = '{}/{}_{}'.format(runner.work_dir,
+                                               mmcv.runner.get_time_str(),
+                                               'train.json')
 
     def log(self, runner):
+        log_dict_iter = dict()
         if runner.mode == 'train':
             lr_str = ', '.join(
                 ['{:.5f}'.format(lr) for lr in runner.current_lr()])
@@ -36,12 +42,20 @@ class TextLoggerHook(LoggerHook):
             log_str += (
                 'time: {log[time]:.3f}, data_time: {log[data_time]:.3f}, '.
                 format(log=runner.log_buffer.output))
+            log_dict_iter['train_epoch'] = runner.epoch + 1
+            log_dict_iter['iter'] = runner.inner_iter + 1
+            log_dict_iter['lr'] = float(lr_str)
+            log_dict_iter['time'] = runner.log_buffer.output['time']
+            log_dict_iter['data_time'] = runner.log_buffer.output['data_time']
+        else:
+            log_dict_iter['val_epoch'] = runner.epoch + 1
         # statistic memory
         if runner.mode == 'train' and torch.cuda.is_available():
             mem = torch.cuda.max_memory_allocated()
             mem_mb = int(mem / (1024 * 1024))
             mem_str = 'memory: {}, '.format(mem_mb)
             log_str += mem_str
+            log_dict_iter['memory'] = mem_mb
         log_items = []
         for name, val in runner.log_buffer.output.items():
             if name in ['time', 'data_time']:
@@ -49,5 +63,11 @@ class TextLoggerHook(LoggerHook):
             if isinstance(val, float):
                 val = '{:.4f}'.format(val)
             log_items.append('{}: {}'.format(name, val))
+            log_dict_iter[name] = val
         log_str += ', '.join(log_items)
         runner.logger.info(log_str)
+        if runner.world_size == 1 or (runner.world_size > 1
+                                      and dist.get_rank() == 0):
+            with open(self.dump_log_path, 'a+') as f:
+                mmcv.dump(log_dict_iter, f, file_format='json')
+                f.write('\n')
