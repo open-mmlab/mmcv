5e3d09bc5dc9ce8438325b32f4ac70b6242bd536
Kai Chen <chenkaidev@gmail.com>
2019-04-21 18:51:38 -0700

    add caffe2_xavier_init() because of different implementations of caffe2 and pytorch

--- mmcv/cnn/__init__.py
+++ mmcv/cnn/__init__.py
@@ -2,10 +2,10 @@ from .alexnet import AlexNet
 from .vgg import VGG, make_vgg_layer
 from .resnet import ResNet, make_res_layer
 from .weight_init import (constant_init, xavier_init, normal_init,
-                          uniform_init, kaiming_init)
+                          uniform_init, kaiming_init, caffe2_xavier_init)
 
 __all__ = [
     'AlexNet', 'VGG', 'make_vgg_layer', 'ResNet', 'make_res_layer',
     'constant_init', 'xavier_init', 'normal_init', 'uniform_init',
-    'kaiming_init'
+    'kaiming_init', 'caffe2_xavier_init'
 ]
