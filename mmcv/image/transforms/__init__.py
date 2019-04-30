a092de341cfebfcd25317f17ba0c37b03cc32b54
Gu Wang <guwang12@foxmail.com>
2019-04-30 22:52:14 +0800

    add bgr2hls and hls2bgr

--- mmcv/image/transforms/__init__.py
+++ mmcv/image/transforms/__init__.py
@@ -1,11 +1,12 @@
 from .colorspace import (bgr2gray, gray2bgr, bgr2rgb, rgb2bgr, bgr2hsv,
-                         hsv2bgr, iminvert)
+                         hsv2bgr, bgr2hls, hls2bgr, iminvert)
 from .geometry import imflip, imrotate, imcrop, impad, impad_to_multiple
 from .normalize import imnormalize, imdenormalize
 from .resize import imresize, imresize_like, imrescale
 
 __all__ = [
     'bgr2gray', 'gray2bgr', 'bgr2rgb', 'rgb2bgr', 'bgr2hsv', 'hsv2bgr',
-    'iminvert', 'imflip', 'imrotate', 'imcrop', 'impad', 'impad_to_multiple',
-    'imnormalize', 'imdenormalize', 'imresize', 'imresize_like', 'imrescale'
+    'bgr2hls', 'hls2bgr', 'iminvert', 'imflip', 'imrotate', 'imcrop', 'impad',
+    'impad_to_multiple', 'imnormalize', 'imdenormalize', 'imresize',
+    'imresize_like', 'imrescale'
 ]
