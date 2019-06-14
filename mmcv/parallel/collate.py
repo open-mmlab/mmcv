c3affda0d2c586a7e02c29fa0a92639eac80f5d1
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2019-06-15 00:43:45 +0800

    use deafault yapf

--- mmcv/parallel/collate.py
+++ mmcv/parallel/collate.py
@@ -29,10 +29,8 @@ def collate(batch, samples_per_gpu=1):
             for i in range(0, len(batch), samples_per_gpu):
                 stacked.append(
                     [sample.data for sample in batch[i:i + samples_per_gpu]])
-            return DataContainer(stacked,
-                                 batch[0].stack,
-                                 batch[0].padding_value,
-                                 cpu_only=True)
+            return DataContainer(
+                stacked, batch[0].stack, batch[0].padding_value, cpu_only=True)
         elif batch[0].stack:
             for i in range(0, len(batch), samples_per_gpu):
                 assert isinstance(batch[i].data, torch.Tensor)
@@ -56,8 +54,8 @@ def collate(batch, samples_per_gpu=1):
                             pad[2 * dim -
                                 1] = max_shape[dim - 1] - sample.size(-dim)
                         padded_samples.append(
-                            F.pad(sample.data, pad,
-                                  value=sample.padding_value))
+                            F.pad(
+                                sample.data, pad, value=sample.padding_value))
                     stacked.append(default_collate(padded_samples))
                 elif (batch[i].pad_dims is None):
                     stacked.append(
