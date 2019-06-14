25b854266faafdaefed5746f0c9874dbc930cd96
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2019-06-14 16:34:30 +0800

    fix yapf format

--- mmcv/parallel/collate.py
+++ mmcv/parallel/collate.py
@@ -29,8 +29,10 @@ def collate(batch, samples_per_gpu=1):
             for i in range(0, len(batch), samples_per_gpu):
                 stacked.append(
                     [sample.data for sample in batch[i:i + samples_per_gpu]])
-            return DataContainer(
-                stacked, batch[0].stack, batch[0].padding_value, cpu_only=True)
+            return DataContainer(stacked,
+                                 batch[0].stack,
+                                 batch[0].padding_value,
+                                 cpu_only=True)
         elif batch[0].stack:
             for i in range(0, len(batch), samples_per_gpu):
                 assert isinstance(batch[i].data, torch.Tensor)
@@ -41,29 +43,31 @@ def collate(batch, samples_per_gpu=1):
                     max_shape = [0 for _ in range(batch[i].pad_dims)]
                     for dim in range(1, batch[i].pad_dims + 1):
                         max_shape[dim - 1] = batch[i].size(-dim)
-                    for sample in batch[i: i + samples_per_gpu]:
+                    for sample in batch[i:i + samples_per_gpu]:
                         for dim in range(0, ndim - batch[i].pad_dims):
                             assert batch[i].size(dim) == sample.size(dim)
                         for dim in range(1, batch[i].pad_dims + 1):
-                            max_shape[dim - 1] = max(
-                                    max_shape[dim - 1], sample.size(-dim))
+                            max_shape[dim - 1] = max(max_shape[dim - 1],
+                                                     sample.size(-dim))
                     padded_samples = []
                     for sample in batch[i:i + samples_per_gpu]:
                         pad = [0 for _ in range(batch[i].pad_dims * 2)]
                         for dim in range(1, batch[i].pad_dims + 1):
-                            pad[2*dim-1] = max_shape[dim-1] - sample.size(-dim)
-                        padded_samples.append(F.pad(
-                            sample.data,
-                            pad,
-                            value=sample.padding_value))
+                            pad[2 * dim -
+                                1] = max_shape[dim - 1] - sample.size(-dim)
+                        padded_samples.append(
+                            F.pad(sample.data, pad,
+                                  value=sample.padding_value))
                     stacked.append(default_collate(padded_samples))
                 elif (batch[i].pad_dims is None):
-                    stacked.append(default_collate([
-                        sample.data for sample in batch[i: i + samples_per_gpu]
+                    stacked.append(
+                        default_collate([
+                            sample.data
+                            for sample in batch[i:i + samples_per_gpu]
                         ]))
                 else:
                     raise ValueError(
-                            'pad_dims should be either None or integers (1-3)')
+                        'pad_dims should be either None or integers (1-3)')
 
         else:
             for i in range(0, len(batch), samples_per_gpu):
