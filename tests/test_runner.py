395efbf2e9c4dda3644a46d797e64adfc838a7df
Evgeny Nizhibitsky <nizhibitsky@ya.ru>
2019-06-15 14:56:27 +0300

    Make latest checkpoint symlink relative (#75)
    * Make latest symlink relative. Add tests
    
    * Skip test_save_checkpoint in the absense of torch
    
    * Fix pep8 in test_runner
    
    * Import runner only if torch import has succeeded

---
+++ tests/test_runner.py
@@ -0,0 +1,32 @@
+import os.path as osp
+import tempfile
+import warnings
+
+
+def test_save_checkpoint():
+    try:
+        import torch
+        import torch.nn as nn
+    except ImportError:
+        warnings.warn('Skipping test_save_checkpoint in the absense of torch')
+        return
+
+    import mmcv.runner
+
+    model = nn.Linear(1, 1)
+    runner = mmcv.runner.Runner(
+        model=model,
+        batch_processor=lambda x: x
+    )
+
+    with tempfile.TemporaryDirectory() as root:
+        runner.save_checkpoint(root)
+
+        latest_path = osp.join(root, 'latest.pth')
+        epoch1_path = osp.join(root, 'epoch_1.pth')
+
+        assert osp.exists(latest_path)
+        assert osp.exists(epoch1_path)
+        assert osp.realpath(latest_path) == epoch1_path
+
+        torch.load(latest_path)
