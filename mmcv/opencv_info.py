16ba8fa286c91a5bdc3fc40ef7fc9c21f946b6b8
Kai Chen <chenkaidev@gmail.com>
2019-07-07 16:37:33 +0800

    fix doc generation

--- mmcv/opencv_info.py
+++ mmcv/opencv_info.py
@@ -2,7 +2,11 @@ import cv2
 
 
 def use_opencv2():
-    return cv2.__version__.split('.')[0] == '2'
+    try:
+        major_version = cv2.__version__.split('.')[0]
+    except TypeError:  # solves doc generation issue
+        major_version = 4
+    return major_version == '2'
 
 
 USE_OPENCV2 = use_opencv2()
