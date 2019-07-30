95f59ad67820134e5dea8bb442f28142c59bcbda
Kai Chen <chenkaidev@gmail.com>
2019-07-30 21:31:23 +0800

    minor formatting

--- mmcv/fileio/handlers/yaml_handler.py
+++ mmcv/fileio/handlers/yaml_handler.py
@@ -1,12 +1,11 @@
 import yaml
 
-from .base import BaseFileHandler
-
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 
+from .base import BaseFileHandler  # isort:skip
 
 
 class YamlHandler(BaseFileHandler):
