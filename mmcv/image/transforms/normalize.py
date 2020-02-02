f2e79cbdcfbf0b14b2b801334c5b1ee84a33b2ac
lizz <lizz@sensetime.com>
2020-02-03 00:01:28 +0800

    multipy faster than divide

--- mmcv/image/transforms/normalize.py
+++ mmcv/image/transforms/normalize.py
@@ -8,10 +8,10 @@ from .colorspace import bgr2rgb, rgb2bgr
 def imnormalize(img, mean, std, to_rgb=True):
     img = img.astype(np.float32)
     mean = mean.reshape(1, 3).astype(np.float64)
-    std = std.reshape(1, 3).astype(np.float64)
+    stdinv = 1 / std.reshape(1, 3).astype(np.float64)
     if to_rgb:
         img = bgr2rgb(img)
-    return cv2.divide(cv2.subtract(img, mean), std)
+    return cv2.multiply(cv2.subtract(img, mean), stdinv)
 
 
 def imdenormalize(img, mean, std, to_bgr=True):
