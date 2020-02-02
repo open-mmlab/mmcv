b312e30eac0c239bf4acae6201c4ed6aabdb9b4a
lizz <lizz@sensetime.com>
2020-02-02 18:06:09 +0800

    Use cv2 to normalize img

--- mmcv/image/transforms/normalize.py
+++ mmcv/image/transforms/normalize.py
@@ -1,4 +1,5 @@
 # Copyright (c) Open-MMLab. All rights reserved.
+import cv2
 import numpy as np
 
 from .colorspace import bgr2rgb, rgb2bgr
@@ -6,9 +7,11 @@ from .colorspace import bgr2rgb, rgb2bgr
 
 def imnormalize(img, mean, std, to_rgb=True):
     img = img.astype(np.float32)
+    mean = mean.reshape(1, 3).astype(np.float64)
+    std = std.reshape(1, 3).astype(np.float64)
     if to_rgb:
         img = bgr2rgb(img)
-    return (img - mean) / std
+    return cv2.divide(cv2.subtract(img, mean), std)
 
 
 def imdenormalize(img, mean, std, to_bgr=True):
