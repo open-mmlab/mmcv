d6ce3a2e1b42d6cb1b3505e51f9323419cc9e8a4
Kai Chen <chenkaidev@gmail.com>
2020-02-03 23:44:39 +0800

    add _load_checkpoint() to handle the loading only (#178)

--- mmcv/runner/__init__.py
+++ mmcv/runner/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Open-MMLab. All rights reserved.
-from .checkpoint import (load_checkpoint, load_state_dict, save_checkpoint,
-                         weights_to_cpu)
+from .checkpoint import (_load_checkpoint, load_checkpoint, load_state_dict,
+                         save_checkpoint, weights_to_cpu)
 from .dist_utils import get_dist_info, init_dist, master_only
 from .hooks import (CheckpointHook, ClosureHook, DistSamplerSeedHook, Hook,
                     IterTimerHook, LoggerHook, LrUpdaterHook, OptimizerHook,
@@ -16,8 +16,8 @@ __all__ = [
     'Runner', 'LogBuffer', 'Hook', 'CheckpointHook', 'ClosureHook',
     'LrUpdaterHook', 'OptimizerHook', 'IterTimerHook', 'DistSamplerSeedHook',
     'LoggerHook', 'TextLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook',
-    'WandbLoggerHook', 'load_state_dict', 'load_checkpoint', 'weights_to_cpu',
-    'save_checkpoint', 'parallel_test', 'Priority', 'get_priority',
-    'get_host_info', 'get_time_str', 'obj_from_dict', 'init_dist',
-    'get_dist_info', 'master_only'
+    'WandbLoggerHook', '_load_checkpoint', 'load_state_dict',
+    'load_checkpoint', 'weights_to_cpu', 'save_checkpoint', 'parallel_test',
+    'Priority', 'get_priority', 'get_host_info', 'get_time_str',
+    'obj_from_dict', 'init_dist', 'get_dist_info', 'master_only'
 ]
