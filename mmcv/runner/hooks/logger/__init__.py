a290d7c16d27441e90a0a14760264d397496de9a
Kai Chen <chenkaidev@gmail.com>
2020-02-12 22:39:52 +0800

    remove the outdated PaviLoggerHook and add requirements.txt

--- mmcv/runner/hooks/logger/__init__.py
+++ mmcv/runner/hooks/logger/__init__.py
@@ -1,11 +1,9 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 from .base import LoggerHook
-from .pavi import PaviLoggerHook
 from .tensorboard import TensorboardLoggerHook
 from .text import TextLoggerHook
 from .wandb import WandbLoggerHook
 
 __all__ = [
-    'LoggerHook', 'TextLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook',
-    'WandbLoggerHook'
+    'LoggerHook', 'TextLoggerHook', 'TensorboardLoggerHook', 'WandbLoggerHook'
 ]
