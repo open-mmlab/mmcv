6fb5ca0131b8aa6c34cb139a673b8cb43f4f3173
Kai Chen <chenkaidev@gmail.com>
2020-02-12 16:38:19 +0800

    Fix the missing numpy module for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ extensions = [
     'recommonmark',
 ]
 
-autodoc_mock_imports = ['cv2', 'torch', 'enum', 'pathlib', 'mmcv._ext']
+autodoc_mock_imports = ['cv2', 'numpy', 'torch', 'enum', 'pathlib', 'mmcv._ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
