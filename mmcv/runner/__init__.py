7647ab374c46f3f7e01bec1502778e3126505cbb
Kai Chen <chenkaidev@gmail.com>
2020-02-14 00:52:56 +0800

    fix an import error

--- mmcv/runner/__init__.py
+++ mmcv/runner/__init__.py
@@ -4,8 +4,7 @@ from .checkpoint import (_load_checkpoint, load_checkpoint, load_state_dict,
 from .dist_utils import get_dist_info, init_dist, master_only
 from .hooks import (CheckpointHook, ClosureHook, DistSamplerSeedHook, Hook,
                     IterTimerHook, LoggerHook, LrUpdaterHook, OptimizerHook,
-                    PaviLoggerHook, TensorboardLoggerHook, TextLoggerHook,
-                    WandbLoggerHook)
+                    TensorboardLoggerHook, TextLoggerHook, WandbLoggerHook)
 from .log_buffer import LogBuffer
 from .parallel_test import parallel_test
 from .priority import Priority, get_priority
@@ -15,9 +14,9 @@ from .utils import get_host_info, get_time_str, obj_from_dict
 __all__ = [
     'Runner', 'LogBuffer', 'Hook', 'CheckpointHook', 'ClosureHook',
     'LrUpdaterHook', 'OptimizerHook', 'IterTimerHook', 'DistSamplerSeedHook',
-    'LoggerHook', 'TextLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook',
-    'WandbLoggerHook', '_load_checkpoint', 'load_state_dict',
-    'load_checkpoint', 'weights_to_cpu', 'save_checkpoint', 'parallel_test',
-    'Priority', 'get_priority', 'get_host_info', 'get_time_str',
-    'obj_from_dict', 'init_dist', 'get_dist_info', 'master_only'
+    'LoggerHook', 'TextLoggerHook', 'TensorboardLoggerHook', 'WandbLoggerHook',
+    '_load_checkpoint', 'load_state_dict', 'load_checkpoint', 'weights_to_cpu',
+    'save_checkpoint', 'parallel_test', 'Priority', 'get_priority',
+    'get_host_info', 'get_time_str', 'obj_from_dict', 'init_dist',
+    'get_dist_info', 'master_only'
 ]
