a47ca7a9a268608b047786a00aa7847c2288c371
Kai Chen <chenkaidev@gmail.com>
2020-02-14 00:57:37 +0800

    Fix an import error caused by legacy codes (#186)
    * fix an import error
    
    * fix an import error

--- mmcv/runner/hooks/__init__.py
+++ mmcv/runner/hooks/__init__.py
@@ -3,8 +3,8 @@ from .checkpoint import CheckpointHook
 from .closure import ClosureHook
 from .hook import Hook
 from .iter_timer import IterTimerHook
-from .logger import (LoggerHook, PaviLoggerHook, TensorboardLoggerHook,
-                     TextLoggerHook, WandbLoggerHook)
+from .logger import (LoggerHook, TensorboardLoggerHook, TextLoggerHook,
+                     WandbLoggerHook)
 from .lr_updater import LrUpdaterHook
 from .memory import EmptyCacheHook
 from .optimizer import OptimizerHook
@@ -13,6 +13,5 @@ from .sampler_seed import DistSamplerSeedHook
 __all__ = [
     'Hook', 'CheckpointHook', 'ClosureHook', 'LrUpdaterHook', 'OptimizerHook',
     'IterTimerHook', 'DistSamplerSeedHook', 'EmptyCacheHook', 'LoggerHook',
-    'TextLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook',
-    'WandbLoggerHook'
+    'TextLoggerHook', 'TensorboardLoggerHook', 'WandbLoggerHook'
 ]
