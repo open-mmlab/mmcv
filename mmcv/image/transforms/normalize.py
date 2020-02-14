13da1b64bb781c0830562bd7655686912570e9e3
lizz <lizz@sensetime.com>
2020-02-14 15:53:33 +0800

    even faster
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmcv/image/transforms/normalize.py
+++ mmcv/image/transforms/normalize.py
@@ -6,18 +6,21 @@ from .colorspace import bgr2rgb, rgb2bgr
 
 
 def imnormalize(img, mean, std, to_rgb=True):
-    img = img.astype(np.float32)
-    mean = mean.reshape(1, -1).astype(np.float64)
-    stdinv = 1 / std.reshape(1, -1).astype(np.float64)
+    img = np.float32(img)
+    mean = np.float64(mean.reshape(1, -1))
+    stdinv = 1 / np.float64(std.reshape(1, -1))
     if to_rgb:
         img = bgr2rgb(img)
-    return cv2.multiply(cv2.subtract(img, mean), stdinv)
+    img = cv2.subtract(img, mean)  # make a copy
+    cv2.multiply(img, stdinv, img)  # inplace
+    return img
 
 
 def imdenormalize(img, mean, std, to_bgr=True):
     mean = mean.reshape(1, -1).astype(np.float64)
     std = std.reshape(1, -1).astype(np.float64)
-    img = cv2.add(cv2.multiply(img, std), mean)
+    img = cv2.multiply(img, std)  # make a copy
+    img = cv2.add(img, mean, img)  # inplace
     if to_bgr:
         img = rgb2bgr(img)
     return img
