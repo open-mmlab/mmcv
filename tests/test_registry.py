e56adfa40848448cd425dc365c6d8644c1fc7033
Kai Chen <chenkaidev@gmail.com>
2020-02-23 21:42:56 +0800

    add registry and build_from_cfg

---
+++ tests/test_registry.py
@@ -0,0 +1,88 @@
+import pytest
+
+import mmcv
+
+
+def test_registry():
+    reg_name = 'cat'
+    CATS = mmcv.Registry(reg_name)
+    assert CATS.name == reg_name
+    assert CATS.module_dict == {}
+    assert len(CATS) == 0
+
+    @CATS.register_module
+    class BritishShorthair:
+        pass
+
+    assert len(CATS) == 1
+    assert CATS.get('BritishShorthair') is BritishShorthair
+
+    class Munchkin:
+        pass
+
+    CATS.register_module(Munchkin)
+    assert len(CATS) == 2
+    assert CATS.get('Munchkin') is Munchkin
+
+    assert CATS.get('PersianCat') is None
+
+    assert repr(
+        CATS) == "Registry(name=cat, items=['BritishShorthair', 'Munchkin'])"
+
+
+def test_build_from_cfg():
+    BACKBONES = mmcv.Registry('backbone')
+
+    @BACKBONES.register_module
+    class ResNet:
+
+        def __init__(self, depth, stages=4):
+            self.depth = depth
+            self.stages = stages
+
+    @BACKBONES.register_module
+    class ResNeXt:
+
+        def __init__(self, depth, stages=4):
+            self.depth = depth
+            self.stages = stages
+
+    cfg = dict(type='ResNet', depth=50)
+    model = mmcv.build_from_cfg(cfg, BACKBONES)
+    assert isinstance(model, ResNet)
+    assert model.depth == 50 and model.stages == 4
+
+    cfg = dict(type='ResNet', depth=50)
+    model = mmcv.build_from_cfg(cfg, BACKBONES, default_args={'stages': 3})
+    assert isinstance(model, ResNet)
+    assert model.depth == 50 and model.stages == 3
+
+    cfg = dict(type='ResNeXt', depth=50, stages=3)
+    model = mmcv.build_from_cfg(cfg, BACKBONES)
+    assert isinstance(model, ResNeXt)
+    assert model.depth == 50 and model.stages == 3
+
+    cfg = dict(type=ResNet, depth=50)
+    model = mmcv.build_from_cfg(cfg, BACKBONES)
+    assert isinstance(model, ResNet)
+    assert model.depth == 50 and model.stages == 4
+
+    with pytest.raises(KeyError):
+        cfg = dict(type='VGG')
+        model = mmcv.build_from_cfg(cfg, BACKBONES)
+
+    with pytest.raises(TypeError):
+        cfg = dict(type=1000)
+        model = mmcv.build_from_cfg(cfg, BACKBONES)
+
+    with pytest.raises(TypeError):
+        cfg = dict(depth=50, stages=4)
+        model = mmcv.build_from_cfg(cfg, BACKBONES)
+
+    with pytest.raises(TypeError):
+        dict(type='ResNet', depth=50)
+        model = mmcv.build_from_cfg(cfg, 'BACKBONES')
+
+    with pytest.raises(TypeError):
+        dict(type='ResNet', depth=50)
+        model = mmcv.build_from_cfg(cfg, BACKBONES, default_args=0)
