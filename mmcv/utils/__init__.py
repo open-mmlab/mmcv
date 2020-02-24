34197c5c89cbea4ffc5fb19f1f3b854216ef736b
Kai Chen <chenkaidev@gmail.com>
2020-02-24 22:21:01 +0800

    Add registry and build_from_cfg (#195)
    * add registry and build_from_cfg
    
    * add some corner cases
    
    * add some corner cases
    
    * fix the unittest for python 3.5
    
    * minor fix

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -8,14 +8,16 @@ from .path import (FileNotFoundError, check_file_exist, fopen, is_filepath,
                    mkdir_or_exist, scandir, symlink)
 from .progressbar import (ProgressBar, track_iter_progress,
                           track_parallel_progress, track_progress)
+from .registry import Registry, build_from_cfg
 from .timer import Timer, TimerError, check_time
 
 __all__ = [
-    'ConfigDict', 'Config', 'is_str', 'iter_cast', 'list_cast', 'tuple_cast',
-    'is_seq_of', 'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
-    'check_prerequisites', 'requires_package', 'requires_executable',
-    'is_filepath', 'fopen', 'check_file_exist', 'mkdir_or_exist', 'symlink',
-    'scandir', 'FileNotFoundError', 'ProgressBar', 'track_progress',
+    'ConfigDict', 'Config', 'Registry', 'build_from_cfg', 'is_str',
+    'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of', 'is_list_of',
+    'is_tuple_of', 'slice_list', 'concat_list', 'check_prerequisites',
+    'requires_package', 'requires_executable', 'is_filepath', 'fopen',
+    'check_file_exist', 'mkdir_or_exist', 'symlink', 'scandir',
+    'FileNotFoundError', 'ProgressBar', 'track_progress',
     'track_iter_progress', 'track_parallel_progress', 'Timer', 'TimerError',
     'check_time'
 ]
