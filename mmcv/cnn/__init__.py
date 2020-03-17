fa84b264229d6e09973a7053fd5e6b3eb35ea592
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-03-17 20:48:46 +0800

    add bias_init_with_prob (#213)
    * add bia_init_with_prob
    
    * add test_weight_init

--- mmcv/cnn/__init__.py
+++ mmcv/cnn/__init__.py
@@ -2,11 +2,12 @@
 from .alexnet import AlexNet
 from .resnet import ResNet, make_res_layer
 from .vgg import VGG, make_vgg_layer
-from .weight_init import (caffe2_xavier_init, constant_init, kaiming_init,
-                          normal_init, uniform_init, xavier_init)
+from .weight_init import (bias_init_with_prob, caffe2_xavier_init,
+                          constant_init, kaiming_init, normal_init,
+                          uniform_init, xavier_init)
 
 __all__ = [
     'AlexNet', 'VGG', 'make_vgg_layer', 'ResNet', 'make_res_layer',
     'constant_init', 'xavier_init', 'normal_init', 'uniform_init',
-    'kaiming_init', 'caffe2_xavier_init'
+    'kaiming_init', 'caffe2_xavier_init', 'bias_init_with_prob'
 ]
