3367651621f28f69d993daf4fcfb4b706e314e40
David de la Iglesia Castro <diglesia@gradiant.org>
2020-04-03 12:01:12 +0200

    Simplify using high level API

--- mmcv/runner/hooks/logger/mlflow.py
+++ mmcv/runner/hooks/logger/mlflow.py
@@ -10,8 +10,7 @@ from .base import LoggerHook
 class MlflowLoggerHook(LoggerHook):
 
     def __init__(self,
-                 experiment_name,
-                 tracking_uri = None,
+                 experiment_name=None,
                  tags = None,
                  log_model=True,
                  interval=10,
@@ -20,7 +19,6 @@ class MlflowLoggerHook(LoggerHook):
         super(MlflowLoggerHook, self).__init__(interval, ignore_last,
                                               reset_flag)
         self.import_mlflow()
-        self._mlflow_client = self.mlflow.tracking.MlflowClient(tracking_uri)
         self.experiment_name = experiment_name
         self.tags = tags
         self.log_model = log_model
@@ -37,30 +35,20 @@ class MlflowLoggerHook(LoggerHook):
 
     @master_only
     def before_run(self, runner):
-        experiment = self._mlflow_client.get_experiment_by_name(self.experiment_name)
-        if experiment:
-            self._experiment_id = experiment.experiment_id
-        else:
-            runner.logger.warning(f'Experiment with name {self.experiment_name} not found. Creating it.')
-            self._experiment_id = self._mlflow_client.create_experiment(name=self.experiment_name)
-
-        if self.mlflow.active_run():
-            run = self._mlflow_client.get_run(self.mlflow.active_run())
-        else:
-            run = self._mlflow_client.create_run(experiment_id=self._experiment_id, tags=self.tags)
-        
-        self._run_id = run.info.run_id
+        if self.experiment_name is not None:
+            self.mlflow.set_experiment(experiment_name)
 
     @master_only
     def log(self, runner):
+        metrics = {}
         for var, val in runner.log_buffer.output.items():
             if var in ['time', 'data_time']:
                 continue
             tag = '{}/{}'.format(var, runner.mode)
-            self._mlflow_client.log_metric(self._run_id, tag, val, step=runner.iter)
+            metrics[tag] = val
+        self.mlflow.log_metrics(metrics, step=runner.iter)
 
     @master_only
     def after_run(self, runner):
         if self.log_model:
             self.mlflow_pytorch.log_model(runner.model, "models")
-        self._mlflow_client.set_terminated(self._run_id)
