eed03e5468399cec698b8802cd644857e6ca4f1f
David de la Iglesia Castro <diglesia@gradiant.org>
2020-04-03 10:44:23 +0200

    Add MLflowLoggerHook to __all__

--- mmcv/runner/hooks/logger/__init__.py
+++ mmcv/runner/hooks/logger/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 from .base import LoggerHook
+from .mlflow import MLflowLoggerHook
 from .pavi import PaviLoggerHook
 from .tensorboard import TensorboardLoggerHook
 from .text import TextLoggerHook
 from .wandb import WandbLoggerHook
 
 __all__ = [
-    'LoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook', 'TextLoggerHook',
+    'LoggerHook', 'MLflowLoggerHook', 'PaviLoggerHook', 'TensorboardLoggerHook', 'TextLoggerHook',
     'WandbLoggerHook'
 ]
