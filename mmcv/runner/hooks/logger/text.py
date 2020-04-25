81a9b4326b7e8259e99749b95a07c94a32c3e6e5
xurui <xurui4@sensetime.com>
2020-04-25 13:58:23 +0800

    fix f-string bug in py3.5

--- mmcv/runner/hooks/logger/text.py
+++ mmcv/runner/hooks/logger/text.py
@@ -37,7 +37,7 @@ class TextLoggerHook(LoggerHook):
 
     def _log_info(self, log_dict, runner):
         if runner.meta is not None and 'exp_name' in runner.meta:
-            log_str = f"Exp name: {runner.meta['exp_name']}\t"
+            log_str = 'Exp name: {}\t'.format(runner.meta['exp_name'])
         else:
             log_str = ''
         if runner.mode == 'train':
