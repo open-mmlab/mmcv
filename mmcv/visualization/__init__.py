17e23143317abb0a2049eac643686dec40216990
Kai Chen <chenkaidev@gmail.com>
2020-04-25 23:16:57 +0800

    fix imports

--- mmcv/visualization/__init__.py
+++ mmcv/visualization/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 from .color import Color, color_val
-from .figure import Figure, plot_bbox_labels, plot_img
+from .figure import Figure, plot_bbox_labels, plot_bboxes
 from .image import imshow, imshow_bboxes, imshow_det_bboxes
 from .optflow import flow2rgb, flowshow, make_color_wheel
 
 __all__ = [
-    'Color', 'color_val', 'Figure', 'plot_img', 'plot_bbox_labels', 'imshow',
-    'imshow_bboxes', 'imshow_det_bboxes', 'flowshow', 'flow2rgb',
+    'Color', 'color_val', 'Figure', 'plot_bbox_labels', 'plot_bboxes',
+    'imshow', 'imshow_bboxes', 'imshow_det_bboxes', 'flowshow', 'flow2rgb',
     'make_color_wheel'
 ]
