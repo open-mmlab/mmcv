f7de63fdd0b48a56eadb717cd9260260705f0632
Kai Chen <chenkaidev@gmail.com>
2020-04-27 22:47:49 +0800

    Support specifying names for registry.register_module() (#251)
    * support specifying names for registry.register_module()
    
    * minor fix
    
    * add more unittests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -4,9 +4,8 @@ import mmcv
 
 
 def test_registry():
-    reg_name = 'cat'
-    CATS = mmcv.Registry(reg_name)
-    assert CATS.name == reg_name
+    CATS = mmcv.Registry('cat')
+    assert CATS.name == 'cat'
     assert CATS.module_dict == {}
     assert len(CATS) == 0
 
@@ -31,9 +30,10 @@ def test_registry():
     CATS.register_module(Munchkin, force=True)
     assert len(CATS) == 2
 
+    # force=False
     with pytest.raises(KeyError):
 
-        @CATS.register_module
+        @CATS.register_module()
         class BritishShorthair:
             pass
 
@@ -46,15 +46,79 @@ def test_registry():
     assert CATS.get('PersianCat') is None
     assert 'PersianCat' not in CATS
 
-    # The order of dict keys are not preserved in python 3.5
-    assert repr(CATS) in [
-        "Registry(name=cat, items=['BritishShorthair', 'Munchkin'])",
-        "Registry(name=cat, items=['Munchkin', 'BritishShorthair'])"
-    ]
+    @CATS.register_module(name='Siamese')
+    class SiameseCat:
+        pass
+
+    assert CATS.get('Siamese').__name__ == 'SiameseCat'
+
+    class SphynxCat:
+        pass
+
+    CATS.register_module(name='Sphynx', module=SphynxCat)
+    assert CATS.get('Sphynx') is SphynxCat
+
+    repr_str = 'Registry(name=cat, items={'
+    repr_str += ("'BritishShorthair': <class 'test_registry.test_registry."
+                 "<locals>.BritishShorthair'>, ")
+    repr_str += ("'Munchkin': <class 'test_registry.test_registry."
+                 "<locals>.Munchkin'>, ")
+    repr_str += ("'Siamese': <class 'test_registry.test_registry."
+                 "<locals>.SiameseCat'>, ")
+    repr_str += ("'Sphynx': <class 'test_registry.test_registry."
+                 "<locals>.SphynxCat'>")
+    repr_str += '})'
+    assert repr(CATS) == repr_str
 
     # the registered module should be a class
     with pytest.raises(TypeError):
         CATS.register_module(0)
+
+    # can only decorate a class
+    with pytest.raises(TypeError):
+
+        @CATS.register_module()
+        def some_method():
+            pass
+
+    # begin: test old APIs
+    with pytest.warns(DeprecationWarning):
+        CATS.register_module(SphynxCat)
+        assert CATS.get('SphynxCat').__name__ == 'SphynxCat'
+
+    with pytest.warns(DeprecationWarning):
+        CATS.register_module(SphynxCat, force=True)
+        assert CATS.get('SphynxCat').__name__ == 'SphynxCat'
+
+    with pytest.warns(DeprecationWarning):
+
+        @CATS.register_module
+        class NewCat:
+            pass
+
+        assert CATS.get('NewCat').__name__ == 'NewCat'
+
+    with pytest.warns(DeprecationWarning):
+        CATS.deprecated_register_module(SphynxCat, force=True)
+        assert CATS.get('SphynxCat').__name__ == 'SphynxCat'
+
+    with pytest.warns(DeprecationWarning):
+
+        @CATS.deprecated_register_module
+        class CuteCat:
+            pass
+
+        assert CATS.get('CuteCat').__name__ == 'CuteCat'
+
+    with pytest.warns(DeprecationWarning):
+
+        @CATS.deprecated_register_module(force=True)
+        class NewCat2:
+            pass
+
+        assert CATS.get('NewCat2').__name__ == 'NewCat2'
+
+    # end: test old APIs
 
 
 def test_build_from_cfg():
@@ -94,10 +158,20 @@ def test_build_from_cfg():
     assert isinstance(model, ResNet)
     assert model.depth == 50 and model.stages == 4
 
+    # not a registry
+    with pytest.raises(TypeError):
+        cfg = dict(type='VGG')
+        model = mmcv.build_from_cfg(cfg, 'BACKBONES')
+
     # non-registered class
     with pytest.raises(KeyError):
         cfg = dict(type='VGG')
         model = mmcv.build_from_cfg(cfg, BACKBONES)
+
+    # default_args must be a dict or None
+    with pytest.raises(TypeError):
+        cfg = dict(type='ResNet', depth=50)
+        model = mmcv.build_from_cfg(cfg, BACKBONES, default_args=1)
 
     # cfg['type'] should be a str or class
     with pytest.raises(TypeError):
