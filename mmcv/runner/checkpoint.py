e455123de2ff4d2b0b47c60e4f72d45ececa4620
xurui <xurui4@sensetime.com>
2020-04-27 17:19:49 +0800

    fix bugs

--- mmcv/runner/checkpoint.py
+++ mmcv/runner/checkpoint.py
@@ -248,3 +248,4 @@ def save_checkpoint(model, filename, optimizer=None, meta=None):
     # immediately flush buffer
     with open(filename, 'wb') as f:
         torch.save(checkpoint, f)
+        f.flush()
