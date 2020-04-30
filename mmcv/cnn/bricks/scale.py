be05c916a6680eafa07036f2f7d91c095afa62bc
Jiarui XU <xvjiarui0826@gmail.com>
2020-04-30 13:20:38 +0800

    add doc string

--- mmcv/cnn/bricks/scale.py
+++ mmcv/cnn/bricks/scale.py
@@ -3,8 +3,13 @@ import torch.nn as nn
 
 
 class Scale(nn.Module):
-    """
-    A learnable scale parameter
+    """A learnable scale parameter.
+
+    This layer scales the input by a learnable factor. It multiplies a
+    learnable scale parameter of shape (1,) with input of any shape.
+
+    Args:
+        scale (float): Initial value of scale factor. Default: 1.0
     """
 
     def __init__(self, scale=1.0):
