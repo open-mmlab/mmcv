3654a580fb18aba6334f32cf16795ffae4d682ac
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-22 10:03:37 +0800

    add more test

--- tests/test_load_checkpoint.py
+++ tests/test_load_checkpoint.py
@@ -50,13 +50,23 @@ def test_get_external_models():
         get_external_models()
 
 
-def load_url(url, model_dir):
+def load_url(url, model_dir=None):
     return url, model_dir
 
 
 @patch('mmcv.__path__', [osp.join(osp.dirname(__file__), 'data/')])
 @patch('mmcv.runner.checkpoint.load_url_dist', load_url)
 def test_load_external_url():
+    # test modelzoo://
+    url, model_dir = _load_checkpoint('modelzoo://resnet50')
+    assert url == 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
+    assert model_dir is None
+
+    # test torchvision://
+    url, model_dir = _load_checkpoint('torchvision://resnet50')
+    assert url == 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
+    assert model_dir is None
+
     # test open-mmlab://
     os.environ.pop(ENV_MMCV_HOME, None)
     os.environ.pop(ENV_XDG_CACHE_HOME, None)
@@ -75,3 +85,11 @@ def test_load_external_url():
     url, model_dir = _load_checkpoint('local://test1')
     assert url == 'test1/test1.pth'
     assert model_dir == osp.join(mmcv_home, 'test1')
+
+    # test http:// https://
+    url, model_dir = _load_checkpoint('http://localhost/train.pth')
+    assert url == 'http://localhost/train.pth'
+    assert model_dir is None
+    url, model_dir = _load_checkpoint('https://localhost/train.pth')
+    assert url == 'https://localhost/train.pth'
+    assert model_dir is None
