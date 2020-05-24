025291a7920518dd42641535e15807c30f891e9d
mmeendez8 <miguelmndez@gmail.com>
2020-05-24 11:07:16 +0200

    Force copy of input img

--- mmcv/image/photometric.py
+++ mmcv/image/photometric.py
@@ -14,7 +14,7 @@ def imnormalize(img, mean, std, to_rgb=True):
     Returns:
         ndarray: The normalized image.
     """
-    img = np.float32(img) if img.dtype != np.float32 else img.copy()
+    img = img.copy().astype(np.float32)
     return imnormalize_(img, mean, std, to_rgb)
 
 
