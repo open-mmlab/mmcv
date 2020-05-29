1f1d9f23721f579eb2ed324786425c61e830c197
ZwwWayne <wayne.zw@outlook.com>
2020-05-29 10:47:40 +0800

    update pre-commit version and fix format

--- tests/data/config/a.py
+++ tests/data/config/a.py
@@ -2,8 +2,4 @@ item1 = [1, 2]
 item2 = True
 item3 = 'test'
 item4 = dict(
-    a={'c/d': 'path/d'},
-    b={'8': 666},
-    c={9: 666},
-    d={'a': 0},
-    f=dict(a='666'))
+    a={'c/d': 'path/d'}, b={'8': 666}, c={9: 666}, d={'a': 0}, f=dict(a='666'))
