1fa0b1b100cac58a3d95f39129a0c10db963d0fd
louzan <loudaxian@outlook.com>
2020-06-19 00:38:17 +0800

    add hswish and hsigmoid

---
+++ mmcv/cnn/bricks/hsigmoid.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+from .registry import ACTIVATION_LAYERS
+
+
+@ACTIVATION_LAYERS.register_module()
+class HSigmoid(nn.Module):
+    """Hard Sigmoid Module. Apply the hard sigmoid function:
+    Hsigmoid(x) = min(max(0, (x + 1) / 2), 1)
+
+    Returns:
+        Tensor: The output tensor.
+    """
+
+    def __init__(self):
+        super(HSigmoid, self).__init__()
+
+    @staticmethod
+    def forward(x):
+        x = (x + 1) / 2 
+
+        return x.clamp_(0, 1)
