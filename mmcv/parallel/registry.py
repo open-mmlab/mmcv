68708cfbe6587c61c9497ae41f02a5f7c86399c8
Harry <hejm37@mail2.sysu.edu.cn>
2020-06-19 00:45:46 +0800

    fix: update module wrapper register_module (#357)

--- mmcv/parallel/registry.py
+++ mmcv/parallel/registry.py
@@ -3,5 +3,5 @@ from torch.nn.parallel import DataParallel, DistributedDataParallel
 from mmcv.utils import Registry
 
 MODULE_WRAPPERS = Registry('module wrapper')
-MODULE_WRAPPERS.register_module(DataParallel)
-MODULE_WRAPPERS.register_module(DistributedDataParallel)
+MODULE_WRAPPERS.register_module(module=DataParallel)
+MODULE_WRAPPERS.register_module(module=DistributedDataParallel)
