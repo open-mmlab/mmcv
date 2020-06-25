0c7bbeab7af91fc90655235bb5754f94e2560fac
xiongyu <xiongyuxy@gmail.com>
2020-06-25 14:00:16 +0800

    revise utils.init by try-except-else block

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -10,8 +10,19 @@ from .progressbar import (ProgressBar, track_iter_progress,
                           track_parallel_progress, track_progress)
 from .timer import Timer, TimerError, check_time
 
-try: 
+try:
     import torch
+except ImportError:
+    __all__ = [
+        'Config', 'ConfigDict', 'DictAction', 'is_str', 'iter_cast',
+        'list_cast', 'tuple_cast', 'is_seq_of', 'is_list_of', 'is_tuple_of',
+        'slice_list', 'concat_list', 'check_prerequisites', 'requires_package',
+        'requires_executable', 'is_filepath', 'fopen', 'check_file_exist',
+        'mkdir_or_exist', 'symlink', 'scandir', 'ProgressBar',
+        'track_progress', 'track_iter_progress', 'track_parallel_progress',
+        'Timer', 'TimerError', 'check_time'
+    ]
+else:
     from .env import TORCH_VERSION
     from .logging import get_logger, print_log
     from .parrots_wrapper import (CUDA_HOME, BuildExtension, CppExtension,
@@ -21,7 +32,6 @@ try:
                                   _ConvNd, _ConvTransposeMixin, _InstanceNorm,
                                   _MaxPoolNd, get_build_config)
     from .registry import Registry, build_from_cfg
-
     __all__ = [
         'Config', 'ConfigDict', 'DictAction', 'get_logger', 'print_log',
         'is_str', 'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of',
@@ -36,15 +46,4 @@ try:
         '_InstanceNorm', '_MaxPoolNd', 'get_build_config', 'BuildExtension',
         'CppExtension', 'CUDAExtension', 'DataLoader', 'PoolDataLoader',
         'TORCH_VERSION'
-    ]
-except ImportError:
-    
-    __all__ = [
-        'Config', 'ConfigDict', 'DictAction', 'is_str',
-        'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of', 'is_list_of',
-        'is_tuple_of', 'slice_list', 'concat_list', 'check_prerequisites',
-        'requires_package', 'requires_executable', 'is_filepath', 'fopen',
-        'check_file_exist', 'mkdir_or_exist', 'symlink', 'scandir',
-        'ProgressBar', 'track_progress', 'track_iter_progress',
-        'track_parallel_progress', 'Timer', 'TimerError', 'check_time'
     ]
