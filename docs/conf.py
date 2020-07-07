b68b8a9cf7ead68ca16cb43ca8712cc5d90cb414
ZwwWayne <wayne.zw@outlook.com>
2020-07-07 23:27:00 +0800

    clean code

--- docs/conf.py
+++ docs/conf.py
@@ -63,11 +63,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# #
-# source_suffix = {
-#     '.rst': 'restructuredtext',
-#     '.md': 'markdown',
-# }
+#
 source_suffix = ['.rst', '.md']
 
 # The master toctree document.
