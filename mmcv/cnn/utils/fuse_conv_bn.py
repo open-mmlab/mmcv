c534a3c806462e6067db206069d63a75e2acbb93
Cao Yuhang <yhcao6@gmail.com>
2020-07-08 17:26:01 +0800

    fix doc and variable name

--- mmcv/cnn/utils/fuse_conv_bn.py
+++ mmcv/cnn/utils/fuse_conv_bn.py
@@ -5,11 +5,6 @@ import torch.nn as nn
 def fuse_conv_bn_single(conv, bn):
     """Fuse conv and bn into one module.
 
-    During inference, the functionary of batch norm layers is turned off
-    but only the mean and var alone channels are used, which exposes the
-    chance to fuse it with the preceding conv layers to save computations and
-    simplify network structures.
-
     Args:
         conv (nn.Module): Conv to be fused.
         bn (nn.Module): BN to be fused.
@@ -28,11 +23,16 @@ def fuse_conv_bn_single(conv, bn):
     return conv
 
 
-def fuse_conv_bn(m):
+def fuse_conv_bn(module):
     """Recursively fuse conv and bn in a module.
 
+    During inference, the functionary of batch norm layers is turned off
+    but only the mean and var alone channels are used, which exposes the
+    chance to fuse it with the preceding conv layers to save computations and
+    simplify network structures.
+
     Args:
-        m: Module to be fused.
+        module (nn.Module): Module to be fused.
 
     Returns:
         nn.Module: Fused module.
@@ -40,19 +40,19 @@ def fuse_conv_bn(m):
     last_conv = None
     last_conv_name = None
 
-    for name, child in m.named_children():
+    for name, child in module.named_children():
         if isinstance(child,
                       (nn.modules.batchnorm._BatchNorm, nn.SyncBatchNorm)):
             if last_conv is None:  # only fuse BN that is after Conv
                 continue
             fused_conv = fuse_conv_bn_single(last_conv, child)
-            m._modules[last_conv_name] = fused_conv
+            module._modules[last_conv_name] = fused_conv
             # To reduce changes, set BN as Identity instead of deleting it.
-            m._modules[name] = nn.Identity()
+            module._modules[name] = nn.Identity()
             last_conv = None
         elif isinstance(child, nn.Conv2d):
             last_conv = child
             last_conv_name = name
         else:
             fuse_conv_bn(child)
-    return m
+    return module
