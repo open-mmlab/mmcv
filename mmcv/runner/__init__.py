64972d613f72c7d47f71b7ea873e021df80fa1f5
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-17 19:27:36 +0800

    add FP16 optimizer

--- mmcv/runner/__init__.py
+++ mmcv/runner/__init__.py
@@ -4,6 +4,7 @@ from .checkpoint import (_load_checkpoint, load_checkpoint, load_state_dict,
                          save_checkpoint, weights_to_cpu)
 from .dist_utils import get_dist_info, init_dist, master_only
 from .epoch_based_runner import EpochBasedRunner, Runner
+from .fp16_utils import auto_fp16, force_fp32
 from .hooks import (HOOKS, CheckpointHook, ClosureHook, DistSamplerSeedHook,
                     Hook, IterTimerHook, LoggerHook, LrUpdaterHook,
                     MlflowLoggerHook, OptimizerHook, PaviLoggerHook,
@@ -27,5 +28,5 @@ __all__ = [
     'obj_from_dict', 'init_dist', 'get_dist_info', 'master_only',
     'OPTIMIZER_BUILDERS', 'OPTIMIZERS', 'DefaultOptimizerConstructor',
     'build_optimizer', 'build_optimizer_constructor', 'IterLoader',
-    'set_random_seed'
+    'set_random_seed', 'auto_fp16', 'force_fp32'
 ]
