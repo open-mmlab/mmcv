4425a972fdfa5ac9fba1bcc057ce4c09fb877212
hezijian <hezijian@dm-ai.cn>
2020-08-10 14:34:38 +0800

    fix the bug #472

--- mmcv/runner/hooks/sampler_seed.py
+++ mmcv/runner/hooks/sampler_seed.py
@@ -6,4 +6,4 @@ from .hook import HOOKS, Hook
 class DistSamplerSeedHook(Hook):
 
     def before_epoch(self, runner):
-        runner.data_loader.sampler.set_epoch(runner.epoch)
+        runner.data_loader.batch_sampler.sampler.set_epoch(runner.epoch)
