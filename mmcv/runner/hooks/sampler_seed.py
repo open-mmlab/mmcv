fbfb1a90fc39097c195a75b09781a4c0a7e91a2c
hezijian <hezijian@dm-ai.cn>
2020-08-12 20:02:47 +0800

    fix the bug (#472)

--- mmcv/runner/hooks/sampler_seed.py
+++ mmcv/runner/hooks/sampler_seed.py
@@ -6,4 +6,9 @@ from .hook import HOOKS, Hook
 class DistSamplerSeedHook(Hook):
 
     def before_epoch(self, runner):
-        runner.data_loader.batch_sampler.sampler.set_epoch(runner.epoch)
+    	if has_attr(runner.data_loader.sampler, 'set_epoch'):
+            # in case the data loader uses `SequentialSampler` in Pytorch
+            runner.data_loader.sampler.set_epoch(runner.epoch)
+    	if has_attr(runner.data_loader.batch_sampler.sampler, 'set_epoch'):
+            # batch sampler in pytorch warps a sampler as its attributes.
+            runner.data_loader.batch_sampler.sampler.set_epoch(runner.epoch)
