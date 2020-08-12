1d86fd325f65290056d338885cfbc2d04ca7fbe3
wangxinjiang <wangxinjiang@sensetime.com>
2020-08-12 21:58:21 +0800

    Add json set_default for unserializable values

--- mmcv/fileio/handlers/json_handler.py
+++ mmcv/fileio/handlers/json_handler.py
@@ -1,7 +1,21 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 import json
 
+import numpy as np
+
 from .base import BaseFileHandler
+
+
+def set_default(obj):
+    """Set default json values for unserializable values.
+
+    It helps convert ``set``, ``range`` and ``np.ndarray`` data types to list.
+    """
+    if isinstance(obj, (set, range)):
+        return list(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    raise TypeError
 
 
 class JsonHandler(BaseFileHandler):
@@ -10,7 +24,7 @@ class JsonHandler(BaseFileHandler):
         return json.load(file)
 
     def dump_to_fileobj(self, obj, file, **kwargs):
-        json.dump(obj, file, **kwargs)
+        json.dump(obj, file, default=set_default, **kwargs)
 
     def dump_to_str(self, obj, **kwargs):
-        return json.dumps(obj, **kwargs)
+        return json.dumps(obj, default=set_default, **kwargs)
