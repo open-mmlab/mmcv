35b50b8a87bbd43fff03105d40bccdd9f8497145
ZwwWayne <wayne.zw@outlook.com>
2020-08-14 22:03:27 +0800

    Fix mmcv-dataparallel

--- mmcv/parallel/data_parallel.py
+++ mmcv/parallel/data_parallel.py
@@ -8,11 +8,35 @@ from .scatter_gather import scatter_kwargs
 
 class MMDataParallel(DataParallel):
 
+    def forward(self, *inputs, **kwargs):
+        """Override the original forward function.
+
+        The main difference lies in the CPU inference
+        """
+        if not self.device_ids:
+            inputs, kwargs = self.scatter(inputs, kwargs, -1)
+            return self.module(*inputs, **kwargs)
+
+        for t in chain(self.module.parameters(), self.module.buffers()):
+            if t.device != self.src_device_obj:
+                raise RuntimeError(
+                    'module must have its parameters and buffers '
+                    f'on device {self.src_device_obj} (device_ids[0]) but '
+                    f'found one of them on device: {t.device}')
+
+        inputs, kwargs = self.scatter(inputs, kwargs, self.device_ids)
+        if len(self.device_ids) == 1:
+            return self.module(*inputs[0], **kwargs[0])
+        replicas = self.replicate(self.module, self.device_ids[:len(inputs)])
+        outputs = self.parallel_apply(replicas, inputs, kwargs)
+        return self.gather(outputs, self.output_device)
+
     def scatter(self, inputs, kwargs, device_ids):
         return scatter_kwargs(inputs, kwargs, device_ids, dim=self.dim)
 
     def train_step(self, *inputs, **kwargs):
         if not self.device_ids:
+            inputs, kwargs = self.scatter(inputs, kwargs, -1)
             return self.module.train_step(*inputs, **kwargs)
 
         assert len(self.device_ids) == 1, \
@@ -32,6 +56,7 @@ class MMDataParallel(DataParallel):
 
     def val_step(self, *inputs, **kwargs):
         if not self.device_ids:
+            inputs, kwargs = self.scatter(inputs, kwargs, -1)
             return self.module.val_step(*inputs, **kwargs)
 
         assert len(self.device_ids) == 1, \
