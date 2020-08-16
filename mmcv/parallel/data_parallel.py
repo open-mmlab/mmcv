f74858d0a746908818405098dd9904013f3548f8
ZwwWayne <wayne.zw@outlook.com>
2020-08-16 17:52:27 +0800

    Fix (parallel): fix CPU inference with MMDataParallel

--- mmcv/parallel/data_parallel.py
+++ mmcv/parallel/data_parallel.py
@@ -1,12 +1,42 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 from itertools import chain
 
+import torch
+from torch.cuda._utils import _get_device_index
 from torch.nn.parallel import DataParallel
+from torch.nn.parallel.data_parallel import _check_balance
 
 from .scatter_gather import scatter_kwargs
 
 
 class MMDataParallel(DataParallel):
+
+    def __init__(self, module, device_ids=None, output_device=None, dim=0):
+        super(DataParallel, self).__init__()
+
+        if not torch.cuda.is_available():
+            self.module = module
+            self.device_ids = []
+            self.dim = dim
+            return
+
+        if device_ids is None:
+            device_ids = list(range(torch.cuda.device_count()))
+        if output_device is None:
+            output_device = device_ids[0]
+
+        self.dim = dim
+        self.module = module
+        self.device_ids = list(
+            map(lambda x: _get_device_index(x, True), device_ids))
+        self.output_device = _get_device_index(output_device, True)
+        self.src_device_obj = torch.device('cuda:{}'.format(
+            self.device_ids[0]))
+
+        _check_balance(self.device_ids)
+
+        if len(self.device_ids) == 1:
+            self.module.cuda(device_ids[0])
 
     def forward(self, *inputs, **kwargs):
         """Override the original forward function.
@@ -14,8 +44,8 @@ class MMDataParallel(DataParallel):
         The main difference lies in the CPU inference
         """
         if not self.device_ids:
-            inputs, kwargs = self.scatter(inputs, kwargs, -1)
-            return self.module(*inputs, **kwargs)
+            inputs, kwargs = self.scatter(inputs, kwargs, [-1])
+            return self.module(*inputs[0], **kwargs[0])
 
         for t in chain(self.module.parameters(), self.module.buffers()):
             if t.device != self.src_device_obj:
