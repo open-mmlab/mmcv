3b46d33e90c397869ad5103075838fdfc9812aa0
Kai Chen <chenkaidev@gmail.com>
2020-08-24 14:01:27 +0800

    bump version to 1.1.1

--- mmcv/version.py
+++ mmcv/version.py
@@ -1,2 +1,19 @@
 # Copyright (c) Open-MMLab. All rights reserved.
-__version__ = '1.1.0'
+__version__ = '1.1.1'
+
+
+def parse_version_info(version_str):
+    version_info = []
+    for x in version_str.split('.'):
+        if x.isdigit():
+            version_info.append(int(x))
+        elif x.find('rc') != -1:
+            patch_version = x.split('rc')
+            version_info.append(int(patch_version[0]))
+            version_info.append(f'rc{patch_version[1]}')
+    return tuple(version_info)
+
+
+version_info = parse_version_info(__version__)
+
+__all__ = ['__version__', 'version_info']
