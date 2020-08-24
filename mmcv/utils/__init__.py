270e470ee17c0f0f3a28eda3ccb90abae4c385f9
Kai Chen <chenkaidev@gmail.com>
2020-08-24 13:41:54 +0800

    [feature] Add collect_env() to collect environment info (#517)
    * [feature] Add collect_env() to collect environment info
    
    * fix unit tests
    
    * fix the case when ops are not compiled
    
    * fix docstring

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -26,19 +26,18 @@ except ImportError:
         'get_git_hash'
     ]
 else:
-    from .env import TORCH_VERSION
+    from .env import collect_env
     from .logging import get_logger, print_log
-    from .parrots_wrapper import (CUDA_HOME, BuildExtension, CppExtension,
-                                  CUDAExtension, DataLoader, PoolDataLoader,
-                                  SyncBatchNorm, _AdaptiveAvgPoolNd,
-                                  _AdaptiveMaxPoolNd, _AvgPoolNd, _BatchNorm,
-                                  _ConvNd, _ConvTransposeMixin, _InstanceNorm,
-                                  _MaxPoolNd, get_build_config)
+    from .parrots_wrapper import (
+        CUDA_HOME, TORCH_VERSION, BuildExtension, CppExtension, CUDAExtension,
+        DataLoader, PoolDataLoader, SyncBatchNorm, _AdaptiveAvgPoolNd,
+        _AdaptiveMaxPoolNd, _AvgPoolNd, _BatchNorm, _ConvNd,
+        _ConvTransposeMixin, _InstanceNorm, _MaxPoolNd, get_build_config)
     from .registry import Registry, build_from_cfg
     __all__ = [
-        'Config', 'ConfigDict', 'DictAction', 'get_logger', 'print_log',
-        'is_str', 'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of',
-        'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
+        'Config', 'ConfigDict', 'DictAction', 'collect_env', 'get_logger',
+        'print_log', 'is_str', 'iter_cast', 'list_cast', 'tuple_cast',
+        'is_seq_of', 'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
         'check_prerequisites', 'requires_package', 'requires_executable',
         'is_filepath', 'fopen', 'check_file_exist', 'mkdir_or_exist',
         'symlink', 'scandir', 'ProgressBar', 'track_progress',
