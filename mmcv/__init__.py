5583b5f94828a52def08066be319a9dcc55f640a
Jiarui XU <xvjiarui0826@gmail.com>
2020-08-31 22:25:31 +0800

    fixed wrong commit

--- mmcv/__init__.py
+++ mmcv/__init__.py
@@ -4,10 +4,9 @@ from .arraymisc import *
 from .fileio import *
 from .image import *
 from .utils import *
-from .version import __version__
-
-# from .video import *
-# from .visualization import *
+from .version import *
+from .video import *
+from .visualization import *
 
 # The following modules are not imported to this level, so mmcv may be used
 # without PyTorch.
