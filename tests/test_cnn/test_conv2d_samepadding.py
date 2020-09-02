adab95f231f518d46daceadc04dc239e015dc076
lixiaojie <lixiaojie@sensetime.com>
2020-09-02 13:25:01 +0800

    add unittest

--- tests/test_cnn/test_conv2d_samepadding.py
+++ tests/test_cnn/test_conv2d_samepadding.py
@@ -10,8 +10,18 @@ def test_conv2d_samepadding():
     output = conv(inputs)
     assert output.shape == inputs.shape
 
+    inputs = torch.rand((1, 3, 13, 13))
+    conv = Conv2dSamePadding(3, 3, kernel_size=3, stride=1)
+    output = conv(inputs)
+    assert output.shape == inputs.shape
+
     # test Conv2dSamePadding with stride=2
     inputs = torch.rand((1, 3, 28, 28))
     conv = Conv2dSamePadding(3, 3, kernel_size=3, stride=2)
     output = conv(inputs)
     assert output.shape == torch.Size([1, 3, 14, 14])
+
+    inputs = torch.rand((1, 3, 13, 13))
+    conv = Conv2dSamePadding(3, 3, kernel_size=3, stride=2)
+    output = conv(inputs)
+    assert output.shape == torch.Size([1, 3, 7, 7])
