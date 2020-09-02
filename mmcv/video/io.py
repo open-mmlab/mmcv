96bd053705c180e0317217fd80161cd5685bb3d0
linjintao <linjintao@sensetime.com>
2020-09-03 00:37:02 +0800

    add url support for VideoReader

--- mmcv/video/io.py
+++ mmcv/video/io.py
@@ -62,7 +62,9 @@ class VideoReader:
     """
 
     def __init__(self, filename, cache_capacity=10):
-        check_file_exist(filename, 'Video file not found: ' + filename)
+        if not (filename.startswith('https://')
+                or filename.startswith('http://')):
+            check_file_exist(filename, 'Video file not found: ' + filename)
         self._vcap = cv2.VideoCapture(filename)
         assert cache_capacity > 0
         self._cache = Cache(cache_capacity)
