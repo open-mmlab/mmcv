488a82c9a713be1f90e4cf3faefb52a9001ea97b
v-qjqs <qjliqiaofei@gmail.com>
2020-09-14 11:03:53 +0800

    rename alpha as factor

--- mmcv/image/photometric.py
+++ mmcv/image/photometric.py
@@ -168,19 +168,19 @@ def imequalize(img):
     return equalized_img
 
 
-def adjust_brightness(img, alpha=1.):
+def adjust_brightness(img, factor=1.):
     """Adjust image brightness.
 
     This function controls the brightness of an image. An
-    enhancement factor (alpha) of 0.0 gives a black image.
+    enhancement factor of 0.0 gives a black image.
     A factor of 1.0 gives the original image. This function
     blends the source image and the degenerated black image:
 
-    ``output = img * alpha + degenerated * (1 - alpha)``
+    ``output = img * factor + degenerated * (1 - factor)``
 
     Args:
         img (ndarray): Image to be brightened.
-        alpha (float): A value controls the enhancement.
+        factor (float): A value controls the enhancement.
             Factor 1.0 returns the original image, lower
             factors mean less color (brightness, contrast,
             etc), and higher values more. Default 1.
@@ -191,26 +191,26 @@ def adjust_brightness(img, alpha=1.):
     degenerated = np.zeros_like(img)
     # Note manually convert the dtype to np.float32, to
     # achieve as close results as PIL.ImageEnhance.Brightness.
-    # Set beta=1-alpha, and gamma=0
+    # Set beta=1-factor, and gamma=0
     brightened_img = cv2.addWeighted(
-        img.astype(np.float32), alpha, degenerated.astype(np.float32),
-        1 - alpha, 0)
+        img.astype(np.float32), factor, degenerated.astype(np.float32),
+        1 - factor, 0)
     return brightened_img.astype(img.dtype)
 
 
-def adjust_contrast(img, alpha=1.):
+def adjust_contrast(img, factor=1.):
     """Adjust image contrast.
 
     This function controls the contrast of an image. An
-    enhancement factor (alpha) of 0.0 gives a solid grey
+    enhancement factor of 0.0 gives a solid grey
     image. A factor of 1.0 gives the original image. It
     blends the source image and the degenerated mean image:
 
-    ``output = img * alpha + degenerated * (1 - alpha)``
+    ``output = img * factor + degenerated * (1 - factor)``
 
     Args:
         img (ndarray): Image to be contrasted. BGR order.
-        alpha (float): Same as :func:`mmcv.adjust_brightness`.
+        factor (float): Same as :func:`mmcv.adjust_brightness`.
 
     Returns:
         ndarray: The contrasted image.
@@ -221,6 +221,6 @@ def adjust_contrast(img, alpha=1.):
     degenerated = (np.ones_like(img[..., 0]) * mean).astype(img.dtype)
     degenerated = gray2bgr(degenerated)
     contrasted_img = cv2.addWeighted(
-        img.astype(np.float32), alpha, degenerated.astype(np.float32),
-        1 - alpha, 0)
+        img.astype(np.float32), factor, degenerated.astype(np.float32),
+        1 - factor, 0)
     return contrasted_img.astype(img.dtype)
