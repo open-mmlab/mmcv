e90e89c505b67a85f058d03aec08d1c9fe8326aa
RyanXLi <xlide@connect.ust.hk>
2020-09-15 12:55:33 +0800

    add 'wrap_fp16_model' to __inti__.py

--- mmcv/runner/__init__.py
+++ mmcv/runner/__init__.py
@@ -4,7 +4,7 @@ from .checkpoint import (_load_checkpoint, load_checkpoint, load_state_dict,
                          save_checkpoint, weights_to_cpu)
 from .dist_utils import get_dist_info, init_dist, master_only
 from .epoch_based_runner import EpochBasedRunner, Runner
-from .fp16_utils import auto_fp16, force_fp32
+from .fp16_utils import auto_fp16, force_fp32, wrap_fp16_model
 from .hooks import (HOOKS, CheckpointHook, ClosureHook, DistSamplerSeedHook,
                     EMAHook, Fp16OptimizerHook, Hook, IterTimerHook,
                     LoggerHook, LrUpdaterHook, MlflowLoggerHook, OptimizerHook,
@@ -24,11 +24,12 @@ __all__ = [
     'OptimizerHook', 'IterTimerHook', 'DistSamplerSeedHook', 'LoggerHook',
     'PaviLoggerHook', 'TextLoggerHook', 'TensorboardLoggerHook',
     'WandbLoggerHook', 'MlflowLoggerHook', '_load_checkpoint',
-    'load_state_dict', 'load_checkpoint', 'weights_to_cpu', 'save_checkpoint',
-    'Priority', 'get_priority', 'get_host_info', 'get_time_str',
-    'obj_from_dict', 'init_dist', 'get_dist_info', 'master_only',
-    'OPTIMIZER_BUILDERS', 'OPTIMIZERS', 'DefaultOptimizerConstructor',
-    'build_optimizer', 'build_optimizer_constructor', 'IterLoader',
-    'set_random_seed', 'auto_fp16', 'force_fp32', 'Fp16OptimizerHook',
-    'SyncBuffersHook', 'EMAHook'
+    'load_state_dict', 'load_checkpoint', 'weights_to_cpu', 'wrap_fp16_model',
+    'save_checkpoint', 'Priority', 'get_priority', 'get_host_info',
+    'get_time_str', 'obj_from_dict', 'init_dist', 'get_dist_info',
+    'master_only', 'OPTIMIZER_BUILDERS', 'OPTIMIZERS',
+    'DefaultOptimizerConstructor', 'build_optimizer',
+    'build_optimizer_constructor', 'IterLoader', 'set_random_seed',
+    'auto_fp16', 'force_fp32', 'Fp16OptimizerHook', 'SyncBuffersHook',
+    'EMAHook'
 ]
