782ce32839d7f85fd4fb942e982c742499014f47
yamengxi <854341266@qq.com>
2020-11-19 19:03:24 +0800

    enhance hsigmoid

--- mmcv/cnn/bricks/hsigmoid.py
+++ mmcv/cnn/bricks/hsigmoid.py
@@ -6,16 +6,28 @@ from .registry import ACTIVATION_LAYERS
 @ACTIVATION_LAYERS.register_module()
 class HSigmoid(nn.Module):
     """Hard Sigmoid Module. Apply the hard sigmoid function:
-    Hsigmoid(x) = min(max((x + 1) / 2, 0), 1)
+    Hsigmoid(x) = min(max((x + bias) / divisor, min_value), max_value)
+    Hsigmoid(x) = min(max((x + 1) / 2, 0), 1) by default
+
+    Args:
+        bias (int | float): Bias of the input feature map. Default: 1.
+        divisor (int | float): Divisor of the input feature map. Default: 2.
+        min_value (int | float): Lower bound value. Default: 0.
+        max_value (int | float): Upper bound value. Default: 1.
 
     Returns:
         Tensor: The output tensor.
     """
 
-    def __init__(self):
+    def __init__(self, bias=1, divisor=2, min_value=0, max_value=1):
         super(HSigmoid, self).__init__()
+        self.bias = bias
+        self.divisor = divisor
+        assert self.divisor != 0
+        self.min_value = min_value
+        self.max_value = max_value
 
     def forward(self, x):
-        x = (x + 1) / 2
+        x = (x + self.bias) / self.divisor
 
-        return x.clamp_(0, 1)
+        return x.clamp_(self.min_value, self.max_value)
