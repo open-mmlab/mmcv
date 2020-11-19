df64a4e241642adda40892b87f2b77c1bd23e6e5
yamengxi <854341266@qq.com>
2020-11-19 20:36:12 +0800

    delete int

--- mmcv/cnn/bricks/hsigmoid.py
+++ mmcv/cnn/bricks/hsigmoid.py
@@ -7,19 +7,19 @@ from .registry import ACTIVATION_LAYERS
 class HSigmoid(nn.Module):
     """Hard Sigmoid Module. Apply the hard sigmoid function:
     Hsigmoid(x) = min(max((x + bias) / divisor, min_value), max_value)
-    Hsigmoid(x) = min(max((x + 1) / 2, 0), 1) by default
+    Default: Hsigmoid(x) = min(max((x + 1) / 2, 0), 1)
 
     Args:
-        bias (int | float): Bias of the input feature map. Default: 1.
-        divisor (int | float): Divisor of the input feature map. Default: 2.
-        min_value (int | float): Lower bound value. Default: 0.
-        max_value (int | float): Upper bound value. Default: 1.
+        bias (float): Bias of the input feature map. Default: 1.0.
+        divisor (float): Divisor of the input feature map. Default: 2.0.
+        min_value (float): Lower bound value. Default: 0.0.
+        max_value (float): Upper bound value. Default: 1.0.
 
     Returns:
         Tensor: The output tensor.
     """
 
-    def __init__(self, bias=1, divisor=2, min_value=0, max_value=1):
+    def __init__(self, bias=1.0, divisor=2.0, min_value=0.0, max_value=1.0):
         super(HSigmoid, self).__init__()
         self.bias = bias
         self.divisor = divisor
