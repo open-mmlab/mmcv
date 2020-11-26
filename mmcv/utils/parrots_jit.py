5ca4f3768e31bb35f8ab7f4faab0d1a7ec389c49
limaolin <limaolin@sensetime.com>
2020-11-26 21:23:04 +0800

    skip test_parrots_jit.py when build without pytorch

--- mmcv/utils/parrots_jit.py
+++ mmcv/utils/parrots_jit.py
@@ -2,17 +2,25 @@ import torch
 
 TORCH_VERSION = torch.__version__
 
-
 if TORCH_VERSION == 'parrots':
     from parrots.jit import pat
     jit = pat
 else:
-    def jit(func=None, check_input=None, full_shape=True,
-            derivate=False, coderize=False, optimize=False):
+
+    def jit(func=None,
+            check_input=None,
+            full_shape=True,
+            derivate=False,
+            coderize=False,
+            optimize=False):
+
         def wrapper(func):
+
             def wrapper_inner(*args, **kargs):
                 return func(*args, **kargs)
+
             return wrapper_inner
+
         if func is None:
             return wrapper
         else:
@@ -22,9 +30,12 @@ else:
 if TORCH_VERSION == 'parrots':
     from parrots.utils.tester import skip_no_cuda, skip_no_elena
 else:
+
     def bypass_decorator(func):
+
         def wrapper(*args, **kargs):
             return func(*args, **kargs)
+
         return wrapper
 
     skip_no_cuda = bypass_decorator
