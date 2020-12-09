e1c7b16e255bba40582d824c470ce783f6a98827
MengLei Jiao <jiaoml1996@163.com>
2020-12-09 11:25:24 +0800

    fix bug

--- mmcv/ops/csrc/box_iou_rotated_cuda.cuh
+++ mmcv/ops/csrc/box_iou_rotated_cuda.cuh
@@ -23,7 +23,6 @@ __global__ void box_iou_rotated_cuda_kernel(const int n_boxes1,
                                             const T* dev_boxes1,
                                             const T* dev_boxes2, T* dev_ious,
                                             const bool aligned) {
-
   if (aligned) {
     CUDA_1D_KERNEL_LOOP(index, n_boxes1) {
       int b1 = index;
