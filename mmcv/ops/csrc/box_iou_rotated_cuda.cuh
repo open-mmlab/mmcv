6ba4382a57f73b69a1e311ede92076dfe94f6ef5
MengLei Jiao <jiaoml1996@163.com>
2020-12-09 10:44:49 +0800

    fix lint

--- mmcv/ops/csrc/box_iou_rotated_cuda.cuh
+++ mmcv/ops/csrc/box_iou_rotated_cuda.cuh
@@ -24,8 +24,8 @@ __global__ void box_iou_rotated_cuda_kernel(const int n_boxes1,
                                             const T* dev_boxes2, T* dev_ious,
                                             const bool aligned) {
 
-  if (aligned){
-    CUDA_1D_KERNEL_LOOP(index, n_boxes1){
+  if (aligned) {
+    CUDA_1D_KERNEL_LOOP(index, n_boxes1) {
       int b1 = index;
       int b2 = index;
 
@@ -51,7 +51,7 @@ __global__ void box_iou_rotated_cuda_kernel(const int n_boxes1,
       dev_ious[index] = single_box_iou_rotated<T>(block_boxes1, block_boxes2);
     }
   } else {
-    CUDA_1D_KERNEL_LOOP(index, n_boxes1 * n_boxes2){
+    CUDA_1D_KERNEL_LOOP(index, n_boxes1 * n_boxes2) {
       int b1 = index / n_boxes2;
       int b2 = index % n_boxes2;
 
--- mmcv/ops/csrc/pytorch/box_iou_rotated_cuda.cu
+++ mmcv/ops/csrc/pytorch/box_iou_rotated_cuda.cu
@@ -18,7 +18,8 @@ void box_iou_rotated_cuda(const Tensor boxes1, const Tensor boxes2, Tensor ious,
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   box_iou_rotated_cuda_kernel<scalar_t>
       <<<GET_BLOCKS(output_size), THREADS_PER_BLOCK, 0, stream>>>(
-        num_boxes1, num_boxes2, boxes1.data_ptr<scalar_t>(),
-        boxes2.data_ptr<scalar_t>(), (scalar_t*)ious.data_ptr<scalar_t>(), aligned);
+          num_boxes1, num_boxes2, boxes1.data_ptr<scalar_t>(),
+          boxes2.data_ptr<scalar_t>(), (scalar_t*)ious.data_ptr<scalar_t>(),
+          aligned);
   AT_CUDA_CHECK(cudaGetLastError());
 }
