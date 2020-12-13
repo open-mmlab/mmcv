bf0a0634fa56f9f511e94822f26d12fceba61ef7
nbei <nbeiray@gmail.com>
2020-12-13 14:24:53 +0800

    fix value bug

--- tests/test_video/test_optflow.py
+++ tests/test_video/test_optflow.py
@@ -157,7 +157,7 @@ def test_flow_warp():
     flow[2, 2, :] = [0.5, 0.7]
 
     res_ = np.copy(img)
-    res_[2, 2] = 0.5 * 0.3 + 0.75 * 0.5 * 0.7
+    res_[2, 2] = 0.5 * 0.3 + 0.75 * 0.5 * 0.3
     res_bi = mmcv.flow_warp(img, flow, interpolate_mode='bilinear')
     assert_array_almost_equal(res_, res_bi, decimal=5)
 
