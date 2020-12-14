47ab059e570754d2645732f8fa236b017177ce01
zhoushilin <zhoushilin@sensetime.com>
2020-12-14 17:23:06 +0800

    fix lint error

--- setup.py
+++ setup.py
@@ -151,12 +151,12 @@ def get_extensions():
         if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
             define_macros += [('MMCV_WITH_CUDA', None)]
         ext_ops = Extension(
-            name = ext_name,
-            sources = op_files,
-            include_dirs = [include_path],
-            define_macros = define_macros,
-            extra_compile_args = extra_compile_args,
-            cuda = True)
+            name=ext_name,
+            sources=op_files,
+            include_dirs=[include_path],
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args,
+            cuda=True)
         extensions.append(ext_ops)
 
         extra_compile_args2={
@@ -177,13 +177,13 @@ def get_extensions():
         include_path = os.path.abspath('./mmcv/ops/csrc')
         cuda_args = os.getenv('MMCV_CUDA_ARGS')
         ext_ops = Extension(
-            name = ext_name,
-            sources = op_files,
-            include_dirs = [include_path],
-            define_macros = define_macros,
-            extra_compile_args = extra_compile_args2,
-            cuda = True,
-            pytorch = True)
+            name=ext_name,
+            sources=op_files,
+            include_dirs=[include_path],
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args2,
+            cuda=True,
+            pytorch=True)
         extensions.append(ext_ops)
     elif EXT_TYPE == 'pytorch':
         ext_name = 'mmcv._ext'
