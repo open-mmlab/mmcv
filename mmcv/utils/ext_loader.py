ad84fb16b686442fdbf624ba04bbf74de8097fd9
zhoushilin <zhoushilin@sensetime.com>
2020-12-14 19:09:58 +0800

    fix yapf bug

--- mmcv/utils/ext_loader.py
+++ mmcv/utils/ext_loader.py
@@ -1,4 +1,3 @@
-import importlib
 import os
 import pkgutil
 from collections import namedtuple
@@ -6,10 +5,10 @@ from collections import namedtuple
 import torch
 
 use_op = [
-    'nms', 'softnms', 'nms_match', 'top_pool_forward',
-    'top_pool_backward', 'bottom_pool_forward', 'bottom_pool_backward',
-    'left_pool_forward', 'left_pool_backward', 'right_pool_forward',
-    'right_pool_backward']
+    'nms', 'softnms', 'nms_match', 'top_pool_forward', 'top_pool_backward',
+    'bottom_pool_forward', 'bottom_pool_backward', 'left_pool_forward',
+    'left_pool_backward', 'right_pool_forward', 'right_pool_backward'
+]
 
 if torch.__version__ != 'parrots':
 
