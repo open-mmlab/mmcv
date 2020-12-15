3b4255fe57d9e20b3f3068fdb230252557b16858
z55250825 <z55250825@163.com>
2020-12-15 21:55:38 +0800

    fix c/cuda code style

--- mmcv/ops/csrc/parrots_pt/gather_points_pytorch.h
+++ mmcv/ops/csrc/parrots_pt/gather_points_pytorch.h
@@ -4,11 +4,9 @@
 #include <torch/extension.h>
 using namespace at;
 
-int gather_points(int b, int c, int n, int npoints,
-                  Tensor points, Tensor idx,
+int gather_points(int b, int c, int n, int npoints, Tensor points, Tensor idx,
                   Tensor out);
 
-int gather_points_backward(int b, int c, int n, int npoints, 
-                           Tensor grad_out, Tensor idx,
-                           Tensor grad_points);
-#endif // _GATHER_POINT_EXT_PYTORCH
+int gather_points_backward(int b, int c, int n, int npoints, Tensor grad_out,
+                           Tensor idx, Tensor grad_points);
+#endif  // _GATHER_POINT_EXT_PYTORCH
