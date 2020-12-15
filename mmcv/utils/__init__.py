9befc39811c4867cf57685f2d4fc3929cd2becc2
lml131 <43259216+lml131@users.noreply.github.com>
2020-12-15 18:20:37 +0800

    Lml/jit decorator (#673)
    * add jit decorator
    
    * add parrots_jit.py
    
    * modify test_parrots_jit.py
    
    * modify for lint
    
    * fix isort
    
    * skip test_parrots_jit.py when build without pytorch
    
    * try ci
    
    * rm log
    
    * fix double quote
    
    * modify for comments and use partial_shape instead of full_shape
    
    * fix for lint
    
    * small modify for parrots 0.9.0rc0
    
    * def skip no elena directly

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -33,6 +33,7 @@ else:
         DataLoader, PoolDataLoader, SyncBatchNorm, _AdaptiveAvgPoolNd,
         _AdaptiveMaxPoolNd, _AvgPoolNd, _BatchNorm, _ConvNd,
         _ConvTransposeMixin, _InstanceNorm, _MaxPoolNd, get_build_config)
+    from .parrots_jit import jit, skip_no_elena, skip_no_parrots
     from .registry import Registry, build_from_cfg
     __all__ = [
         'Config', 'ConfigDict', 'DictAction', 'collect_env', 'get_logger',
@@ -48,5 +49,6 @@ else:
         '_InstanceNorm', '_MaxPoolNd', 'get_build_config', 'BuildExtension',
         'CppExtension', 'CUDAExtension', 'DataLoader', 'PoolDataLoader',
         'TORCH_VERSION', 'deprecated_api_warning', 'digit_version',
-        'get_git_hash', 'import_modules_from_strings'
+        'get_git_hash', 'import_modules_from_strings', 'jit', 'skip_no_elena',
+        'skip_no_parrots'
     ]
