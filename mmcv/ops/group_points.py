5265b1caf02d7c849da031944c9d50c89b42f417
z55250825 <z55250825@163.com>
2020-12-15 22:10:42 +0800

    fix python code style

--- mmcv/ops/group_points.py
+++ mmcv/ops/group_points.py
@@ -42,12 +42,17 @@ class GroupingOperation(Function):
 
         if torch.__version__ == 'parrots':
             indata_list = [features, indices, output]
-            indata_dict = {'b' : B, 'c' : C, 'n' : N, 'npoints' : nfeatures, 
-                           'nsample' : nsample}
+            indata_dict = {
+                'b': B,
+                'c': C,
+                'n': N,
+                'npoints': nfeatures,
+                'nsample': nsample
+            }
             ext_module.group_points(*indata_list, **indata_dict)
         else:
-            ext_module.group_points(B, C, N, nfeatures, nsample, features, 
-                                indices, output)
+            ext_module.group_points(B, C, N, nfeatures, nsample, features,
+                                    indices, output)
 
         ctx.for_backwards = (indices, N)
         return output
@@ -72,8 +77,13 @@ class GroupingOperation(Function):
         grad_out_data = grad_out.data.contiguous()
         if torch.__version__ == 'parrots':
             indata_list = [grad_out_data, idx, grad_features.data]
-            indata_dict = {'b' : B, 'c' : C, 'n' : N,
-                           'npoints' : npoint, 'nsample' : nsample}
+            indata_dict = {
+                'b': B,
+                'c': C,
+                'n': N,
+                'npoints': npoint,
+                'nsample': nsample
+            }
             ext_module.group_points_backward(*indata_list, **indata_dict)
         else:
             ext_module.group_points_backward(B, C, N, npoint, nsample,
