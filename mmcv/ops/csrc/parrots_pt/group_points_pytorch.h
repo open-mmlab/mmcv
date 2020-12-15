586c53dbdd0f9e2851d454a6314e63c3d1673d84
z55250825 <z55250825@163.com>
2020-12-15 21:56:43 +0800

    fix c/cuda code style

--- mmcv/ops/csrc/parrots_pt/group_points_pytorch.h
+++ mmcv/ops/csrc/parrots_pt/group_points_pytorch.h
@@ -10,4 +10,4 @@ int group_points(int b, int c, int n, int npoints, int nsample, Tensor points,
 int group_points_backward(int b, int c, int n, int npoints, int nsample,
                           Tensor grad_out, Tensor idx, Tensor grad_points);
 
-#endif //_GROUP_POINTS_EXT_PYTORCH
+#endif  //_GROUP_POINTS_EXT_PYTORCH
