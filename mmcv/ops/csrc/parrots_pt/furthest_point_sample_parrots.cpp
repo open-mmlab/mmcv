c7c2717a8de50bdde6dc03105ba0666fee1aea21
z55250825 <z55250825@163.com>
2020-12-15 22:22:38 +0800

    fix c/cuda code style

--- mmcv/ops/csrc/parrots_pt/furthest_point_sample_parrots.cpp
+++ mmcv/ops/csrc/parrots_pt/furthest_point_sample_parrots.cpp
@@ -1,20 +1,17 @@
+#include <parrots/compute/aten.hpp>
 #include <parrots/extension.hpp>
 #include <parrots/foundation/ssattrs.hpp>
-#include <parrots/compute/aten.hpp>
+
 #include "furthest_point_sample_pytorch.h"
 
 using namespace parrots;
 
-template<typename T>
+template <typename T>
 void furthest_point_sampling_parrots(T& ctx, const SSElement& attr,
-                        const OperatorBase::in_list_t& ins,
-                        OperatorBase::out_list_t& outs){
+                                     const OperatorBase::in_list_t& ins,
+                                     OperatorBase::out_list_t& outs) {
   int b, n, m;
-  SSAttrs(attr)
-     .get("b", b)
-     .get("n", n)
-     .get("m", m)
-     .done();
+  SSAttrs(attr).get("b", b).get("n", n).get("m", m).done();
 
   at::Tensor points_tensor, temp_tensor, idx_tensor;
   points_tensor = buildATensor(ctx, ins[0]);
@@ -24,23 +21,20 @@ void furthest_point_sampling_parrots(T& ctx, const SSElement& attr,
   furthest_point_sampling(b, n, m, points_tensor, temp_tensor, idx_tensor);
 }
 
-template<typename T>
-void furthest_point_sampling_with_dist_parrots(T& ctx, const SSElement& attr,
-                        const OperatorBase::in_list_t& ins,
-                        OperatorBase::out_list_t& outs){
+template <typename T>
+void furthest_point_sampling_with_dist_parrots(
+    T& ctx, const SSElement& attr, const OperatorBase::in_list_t& ins,
+    OperatorBase::out_list_t& outs) {
   int b, n, m;
-  SSAttrs(attr)
-     .get("b", b)
-     .get("n", n)
-     .get("m", m)
-     .done();
+  SSAttrs(attr).get("b", b).get("n", n).get("m", m).done();
 
   at::Tensor points_tensor, temp_tensor, idx_tensor;
   points_tensor = buildATensor(ctx, ins[0]);
   temp_tensor = buildATensor(ctx, outs[0]);
   idx_tensor = buildATensor(ctx, outs[1]);
 
-  furthest_point_sampling_with_dist(b, n, m, points_tensor, temp_tensor, idx_tensor);
+  furthest_point_sampling_with_dist(b, n, m, points_tensor, temp_tensor,
+                                    idx_tensor);
 }
 
 PARROTS_EXTENSION_REGISTER(furthest_point_sampling)
--- mmcv/ops/csrc/parrots_pt/furthest_point_sample_pytorch.h
+++ mmcv/ops/csrc/parrots_pt/furthest_point_sample_pytorch.h
@@ -3,13 +3,9 @@
 #include <torch/extension.h>
 using namespace at;
 
-int furthest_point_sampling(int b, int n, int m,
-                            const Tensor points,
-                            Tensor temp,
-                            Tensor idx);
+int furthest_point_sampling(int b, int n, int m, const Tensor points,
+                            Tensor temp, Tensor idx);
 
-int furthest_point_sampling_with_dist(int b, int n, int m,
-                                      const Tensor points,
-                                      Tensor temp,
-                                      Tensor idx);
-#endif //_FURTHEST_POINT_SAMPLE_EXT_PYTORCH
+int furthest_point_sampling_with_dist(int b, int n, int m, const Tensor points,
+                                      Tensor temp, Tensor idx);
+#endif  //_FURTHEST_POINT_SAMPLE_EXT_PYTORCH
