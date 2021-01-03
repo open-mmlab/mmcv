f60ed599ef3390067df5b67d7ddfbbb64efb1e21
nbei <nbeiray@gmail.com>
2021-01-03 10:09:03 +0800

    fix ddp bug when pytorch>=1.7

--- mmcv/parallel/distributed.py
+++ mmcv/parallel/distributed.py
@@ -3,7 +3,7 @@ import torch
 from torch.nn.parallel.distributed import (DistributedDataParallel,
                                            _find_tensors)
 
-from mmcv.utils import TORCH_VERSION
+from mmcv.utils import TORCH_VERSION, get_logger
 from .scatter_gather import scatter_kwargs
 
 
@@ -28,6 +28,14 @@ class MMDistributedDataParallel(DistributedDataParallel):
         ``self.module.forward()`` with ``self.module.train_step()``.
         It is compatible with PyTorch 1.1 - 1.5.
         """
+
+        # In PyTorch >= 1.7, they move the _rebuild_buckets from the end of
+        # backward to the beginning of forward.
+        if torch.__version__ >= '1.7' and self.reducer._rebuild_buckets():
+            _logger = get_logger('mmcv')
+            _logger.info(
+                'Reducer buckets have been rebuilt in this iteration.')
+
         if getattr(self, 'require_forward_param_sync', True):
             self._sync_params()
         if self.device_ids:
