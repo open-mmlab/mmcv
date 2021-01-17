608016be901fb8acd9f0cad09cb5c9706d7259da
zhoushilin <zhoushilin@sensetime.com>
2021-01-17 19:19:50 +0800

    add nms and nms_rotated op, test success

--- mmcv/ops/nms.py
+++ mmcv/ops/nms.py
@@ -119,28 +119,12 @@ def nms(boxes, scores, iou_threshold, offset=0):
     assert offset in (0, 1)
 
     if torch.__version__ == 'parrots':
-        x1 = boxes[:, 0]
-        y1 = boxes[:, 1]
-        x2 = boxes[:, 2]
-        y2 = boxes[:, 3]
-        areas = (x2 - x1 + offset) * (y2 - y1 + offset)
-        _, order = scores.sort(0, descending=True)
-        if boxes.device == 'cpu':
-            indata_list = [boxes, order, areas]
-            indata_dict = {
-                'iou_threshold': float(iou_threshold),
-                'offset': int(offset)
-            }
-            select = ext_module.nms(*indata_list, **indata_dict).byte()
-        else:
-            boxes_sorted = boxes.index_select(0, order)
-            indata_list = [boxes_sorted, order, areas]
-            indata_dict = {
-                'iou_threshold': float(iou_threshold),
-                'offset': int(offset)
-            }
-            select = ext_module.nms(*indata_list, **indata_dict)
-        inds = order.masked_select(select)
+        indata_list = [boxes, scores]
+        indata_dict = {
+            'iou_threshold': float(iou_threshold),
+            'offset': int(offset)
+        }
+        inds = ext_module.nms(*indata_list, **indata_dict)
     else:
         if torch.onnx.is_in_onnx_export() and offset == 0:
             # ONNX only support offset == 1
@@ -210,12 +194,8 @@ def soft_nms(boxes,
     assert method in method_dict.keys()
 
     if torch.__version__ == 'parrots':
-        x1 = boxes[:, 0]
-        y1 = boxes[:, 1]
-        x2 = boxes[:, 2]
-        y2 = boxes[:, 3]
-        areas = (x2 - x1 + offset) * (y2 - y1 + offset)
-        indata_list = [boxes.cpu(), scores.cpu(), areas.cpu()]
+        dets = boxes.new_empty((boxes.size(0), 5), device='cpu')
+        indata_list = [boxes.cpu(), scores.cpu(), dets.cpu()]
         indata_dict = {
             'iou_threshold': float(iou_threshold),
             'sigma': float(sigma),
@@ -223,8 +203,7 @@ def soft_nms(boxes,
             'method': method_dict[method],
             'offset': int(offset)
         }
-        dets, inds, num_out = ext_module.softnms(*indata_list, **indata_dict)
-        inds = inds[:num_out]
+        inds = ext_module.softnms(*indata_list, **indata_dict)
     else:
         dets, inds = SoftNMSop.apply(boxes.cpu(), scores.cpu(),
                                      float(iou_threshold), float(sigma),
@@ -329,7 +308,11 @@ def nms_match(dets, iou_threshold):
             dets_t = dets.detach().cpu()
         else:
             dets_t = torch.from_numpy(dets)
-        matched = ext_module.nms_match(dets_t, float(iou_threshold))
+        indata_list = [dets_t]
+        indata_dict = {'iou_threshold': float(iou_threshold)}
+        matched = ext_module.nms_match(*indata_list, **indata_dict)
+        if torch.__version__ == 'parrots':
+            matched = matched.tolist()
 
     if isinstance(dets, torch.Tensor):
         return [dets.new_tensor(m, dtype=torch.long) for m in matched]
@@ -366,16 +349,9 @@ def nms_rotated(dets, scores, iou_threshold, labels=None):
     dets_sorted = dets_wl.index_select(0, order)
 
     if torch.__version__ == 'parrots':
-        select = torch.zeros((dets.shape[0]),
-                             dtype=torch.int64).to(dets.device)
-        ext_module.nms_rotated(
-            dets_wl,
-            scores,
-            dets_sorted,
-            select,
-            iou_threshold=iou_threshold,
+        keep_inds = ext_module.nms_rotated(dets_wl, scores, order,
+            dets_sorted, iou_threshold=iou_threshold,
             multi_label=multi_label)
-        keep_inds = order.masked_select(select == 1)
     else:
         keep_inds = ext_module.nms_rotated(dets_wl, scores, order, dets_sorted,
                                            iou_threshold, multi_label)
--- mmcv/utils/ext_loader.py
+++ mmcv/utils/ext_loader.py
@@ -16,7 +16,7 @@ else:
     from parrots import extension
 
     has_return_value_ops = [
-        'nms', 'softnms', 'nms_match', 'top_pool_forward', 'top_pool_backward',
+        'nms', 'softnms', 'nms_match', 'nms_rotated', 'top_pool_forward', 'top_pool_backward',
         'bottom_pool_forward', 'bottom_pool_backward', 'left_pool_forward',
         'left_pool_backward', 'right_pool_forward', 'right_pool_backward'
     ]
