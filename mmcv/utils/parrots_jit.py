7b9fd9b079cf78d6176d1ad0464b4c5ade363cd0
limaolin <limaolin@sensetime.com>
2021-02-04 01:05:01 +0800

    fix for lint

--- mmcv/utils/parrots_jit.py
+++ mmcv/utils/parrots_jit.py
@@ -1,10 +1,10 @@
 import os
+
 from .parrots_wrapper import TORCH_VERSION
 
 parrots_jit_option = os.getenv(PARROTS_JIT_OPTION)
 
-
-if TORCH_VERSION == 'parrots' and parrots_jit_option == 'ON'
+if TORCH_VERSION == 'parrots' and parrots_jit_option == 'ON':
     from parrots.jit import pat as jit
 else:
 
