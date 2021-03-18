00870b9c4e95396365f34287595277a1c61bcf3f
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2021-03-18 20:52:45 +0800

    [Refactoring] Revise function of is_init flag in BaseModule (#898)

--- mmcv/runner/base_module.py
+++ mmcv/runner/base_module.py
@@ -40,14 +40,12 @@ class BaseModule(nn.Module, metaclass=ABCMeta):
         from ..cnn import initialize
 
         if not self._is_init:
-
             if hasattr(self, 'init_cfg'):
                 initialize(self, self.init_cfg)
-                self._is_init = True
             for module in self.children():
                 if 'init_weight' in dir(module):
                     module.init_weight()
-
+            self._is_init = True
         else:
             warnings.warn('This module has bee initialized, \
                 please call initialize(module, init_cfg) to reinitialize it')
