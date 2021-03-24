29b3f856cc0e65a3e2ae828f5036f88e4be0f72d
lixinran <lixr423@outlook.com>
2021-03-24 14:38:35 +0800

    add cutout

--- mmcv/image/__init__.py
+++ mmcv/image/__init__.py
@@ -2,9 +2,9 @@
 from .colorspace import (bgr2gray, bgr2hls, bgr2hsv, bgr2rgb, bgr2ycbcr,
                          gray2bgr, gray2rgb, hls2bgr, hsv2bgr, imconvert,
                          rgb2bgr, rgb2gray, rgb2ycbcr, ycbcr2bgr, ycbcr2rgb)
-from .geometric import (imcrop, imflip, imflip_, impad, impad_to_multiple,
-                        imrescale, imresize, imresize_like, imrotate, imshear,
-                        imtranslate, rescale_size)
+from .geometric import (imcrop, imcutout, imflip, imflip_, impad,
+                        impad_to_multiple, imrescale, imresize, imresize_like,
+                        imrotate, imshear, imtranslate, rescale_size)
 from .io import imfrombytes, imread, imwrite, supported_backends, use_backend
 from .misc import tensor2imgs
 from .photometric import (adjust_brightness, adjust_color, adjust_contrast,
@@ -22,5 +22,5 @@ __all__ = [
     'rgb2ycbcr', 'bgr2ycbcr', 'ycbcr2rgb', 'ycbcr2bgr', 'tensor2imgs',
     'imshear', 'imtranslate', 'adjust_color', 'imequalize',
     'adjust_brightness', 'adjust_contrast', 'lut_transform', 'clahe',
-    'adjust_sharpness', 'auto_contrast'
+    'adjust_sharpness', 'auto_contrast', 'imcutout'
 ]
