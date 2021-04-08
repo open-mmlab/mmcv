6abd6cb3bdb1fea9b6e801e403edf7fbba45d2fd
MeowZheng <meowzheng@outlook.com>
2021-04-09 00:47:36 +0800

    add pytest param

--- tests/test_runner/test_hooks.py
+++ tests/test_runner/test_hooks.py
@@ -251,8 +251,8 @@ def test_cosine_runner_hook():
     ]
     hook.writer.add_scalars.assert_has_calls(calls, any_order=True)
 
-
-def test_one_cycle_runner_hook():
+@pytest.mark.parametrize('max_iters', (10, 2))
+def test_one_cycle_runner_hook(max_iters):
     """Test OneCycleLrUpdaterHook and OneCycleMomentumUpdaterHook."""
     with pytest.raises(AssertionError):
         # by_epoch should be False
@@ -320,47 +320,34 @@ def test_one_cycle_runner_hook():
     sys.modules['pavi'] = MagicMock()
     loader = DataLoader(torch.ones((10, 2)))
     runner = _build_demo_runner(
-        runner_type='IterBasedRunner', max_epochs=None, max_iters=10)
-    hook_cfg = dict(
-        type='OneCycleLrUpdaterHook',
-        max_lr=0.01,
-        total_steps=2,
-        pct_start=0.5,
-        anneal_strategy='linear',
-        div_factor=25,
-        final_div_factor=1e4,
-        three_phase=False)
-    # test total_steps < max_iters
-    with pytest.raises(ValueError):
-        runner.register_hook_from_cfg(hook_cfg)
+        runner_type='IterBasedRunner', max_epochs=None, max_iters=max_iters)
+    
+    args = dict(max_lr=0.01,
+            total_steps=5,
+            pct_start=0.5,
+            anneal_strategy='linear',
+            div_factor=25,
+            final_div_factor=1e4,)
+    hook= OneCycleLrUpdaterHook(**args)
+    runner.register_hook(hook)
+    if max_iters == 10:
+        # test total_steps < max_iters
+        with pytest.raises(ValueError):
+            runner.run([loader], [('train', 1)])
+    else:
+        # test total_steps > max_iters
         runner.run([loader], [('train', 1)])
+        lr_last = runner.current_lr()
+        t = torch.tensor([0.0], requires_grad=True)
+        optim = torch.optim.SGD([t], lr=0.01)
+        lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optim, **args)
+        lr_target = []
+        for _ in range(max_iters):
+            optim.step()
+            lr_target.append(optim.param_groups[0]['lr'])
+            lr_scheduler.step()
 
-    # test total_steps > max_iters
-    max_iters = 10
-    args = dict(
-        max_lr=0.01,
-        total_steps=12,
-        pct_start=0.5,
-        anneal_strategy='linear',
-        div_factor=25,
-        final_div_factor=1e4)
-    hook_cfg.update(args)
-    runner = _build_demo_runner(
-        runner_type='IterBasedRunner', max_epochs=None, max_iters=10)
-    runner.register_hook_from_cfg(hook_cfg)
-    runner.run([loader], [('train', 1)])
-    lr_last = runner.current_lr()
-
-    t = torch.tensor([0.0], requires_grad=True)
-    optim = torch.optim.SGD([t], lr=0.01)
-    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optim, **args)
-    lr_target = []
-    for _ in range(max_iters):
-        optim.step()
-        lr_target.append(optim.param_groups[0]['lr'])
-        lr_scheduler.step()
-
-    assert lr_target[-1] == lr_last[0]
+        assert lr_target[-1] == lr_last[0]
 
 
 def test_cosine_restart_lr_update_hook():
