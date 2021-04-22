eae296a63a7b9f252e621c7776f8176628c24f04
zhouzaida <zhouzaida@163.com>
2021-04-22 15:45:16 +0800

    pring log and fix docstring

--- mmcv/runner/hooks/profiler.py
+++ mmcv/runner/hooks/profiler.py
@@ -10,7 +10,7 @@ from .hook import HOOKS, Hook
 
 @HOOKS.register_module()
 class ProfilerHook(Hook):
-    """Profiler context manager.
+    """Profiler to analyze perfromance during training and inference.
 
     PyTorch Profiler is a tool that allows the collection of the performance
     metrics during the training and inference. More details on Profiler can
@@ -19,39 +19,29 @@ class ProfilerHook(Hook):
 
     Args:
         activities (list[str]): List of activity groups (CPU, CUDA) to use in
-            profiling.
-            Default: ['cpu', 'cuda'].
+            profiling. Default: ['cpu', 'cuda'].
         schedule (dict, optional): Config of generating the callable schedule.
             if schedule is None, profiler will not add step markers into the
-            trace and table view.
-            Default: None.
+            trace and table view. Default: None.
         on_trace_ready (callable, dict): Either a handler or a dict of generate
-            handler.
-            Default: None.
+            handler. Default: None.
         record_shapes (bool): Save information about operator's input shapes.
             Default: False.
         profile_memory (bool): Track tensor memory allocation/deallocation.
             Default: False.
         with_stack (bool): Record source information (file and line number)
-            for the ops.
-            Default: False.
+            for the ops. Default: False.
         with_flops (bool): Use formula to estimate the FLOPS of specific
             operators (matrix multiplication and 2D convolution).
             Default: False.
         json_trace_path (str, optional): Exports the collected trace in Chrome
-            JSON format.
-            Default: None.
+            JSON format. Default: None.
 
     Example:
         >>> runner = ... # instantiate a Runner
-        >>> schedule = dict(wait=1, warmup=1, active=2)
-        >>> # tensorboard trace or log trace
+        >>> # tensorboard trace
         >>> trace_config = dict(type='tb_trace', dir_name='work_dir')
-        >>> trace_config = dict(type='log_trace',
-        >>>                     sort_by='self_cuda_time_total',
-        >>>                     row_limit=-1)
-        >>> profiler_config = dict(schedule=schedule,
-        >>>                        on_trace_ready=trace_config)
+        >>> profiler_config = dict(on_trace_ready=trace_config)
         >>> runner.register_profiler_hook(profiler_config)
         >>> runner.run(data_loaders=[trainloader], workflow=[('train', 1)])
     """
@@ -66,9 +56,9 @@ class ProfilerHook(Hook):
                  with_flops: bool = False,
                  json_trace_path: Optional[str] = None) -> None:
         try:
-            from torch import profiler  # torch version >= 1.8.0
+            from torch import profiler  # torch version >= 1.8.1
         except ImportError:
-            raise ImportError('profiler is the new feature of torch1.8.0, '
+            raise ImportError('profiler is the new feature of torch1.8.1, '
                               f'but your verison is {torch.__version__}')
 
         if not isinstance(activities, list):
@@ -102,11 +92,12 @@ class ProfilerHook(Hook):
         if callable(self.on_trace_ready):  # handler
             _on_trace_ready = self.on_trace_ready
         elif isinstance(self.on_trace_ready, dict):  # config of handler
-            trace_type = self.on_trace_ready.pop('type')  # log_trace handler
+            trace_cfg = self.on_trace_ready.copy()
+            trace_type = trace_cfg.pop('type')  # log_trace handler
             if trace_type == 'log_trace':
 
                 def _log_hanlder(prof):
-                    print(prof.key_averages().table(**self.on_trace_ready))
+                    print(prof.key_averages().table(**trace_cfg))
 
                 _on_trace_ready = _log_hanlder
             elif trace_type == 'tb_trace':  # tensorboard_trace handler
@@ -117,7 +108,7 @@ class ProfilerHook(Hook):
                                       'torch-tb-profiler" to install '
                                       'torch_tb_profiler')
                 _on_trace_ready = torch.profiler.tensorboard_trace_handler(
-                    **self.on_trace_ready)
+                    **trace_cfg)
             else:
                 raise ValueError('trace_type should be "log_trace" or '
                                  f'"tb_trace", but got {trace_type}')
@@ -128,10 +119,12 @@ class ProfilerHook(Hook):
                              f'but got {type(self.on_trace_ready)}')
 
         if runner.max_epochs > 1:
-            warnings.warn(f'epoch should be 1, but got {runner.max_epochs}. '
-                          'profiler will slow down your training, so it is '
-                          'not advised to set epoch greater than 1 if you use '
-                          'ProfilerHook.')
+            warnings.warn(f'profiler will profile {runner.max_epochs} epochs '
+                          'instead of 1 epoch. Since profiler will slow down '
+                          'the training, it is recommended to train 1 epoch '
+                          'with ProfilerHook and adjust your setting according'
+                          ' to the profiler summary. During normal training '
+                          '(epoch > 1), you may disable the ProfilerHook.')
 
         self.profiler = torch.profiler.profile(
             activities=self.activities,
@@ -143,6 +136,7 @@ class ProfilerHook(Hook):
             with_flops=self.with_flops)
 
         self.profiler.__enter__()
+        runner.logger.info('profiler is profiling...')
 
     @master_only
     def after_train_iter(self, runner):
