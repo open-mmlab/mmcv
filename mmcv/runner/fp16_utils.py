d27eec70bc3d8a01516e874e208fa898c86f66bc
xiongyu <xiongyuxy@gmail.com>
2021-04-26 16:26:31 +0800

    add doc to explain the behavior differences between real amp and ours

--- mmcv/runner/fp16_utils.py
+++ mmcv/runner/fp16_utils.py
@@ -13,6 +13,8 @@ from .dist_utils import allreduce_grads as _allreduce_grads
 try:
     # If PyTorch version >= 1.6.0, torch.cuda.amp.autocast would be imported
     # and used; otherwise, auto fp16 will adopt mmcv's implementation.
+    # Note that when PyTorch >= 1.6.0, we still cast tensor types to fp16
+    # manually, so the behavior may not be consistant with real amp.
     from torch.cuda.amp import autocast
 except ImportError:
     pass
