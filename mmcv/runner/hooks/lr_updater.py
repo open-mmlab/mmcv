1a5bf7623701497f927f3250405604791485a6e3
gengenkai <30782254+gengenkai@users.noreply.github.com>
2021-05-11 13:25:43 +0800

    [Enhancement] LR triangular (#1001)
    * [Fix] lr add triangular
    
    * [Fix] lr_triangular
    
    * lr add docstring
    
    * lr add docstring
    
    * add unittest of triangular lr update

--- mmcv/runner/hooks/lr_updater.py
+++ mmcv/runner/hooks/lr_updater.py
@@ -367,13 +367,16 @@ class CyclicLrUpdaterHook(LrUpdaterHook):
     triangular policy inside a cycle. This improves the performance in the
     3D detection area.
 
-    Attributes:
+    Args:
+        by_epoch (bool): Whether to update LR by epoch.
         target_ratio (tuple[float]): Relative ratio of the highest LR and the
             lowest LR to the initial LR.
         cyclic_times (int): Number of cycles during training
         step_ratio_up (float): The ratio of the increasing process of LR in
             the total cycle.
-        by_epoch (bool): Whether to update LR by epoch.
+        anneal_strategy (str): {'cos', 'linear'}
+            Specifies the annealing strategy: 'cos' for cosine annealing,
+            'linear' for linear annealing. Default: 'cos'.
     """
 
     def __init__(self,
@@ -381,6 +384,7 @@ class CyclicLrUpdaterHook(LrUpdaterHook):
                  target_ratio=(10, 1e-4),
                  cyclic_times=1,
                  step_ratio_up=0.4,
+                 anneal_strategy='cos',
                  **kwargs):
         if isinstance(target_ratio, float):
             target_ratio = (target_ratio, target_ratio / 1e5)
@@ -400,6 +404,14 @@ class CyclicLrUpdaterHook(LrUpdaterHook):
         self.cyclic_times = cyclic_times
         self.step_ratio_up = step_ratio_up
         self.lr_phases = []  # init lr_phases
+        # validate anneal_strategy
+        if anneal_strategy not in ['cos', 'linear']:
+            raise ValueError('anneal_strategy must be one of "cos" or '
+                             f'"linear", instead got {anneal_strategy}')
+        elif anneal_strategy == 'cos':
+            self.anneal_func = annealing_cos
+        elif anneal_strategy == 'linear':
+            self.anneal_func = annealing_linear
 
         assert not by_epoch, \
             'currently only support "by_epoch" = False'
@@ -425,9 +437,9 @@ class CyclicLrUpdaterHook(LrUpdaterHook):
             curr_iter %= max_iter_per_phase
             if start_iter <= curr_iter < end_iter:
                 progress = curr_iter - start_iter
-                return annealing_cos(base_lr * start_ratio,
-                                     base_lr * end_ratio,
-                                     progress / (end_iter - start_iter))
+                return self.anneal_func(base_lr * start_ratio,
+                                        base_lr * end_ratio,
+                                        progress / (end_iter - start_iter))
 
 
 @HOOKS.register_module()
