7c441d51889d38af1949a86954dd12d74790e5da
XUANBABY <XUANBABY_1993@163.com>
2021-05-13 11:15:54 +0800

    porting mmcv for hip

--- setup.py
+++ setup.py
@@ -220,18 +220,41 @@ def get_extensions():
         define_macros = []
         extra_compile_args = {'cxx': []}
 
-        if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
+        is_rocm_pytorch = False
+        if torch.__version__ >= '1.5':
+            from torch.utils.cpp_extension import ROCM_HOME
+            is_rocm_pytorch = True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
+
+        this_dir = "mmcv/ops/csrc/"
+        if is_rocm_pytorch:
+            from torch.utils.hipify import hipify_python
+            hipify_python.hipify(
+                project_directory=this_dir,
+                    output_directory=this_dir,
+                    includes="mmcv/ops/csrc/*",
+                    show_detailed=True,
+                is_pytorch_extension=True,
+            )
+            define_macros += [('MMCV_WITH_CUDA', None)]
+            define_macros += [('HIP_DIFF', None)]
+            extra_compile_args['nvcc'] = [cuda_args] if cuda_args else []
+            op_files = glob.glob('./mmcv/ops/csrc/pytorch/hip/*')
+            extension = CUDAExtension
+            include_path = os.path.abspath('./mmcv/ops/csrc/hip')
+
+        elif torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
             define_macros += [('MMCV_WITH_CUDA', None)]
             cuda_args = os.getenv('MMCV_CUDA_ARGS')
             extra_compile_args['nvcc'] = [cuda_args] if cuda_args else []
             op_files = glob.glob('./mmcv/ops/csrc/pytorch/*')
             extension = CUDAExtension
+            include_path = os.path.abspath('./mmcv/ops/csrc')
         else:
             print(f'Compiling {ext_name} without CUDA')
             op_files = glob.glob('./mmcv/ops/csrc/pytorch/*.cpp')
             extension = CppExtension
+            include_path = os.path.abspath('./mmcv/ops/csrc')
 
-        include_path = os.path.abspath('./mmcv/ops/csrc')
         ext_ops = extension(
             name=ext_name,
             sources=op_files,
