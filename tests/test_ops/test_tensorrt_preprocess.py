5cd8a54fbdfdf14f5e94eaa65555317f4c21c0f5
Semyon Bevzyuk <semen.bevzuk@gmail.com>
2021-05-19 23:53:51 +0300

    Changed torch version from 1.3.1 to 1.4.0

--- tests/test_ops/test_tensorrt_preprocess.py
+++ tests/test_ops/test_tensorrt_preprocess.py
@@ -41,7 +41,7 @@ def export_nms_module_to_onnx(module, onnx_file):
         output_names=['output'])
 
     onnx_model = onnx.load(onnx_file)
-    if torch.__version__.split('+')[0] > '1.3.1':
+    if torch.__version__.split('+')[0] > '1.4.0':
         onnx.checker.check_model(onnx_model)
     return onnx_model
 
