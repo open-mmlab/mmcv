b1ab260e97491310dc4fe4a748fa51fae65cbb90
zhangshilong <2392587229zsl@gmail.com>
2021-06-08 23:36:23 +0800

    add comments for drop path

--- mmcv/cnn/bricks/drop.py
+++ mmcv/cnn/bricks/drop.py
@@ -15,8 +15,8 @@ def drop_path(x, drop_prob=0., training=False):
     if drop_prob == 0. or not training:
         return x
     keep_prob = 1 - drop_prob
-    shape = (x.shape[0], ) + (1, ) * (
-        x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    # handle tensors with different dimensions, not just 4D tensors.
+    shape = (x.shape[0], ) + (1, ) * (x.ndim - 1)
     random_tensor = keep_prob + torch.rand(
         shape, dtype=x.dtype, device=x.device)
     output = x.div(keep_prob) * random_tensor.floor()
