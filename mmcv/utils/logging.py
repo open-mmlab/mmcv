b2034410fd905525ff7dfdb90e62309a15883507
Qizhu Li <qizhu.li@eng.ox.ac.uk>
2021-07-08 02:09:57 +0800

    Run the root logger handler fix on uninitialised loggers only
    Adds a more detailed explanation in comments

--- mmcv/utils/logging.py
+++ mmcv/utils/logging.py
@@ -28,12 +28,6 @@ def get_logger(name, log_file=None, log_level=logging.INFO, file_mode='w'):
         logging.Logger: The expected logger.
     """
     logger = logging.getLogger(name)
-
-    # set the root logger's StreamHandler, if any, to log at the ERROR level
-    for handler in logger.root.handlers:
-        if type(handler) is logging.StreamHandler:
-            handler.setLevel(logging.ERROR)
-
     if name in logger_initialized:
         return logger
     # handle hierarchical names
@@ -42,6 +36,17 @@ def get_logger(name, log_file=None, log_level=logging.INFO, file_mode='w'):
     for logger_name in logger_initialized:
         if name.startswith(logger_name):
             return logger
+
+    # handle duplicate logs to the console
+    # Starting in 1.8.0, PyTorch DDP attaches a StreamHandler <stderr> (NOTSET)
+    # to the root logger. As logger.propagate is True by default, this root
+    # level handler causes logging messages from rank>0 processes to
+    # unexpectedly show up on the console, creating much unwanted clutter.
+    # To fix this issue, we set the root logger's StreamHandler, if any, to log
+    # at the ERROR level.
+    for handler in logger.root.handlers:
+        if type(handler) is logging.StreamHandler:
+            handler.setLevel(logging.ERROR)
 
     stream_handler = logging.StreamHandler()
     handlers = [stream_handler]
