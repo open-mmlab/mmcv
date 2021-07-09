3bc2e35ea4057e6c1def7f608187fcfc0478495a
Kenny <dhd.efz@gmail.com>
2021-07-09 21:33:28 +0800

    more testing for digit_version

--- mmcv/utils/version_utils.py
+++ mmcv/utils/version_utils.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
+import warnings
 from pkg_resources import parse_version
 
 
 def digit_version(version_str: str, length: int = 4):
     """Convert a version string into a tuple of integers.
 
-    This method is usually used for comparing two versions.
+    This method is usually used for comparing two versions. For pre-release
+    versions: alpha < beta < rc.
 
     Args:
         version_str (str): The version string.
@@ -22,9 +24,21 @@ def digit_version(version_str: str, length: int = 4):
     if len(release) < length:
         release = release + [0] * (length - len(release))
     if version.is_prerelease:
-        release.extend([-1, version.pre[-1]])
+        mapping = {'a': -3, 'b': -2, 'rc': -1}
+        val = -4
+        # version.pre can be None
+        if version.pre:
+            if version.pre[0] not in mapping:
+                warnings.warn(f'unknown prerelease version {version.pre[0]}, '
+                              'version checking may go wrong')
+            else:
+                val = mapping[version.pre[0]]
+            release.extend([val, version.pre[-1]])
+        else:
+            release.extend([val, 0])
+
     elif version.is_postrelease:
-        release.extend([1, version.post[-1]])
+        release.extend([1, version.post])
     else:
         release.extend([0, 0])
     return tuple(release)
