2e5d4c01415c1bcccb701c3d77e89133ff9a3b6d
Ye Liu <22849092+yeliudev@users.noreply.github.com>
2021-07-10 10:58:23 +0800

    Register CrissCrossAttention into plugin layers

--- mmcv/ops/cc_attention.py
+++ mmcv/ops/cc_attention.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd.function import once_differentiable
 
-from mmcv.cnn import Scale
+from mmcv.cnn import PLUGIN_LAYERS, Scale
 from ..utils import ext_loader
 
 ext_module = ext_loader.load_ext(
@@ -66,6 +66,7 @@ ca_weight = CAWeightFunction.apply
 ca_map = CAMapFunction.apply
 
 
+@PLUGIN_LAYERS.register_module()
 class CrissCrossAttention(nn.Module):
     """Criss-Cross Attention Module."""
 
