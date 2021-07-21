813fd8be9e185609e634be229fb624deb44eea51
MeowZheng <meowzheng@outlook.com>
2021-07-21 21:02:09 +0800

    format

--- docs/_static/community/im2base64.py
+++ docs/_static/community/im2base64.py
@@ -4,7 +4,7 @@ import os
 for img in os.listdir('./'):
     if img.endswith('.png') or img.endswith('.jpg'):
         f = open(img, 'rb')
-        ls_f=base64.b64encode(f.read())
+        ls_f = base64.b64encode(f.read())
         f.close()
         print(img)
         print(ls_f)
