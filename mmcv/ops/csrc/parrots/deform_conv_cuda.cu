4884533fb7c7df778fd442dba326d5cb24e5766d
zhangshilong <2392587229zsl@gmail.com>
2021-07-22 16:26:22 +0800

    fix dcn forward and backward when batchsize is larger than im2col_step

--- mmcv/ops/csrc/parrots/deform_conv_cuda.cu
+++ mmcv/ops/csrc/parrots/deform_conv_cuda.cu
@@ -278,6 +278,8 @@ void DeformConvForwardCUDAKernelLauncher(Tensor input, Tensor weight,
     }
     columns =
         columns.view({columns.size(0) * columns.size(1), columns.size(2)});
+    weight = weight.view({weight.size(0) * weight.size(1), weight.size(2),
+                          weight.size(3), weight.size(4)});
   }
 
   output_buffer = output_buffer.view(
@@ -384,6 +386,8 @@ void DeformConvBackwardInputCUDAKernelLauncher(
     deformable_col2im(columns, offset[elt], nInputPlane, inputHeight,
                       inputWidth, kH, kW, padH, padW, dH, dW, dilationH,
                       dilationW, im2col_step, deformable_group, gradInput[elt]);
+    weight = weight.view({weight.size(0) * weight.size(1), weight.size(2),
+                          weight.size(3), weight.size(4)});
   }
 
   gradOutput.transpose_(1, 2);
