844b52d26b2de72525004e8e11f2059d77519327
mzr1996 <mzr1996@163.com>
2021-07-28 11:15:22 +0800

    Add type check, BN warning and resume warning. And fix typo, lint the code.

--- mmcv/runner/hooks/optimizer.py
+++ mmcv/runner/hooks/optimizer.py
@@ -5,7 +5,7 @@ from itertools import chain
 
 from torch.nn.utils import clip_grad
 
-from mmcv.utils import TORCH_VERSION, digit_version
+from mmcv.utils import TORCH_VERSION, _BatchNorm, digit_version
 from ..dist_utils import allreduce_grads
 from ..fp16_utils import LossScaler, wrap_fp16_model
 from .hook import HOOKS, Hook
@@ -66,30 +66,67 @@ class GradientCumulativeOptimizerHook(OptimizerHook):
 
     def __init__(self, grad_clip=None, cumulative_iters=1):
         super(GradientCumulativeOptimizerHook, self).__init__(grad_clip)
+
+        assert isinstance(cumulative_iters, int) and cumulative_iters > 0, \
+            f'cumulative_iters only accepts positive int, but got ' \
+            f'{type(cumulative_iters)} instead.'
+
         self.cumulative_iters = cumulative_iters
-        self.divisible_ietrs = 0
+        self.divisible_iters = 0
         self.remainder_iters = 0
         self.initialized = False
 
+    def has_batch_norm(self, module):
+        if isinstance(module, _BatchNorm):
+            return True
+        for m in module.children():
+            if self.has_batch_norm(m):
+                return True
+        return False
+
     def _init(self, runner):
-        self.divisible_ietrs = runner.max_iters // self.cumulative_iters * self.cumulative_iters
-        self.remainder_iters = runner.max_iters % self.cumulative_iters
+        if runner.iter % self.cumulative_iters > 0:
+            runner.logger.warning(
+                'Resume iter number is not divisible by cumulative_iters in '
+                'GradientCumulativeOptimizerHook, which means the gradient of '
+                'some iters is lost and the result may be influenced slightly.'
+            )
+
+        if self.has_batch_norm(runner.model) and self.cumulative_iters > 1:
+            runner.logger.warning(
+                'GradientCumulativeOptimizerHook may slightly decrease '
+                'performance if the model has BatchNorm layers.')
+
+        residual_iters = runner.max_iters - runner.iter
+
+        self.divisible_iters = (
+            residual_iters // self.cumulative_iters * self.cumulative_iters)
+        self.remainder_iters = residual_iters - self.divisible_iters
+
         self.initialized = True
 
     def after_train_iter(self, runner):
         if not self.initialized:
             self._init(runner)
-        loss_factor = self.cumulative_iters if runner.iter < self.divisible_ietrs else self.remainder_iters
-        runner.outputs['loss'] = runner.outputs['loss'] / loss_factor
-        runner.outputs['loss'].backward()
-        if (runner.iter + 1) % self.cumulative_iters == 0 or runner.iter == runner.max_iters:
-            runner.optimizer.step()
+
+        if runner.iter < self.divisible_iters:
+            loss_factor = self.cumulative_iters
+        else:
+            loss_factor = self.remainder_iters
+        loss = runner.outputs['loss']
+        loss = loss / loss_factor
+        loss.backward()
+
+        if (self.every_n_iters(runner, self.cumulative_iters)
+                or self.is_last_iter(runner)):
+
             if self.grad_clip is not None:
                 grad_norm = self.clip_grads(runner.model.parameters())
                 if grad_norm is not None:
                     # Add grad norm to the logger
                     runner.log_buffer.update({'grad_norm': float(grad_norm)},
                                              runner.outputs['num_samples'])
+            runner.optimizer.step()
             runner.optimizer.zero_grad()
 
 
