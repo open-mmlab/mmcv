ae4570b54c5fb5a87c7569e2ce4851c3a82bc01d
densechen <densechen@foxmail.com>
2021-08-03 15:53:10 +0800

    runner constructor

--- mmcv/runner/builder.py
+++ mmcv/runner/builder.py
@@ -1,7 +1,20 @@
 from ..utils import Registry, build_from_cfg
+import copy
 
 RUNNERS = Registry('runner')
+RUNNER_BUILDERS = Registry('runner builder')
 
+def build_runner_constructor(cfg):
+    return build_from_cfg(cfg, RUNNER_BUILDERS)
 
 def build_runner(cfg, default_args=None):
-    return build_from_cfg(cfg, RUNNERS, default_args=default_args)
+    runner_cfg = copy.deepcopy(cfg)
+    constructor_type = runner_cfg.pop('constructor',
+                                         'DefaultRunnerConstructor')
+    runner_constructor = build_runner_constructor(
+        dict(
+            type=constructor_type,
+            runner_cfg=runner_cfg,
+            default_args=default_args))
+    runner = runner_constructor()
+    return runner
