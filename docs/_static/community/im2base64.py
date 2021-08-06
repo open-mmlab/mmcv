4a26cb0f8eb081aef17d12456884e887565d9aa1
MeowZheng <meowzheng@outlook.com>
2021-08-06 15:47:11 +0800

    del unused

--- docs/_static/community/im2base64.py
+++
@@ -1,8 +0,0 @@
-import base64
-import os
-
-for img in os.listdir('./'):
-    if img.endswith('.png') or img.endswith('.jpg'):
-        f = open(img, 'rb')
-        ls_f = base64.b64encode(f.read())
-        f.close()
