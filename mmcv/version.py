a8d199e3380548da4a16d347456ceadf974d978c
Kenny <dhd.efz@gmail.com>
2021-08-07 12:59:08 +0800

    update

--- mmcv/version.py
+++ mmcv/version.py
@@ -30,6 +30,6 @@ def parse_version_info(version_str: str, length: int = 4) -> tuple:
     return tuple(release)
 
 
-version_info = tuple([int(x) for x in __version__.split('.')])
+version_info = tuple(int(x) for x in __version__.split('.')[:3])
 
 __all__ = ['__version__', 'version_info', 'parse_version_info']
