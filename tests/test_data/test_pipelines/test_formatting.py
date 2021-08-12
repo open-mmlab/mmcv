c355f1f9ca77237bbd3a32dda5f26ede3b94f0d2
mzr1996 <mzr1996@163.com>
2021-08-12 16:58:41 +0800

    Add unit tests for `ImageToTensor`.

--- tests/test_data/test_pipelines/test_formatting.py
+++ tests/test_data/test_pipelines/test_formatting.py
@@ -2,8 +2,10 @@ import copy
 import os.path as osp
 
 import numpy as np
+import pytest
 import torch
 
+import mmcv
 from mmcv.datasets import PIPELINES
 
 
@@ -60,3 +62,30 @@ class TestFormatting:
         assert torch.allclose(results['img'], torch.tensor([1, 2]))
         assert isinstance(results['ann']['label'], torch.Tensor)
         assert torch.allclose(results['ann']['label'], torch.tensor([1, 2]))
+
+    def test_image_to_tensor(self):
+        img = mmcv.imread(osp.join(self.data_prefix, 'color.jpg'))
+        transform_cfg = dict(type='ImageToTensor')
+        transform = PIPELINES.build(transform_cfg)
+
+        # test basic function
+        data_info = dict(img=img, img_fields=['img'])
+        results = transform(copy.deepcopy(data_info))
+        assert isinstance(results['img'], torch.Tensor)
+        assert torch.allclose(results['img'],
+                              torch.from_numpy(img).permute([2, 0, 1]))
+
+        # test multi img_fields
+        data_info = dict(img1=img, img2=img, img_fields=['img1', 'img2'])
+        results = transform(copy.deepcopy(data_info))
+        assert isinstance(results['img1'], torch.Tensor)
+        assert torch.allclose(results['img1'],
+                              torch.from_numpy(img).permute([2, 0, 1]))
+        assert isinstance(results['img2'], torch.Tensor)
+        assert torch.allclose(results['img2'],
+                              torch.from_numpy(img).permute([2, 0, 1]))
+
+        # test without img_fields
+        with pytest.raises(AssertionError):
+            data_info = dict(img=img)
+            transform(copy.deepcopy(data_info))
