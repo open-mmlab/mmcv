193eacdeb57da0fbe6889ee74021c050c9e568f0
zhouzaida <zhouzaida@163.com>
2021-08-12 15:39:33 +0800

    add unittest to check to_rgb argument

--- tests/test_data/test_pipelines/test_transform/test_transform.py
+++ tests/test_data/test_pipelines/test_transform/test_transform.py
@@ -2,19 +2,19 @@ import copy
 import os.path as osp
 
 import numpy as np
+import pytest
 
 import mmcv
 from mmcv.datasets.builder import PIPELINES
 from mmcv.utils import build_from_cfg
 
 
-def test_normalize():
+@pytest.mark.parametrize('to_rgb', [True, False])
+def test_normalize(to_rgb):
     img_norm_cfg = dict(
         mean=[123.675, 116.28, 103.53],
         std=[58.395, 57.12, 57.375],
-        to_rgb=True)
-    transform_cfg = dict(type='Normalize', **img_norm_cfg)
-    transform = build_from_cfg(transform_cfg, PIPELINES)
+        to_rgb=to_rgb)
     results = dict()
     img = mmcv.imread(
         osp.join(osp.dirname(__file__), '../../../data/color.jpg'), 'color')
@@ -26,12 +26,21 @@ def test_normalize():
     # Set initial values for default meta_keys
     results['pad_shape'] = img.shape
     results['scale_factor'] = 1.0
-    results['img_fields'] = ['img', 'img2']
 
+    transform_cfg = dict(type='Normalize', **img_norm_cfg)
+    transform = build_from_cfg(transform_cfg, PIPELINES)
+
+    with pytest.raises(AssertionError):
+        # Required key of results is 'img_fields'
+        results = transform(results)
+
+    results['img_fields'] = ['img', 'img2']
     results = transform(results)
     assert np.equal(results['img'], results['img2']).all()
 
     mean = np.array(img_norm_cfg['mean'])
     std = np.array(img_norm_cfg['std'])
-    converted_img = (original_img[..., ::-1] - mean) / std
+    if to_rgb:
+        original_img = original_img[..., ::-1]
+    converted_img = (original_img - mean) / std
     assert np.allclose(results['img'], converted_img)
