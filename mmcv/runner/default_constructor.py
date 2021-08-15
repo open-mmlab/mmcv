33342232cc6e9b451f54fb276227d3a9a636cc68
densechen <densechen@foxmail.com>
2021-08-15 20:49:08 +0800

    fix yapf

--- mmcv/runner/default_constructor.py
+++ mmcv/runner/default_constructor.py
@@ -1,7 +1,3 @@
-import warnings
-
-import torch
-
 from .builder import RUNNER_BUILDERS, RUNNERS
 from mmcv.utils import build_from_cfg
 
@@ -10,12 +6,14 @@ class DefaultRunnerConstructor:
     """Default constructor for runners.
     """
 
-    def __init__(self, runner_cfg, default_args = None):
+    def __init__(self, runner_cfg, default_args=None):
         if not isinstance(runner_cfg, dict):
             raise TypeError('runner_cfg should be a dict',
                             f'but got {type(runner_cfg)}')
         self.runner_cfg = runner_cfg
         self.default_args = default_args
-        
+
     def __call__(self):
-        return build_from_cfg(self.runner_cfg, RUNNERS, default_args=self.default_args)
+        return build_from_cfg(
+            self.runner_cfg, RUNNERS, default_args=self.default_args)
+
