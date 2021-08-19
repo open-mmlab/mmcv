8c3232725a136b6ff6cca850f3c79944c6e6d1d5
mzr1996 <mzr1996@163.com>
2021-08-19 09:44:46 +0800

    Fix ImportError in CI without torch

--- mmcv/datasets/__init__.py
+++ mmcv/datasets/__init__.py
@@ -1,5 +1,13 @@
+# flake8: noqa
+# Copyright (c) OpenMMLab. All rights reserved.
 from .builder import PIPELINES
-from .pipelines.formatting import ImageToTensor, ToTensor
 from .pipelines.transforms import Normalize
 
-__all__ = ['PIPELINES', 'Normalize', 'ToTensor', 'ImageToTensor']
+__all__ = ['PIPELINES', 'Normalize']
+try:
+    import torch
+except ImportError:
+    pass
+else:
+    from .pipelines.formatting import ImageToTensor, ToTensor
+    __all__ += ['ImageToTensor', 'ToTensor']
--- mmcv/datasets/pipelines/__init__.py
+++ mmcv/datasets/pipelines/__init__.py
@@ -1,4 +1,13 @@
-from .formatting import ImageToTensor, ToTensor
+# flake8: noqa
+# Copyright (c) OpenMMLab. All rights reserved.
 from .transforms import Normalize
 
-__all__ = ['Normalize', 'ToTensor', 'ImageToTensor']
+__all__ = ['Normalize']
+
+try:
+    import torch
+except ImportError:
+    pass
+else:
+    from .formatting import ImageToTensor, ToTensor
+    __all__ += ['ImageToTensor', 'ToTensor']
