fb7f6ccf6a19b3958c737c03391031b93437d27d
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-08-26 10:24:15 +0200

    Updated DVCLiveLoggerHook

--- mmcv/runner/hooks/logger/dvclive.py
+++ mmcv/runner/hooks/logger/dvclive.py
@@ -1,16 +1,21 @@
-# Copyright (c) Open-MMLab. All rights reserved.
-from ...dist_utils import master_only
-from ..hook import HOOKS
-from .base import LoggerHook
+from pathlib import Path
+
+from mmcv.runner.dist_utils import master_only
+from mmcv.runner.hooks import HOOKS
+from mmcv.runner.hooks.logger.base import LoggerHook
 
 
 @HOOKS.register_module()
-class DvcliveLoggerHook(LoggerHook):
+class DVCLiveLoggerHook(LoggerHook):
     """Class to log metrics with dvclive.
 
     It requires `dvclive`_ to be installed.
 
     Args:
+        model_file (str):
+            Default None.
+            If not None, after each epoch the model will
+            be saved to {model_file}.
         interval (int): Logging interval (every k iterations).
             Default 10.
             If `by_epoch` is True, the value will be set to 0 in
@@ -19,7 +24,7 @@ class DvcliveLoggerHook(LoggerHook):
             if less than `interval`.
             Default: True.
         reset_flag (bool): Whether to clear the output buffer after logging.
-            Default: True.
+            Default: False.
         by_epoch (bool): Whether EpochBasedRunner is used.
             Default: True.
 
@@ -27,15 +32,16 @@ class DvcliveLoggerHook(LoggerHook):
         https://dvc.org/doc/dvclive
     """
 
-    def __init__(self,
-                 interval=10,
-                 ignore_last=True,
-                 reset_flag=True,
-                 by_epoch=True):
-        if by_epoch:
-            interval = 0
-        super(DvcliveLoggerHook, self).__init__(interval, ignore_last,
-                                                reset_flag, by_epoch)
+    def __init__(
+        self,
+        model_file=None,
+        interval=1,
+        ignore_last=True,
+        reset_flag=False,
+        by_epoch=True,
+    ):
+        self.model_file = model_file
+        super().__init__(interval, ignore_last, reset_flag, by_epoch)
         self.import_dvclive()
 
     def import_dvclive(self):
@@ -49,17 +55,17 @@ class DvcliveLoggerHook(LoggerHook):
     @master_only
     def log(self, runner):
         tags = self.get_loggable_tags(runner)
-        if self.by_epoch:
-            step = self.get_epoch(runner)
-        else:
-            step = self.get_iter(runner)
         if tags:
             for k, v in tags.items():
+                step = self.get_iter(runner)
                 self.dvclive.log(k, v, step=step)
 
     @master_only
     def after_train_epoch(self, runner):
         super().after_train_epoch(runner)
-        runner.save_checkpoint(
-            runner.work_dir, filename_tmpl='model.pth', create_symlink=False)
-        self.dvclive.next_step()
+        if self.model_file is not None:
+            runner.save_checkpoint(
+                Path(self.model_file).parent,
+                filename_tmpl=Path(self.model_file).name,
+                create_symlink=False,
+            )
