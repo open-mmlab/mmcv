5617ad72d0938dbc6670546f243e6be329dfcc4f
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2021-08-27 16:25:30 +0800

    [Feature] Add python3.9 in CI (#1291)
    * [Feature] Add python3.9 in CI
    
    * fix dependencies for compiling onnx

--- setup.py
+++ setup.py
@@ -335,6 +335,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Utilities',
     ],
     url='https://github.com/open-mmlab/mmcv',
