e621e08d541634fd286ce0d314c3a05f4ead7e73
Eugene Liu <eugene123tw@gmail.com>
2021-08-29 13:48:31 +0100

    Add DCN and Modulated DCN CPU implementation (#1278)
    * DCN cpu version
    
    * add modulated dcn cpu version
    
    * move deform_conv_shape_check to deform conv utils
    
    * add inline to deform_conv_shape_check
    
    * add tests
    
    * run linter
    
    * add newline at file end
    
    * run pre-commit against modulated deform conv cpp
    
    * update saconv test
    
    * run clang-format
    
    * remove cuda device inline
    
    * refactor dcn cuda/cpu functions
    
    * remove DCN util
    
    * remove DCN util hpp from all included files
    
    * Addressing PR comment by refactoring modulated-DCN
    
    * fix lint in cpp files

--- tests/test_ops/test_deform_conv.py
+++ tests/test_ops/test_deform_conv.py
@@ -36,13 +36,16 @@ gt_deform_weight_grad = [[[[3.62, 0.], [0.40, 0.18]]]]
 
 class TestDeformconv(object):
 
-    def _test_deformconv(self, dtype=torch.float, threshold=1e-3):
-        if not torch.cuda.is_available():
-            return
+    def _test_deformconv(self,
+                         dtype=torch.float,
+                         threshold=1e-3,
+                         device='cuda'):
+        if not torch.cuda.is_available() and device == 'cuda':
+            pytest.skip('test requires GPU')
         from mmcv.ops import DeformConv2dPack
         c_in = 1
         c_out = 1
-        x = torch.Tensor(input).cuda().type(dtype)
+        x = torch.tensor(input, device=device, dtype=dtype)
         x.requires_grad = True
         model = DeformConv2dPack(c_in, c_out, 2, stride=1, padding=0)
         model.conv_offset.weight.data = torch.nn.Parameter(
@@ -51,7 +54,9 @@ class TestDeformconv(object):
             torch.Tensor(offset_bias).reshape(8))
         model.weight.data = torch.nn.Parameter(
             torch.Tensor(deform_weight).reshape(1, 1, 2, 2))
-        model.cuda().type(dtype)
+        if device == 'cuda':
+            model.cuda()
+        model.type(dtype)
 
         out = model(x)
         out.backward(torch.ones_like(out))
@@ -67,6 +72,7 @@ class TestDeformconv(object):
                            gt_deform_weight_grad, threshold)
 
         from mmcv.ops import DeformConv2d
+
         # test bias
         model = DeformConv2d(1, 1, 2, stride=1, padding=0)
         assert not hasattr(model, 'bias')
@@ -121,6 +127,7 @@ class TestDeformconv(object):
                            gt_deform_weight_grad, threshold)
 
         from mmcv.ops import DeformConv2d
+
         # test bias
         model = DeformConv2d(1, 1, 2, stride=1, padding=0)
         assert not hasattr(model, 'bias')
@@ -135,9 +142,11 @@ class TestDeformconv(object):
             model = DeformConv2d(3, 4, 3, groups=3)
 
     def test_deformconv(self):
+        self._test_deformconv(torch.double, device='cpu')
+        self._test_deformconv(torch.float, device='cpu', threshold=1e-1)
         self._test_deformconv(torch.double)
         self._test_deformconv(torch.float)
-        self._test_deformconv(torch.half, 1e-1)
+        self._test_deformconv(torch.half, threshold=1e-1)
 
         # test amp when torch version >= '1.6.0', the type of
         # input data for deformconv might be torch.float or torch.half
