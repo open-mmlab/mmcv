9e2ae4a6e672eae6c64680a15f20c61fd49b56af
le0jc <bugs_coding@outlook.com>
2021-09-09 14:52:13 +0800

    [docs]polish the docstring

--- mmcv/ops/cc_attention.py
+++ mmcv/ops/cc_attention.py
@@ -22,20 +22,21 @@ class CrissCrossAttention(nn.Module):
     .. note::
         Before v1.3.13, we use a CUDA op. Since v1.3.13, we switch
         to a pure PyTorch and equivalent implementation. For more
-        details, please refer to PR #1201.
-        Time consuming for one forward pass:
-            Settings:
-                Input size: [2,512,97,97]
-                Device: 1 NVIDIA GeForce RTX 2080 Ti
-            Results:
-                (with torch.no_grad())
-                    The CUDA version: 0.0299619 s
-                    This implementation: 0.00554402 s
-                    Relative speed: 5.4x
-                (not with torch.no_grad())
-                    The CUDA version: 0.0301349 s
-                    This implementation: 0.00562803 s
-                    Relative speed: 5.4x
+        details, please refer to https://github.com/open-mmlab/mmcv/pull/1201.
+
+        Speed comparison for one forward pass:
+            Settings
+                * Input size: [2,512,97,97]
+                * Device: 1 NVIDIA GeForce RTX 2080 Ti
+            Results
+                with torch.no_grad()
+                    * The CUDA version: 0.0299619 s
+                    * This implementation: 0.00554402 s
+                    * Relative speed: 5.4x
+                not with torch.no_grad()
+                    * The CUDA version: 0.0301349 s
+                    * This implementation: 0.00562803 s
+                    * Relative speed: 5.4x
 
     Args:
         in_channels (int): Channels of the input feature map.
