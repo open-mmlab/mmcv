1ba7c5ca53708536bfc0b736d1b4052b1ec9b8c0
hudingchang.vendor <hudingchang.vendor@sensetime.com>
2021-09-17 20:55:31 +0800

    fix typo

--- mmcv/ops/ball_query.py
+++ mmcv/ops/ball_query.py
@@ -16,8 +16,7 @@ class BallQuery(Function):
     @staticmethod
     def forward(ctx, min_radius: float, max_radius: float, sample_num: int,
                 xyz: torch.Tensor, center_xyz: torch.Tensor) -> torch.Tensor:
-        """forward.
-
+        """
         Args:
             min_radius (float): minimum radius of the balls.
             max_radius (float): maximum radius of the balls.
