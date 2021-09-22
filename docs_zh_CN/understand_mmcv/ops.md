6ada830032c383a3a98f34baa3bf45553d689a58
hudingchang.vendor <hudingchang.vendor@sensetime.com>
2021-09-22 14:51:59 +0800

    fix typo

--- docs_zh_CN/understand_mmcv/ops.md
+++ docs_zh_CN/understand_mmcv/ops.md
@@ -9,6 +9,8 @@ MMCV 提供了检测、分割等任务中常用的 CUDA 算子
 - CornerPool
 - Deformable Convolution v1/v2
 - Deformable RoIPool
+- FurthestPointSample
+- FurthestPointSampleWithDist
 - GeneralizedAttention
 - MaskedConv
 - NMS
--- tests/test_ops/test_furthest_point_sample.py
+++ tests/test_ops/test_furthest_point_sample.py
@@ -7,8 +7,6 @@ from mmcv.ops import furthest_point_sample, furthest_point_sample_with_dist
 @pytest.mark.skipif(
     not torch.cuda.is_available(), reason='requires CUDA support')
 def test_fps():
-    if not torch.cuda.is_available():
-        pytest.skip()
     xyz = torch.tensor([[[-0.2748, 1.0020, -1.1674], [0.1015, 1.3952, -1.2681],
                          [-0.8070, 2.4137,
                           -0.5845], [-1.0001, 2.1982, -0.5859],
