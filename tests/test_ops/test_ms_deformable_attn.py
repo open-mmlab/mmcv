4635f9b913abf56d4aeba1d62dab02513c0f1639
zhangshilong <2392587229zsl@gmail.com>
2021-10-14 15:04:00 +0800

    add more device

--- tests/test_ops/test_ms_deformable_attn.py
+++ tests/test_ops/test_ms_deformable_attn.py
@@ -20,27 +20,29 @@ def test_multiscale_deformable_attention():
             embed_dims=256,
             num_heads=7,
         )
-
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    msda = MultiScaleDeformableAttention(
-        embed_dims=3, num_levels=2, num_heads=3)
-    msda.init_weights()
-    num_query = 5
-    bs = 1
-    embed_dims = 3
-    query = torch.rand(num_query, bs, embed_dims).to(device)
-    key = torch.rand(num_query, bs, embed_dims).to(device)
-    spatial_shapes = torch.Tensor([[2, 2], [1, 1]]).long().to(device)
-    level_start_index = torch.Tensor([0, 4]).long().to(device)
-    reference_points = torch.rand(bs, num_query, 2, 2).to(device)
-    msda.to(device)
-    msda(
-        query,
-        key,
-        key,
-        reference_points=reference_points,
-        spatial_shapes=spatial_shapes,
-        level_start_index=level_start_index)
+    available_devices = ['cpu']
+    if torch.cuda.is_available():
+        available_devices.append('cuda:0')
+    for device in available_devices:
+        msda = MultiScaleDeformableAttention(
+            embed_dims=3, num_levels=2, num_heads=3)
+        msda.init_weights()
+        num_query = 5
+        bs = 1
+        embed_dims = 3
+        query = torch.rand(num_query, bs, embed_dims).to(device)
+        key = torch.rand(num_query, bs, embed_dims).to(device)
+        spatial_shapes = torch.Tensor([[2, 2], [1, 1]]).long().to(device)
+        level_start_index = torch.Tensor([0, 4]).long().to(device)
+        reference_points = torch.rand(bs, num_query, 2, 2).to(device)
+        msda.to(device)
+        msda(
+            query,
+            key,
+            key,
+            reference_points=reference_points,
+            spatial_shapes=spatial_shapes,
+            level_start_index=level_start_index)
 
 
 def test_forward_multi_scale_deformable_attn_pytorch():
