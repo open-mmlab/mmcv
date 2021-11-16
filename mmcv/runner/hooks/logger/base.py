d4b6b1e9e4ce9e0b3d81299058da8a325e65fdb3
Wangjiazhen <841713301@qq.com>
2021-11-16 14:54:10 +0800

    [Fix] fix val loss log not show using IterBasedRunner

--- mmcv/runner/hooks/logger/base.py
+++ mmcv/runner/hooks/logger/base.py
@@ -153,6 +153,18 @@ class LoggerHook(Hook):
             if self.reset_flag:
                 runner.log_buffer.clear_output()
 
+    def after_val_iter(self, runner):
+        if self.every_n_inner_iters(runner, self.interval):
+            runner.log_buffer.average(self.interval)
+        elif self.end_of_epoch(runner) and not self.ignore_last:
+            # not precise but more stable
+            runner.log_buffer.average(self.interval)
+
+        if runner.log_buffer.ready:
+            self.log(runner)
+            if self.reset_flag:
+                runner.log_buffer.clear_output()
+
     def after_train_epoch(self, runner):
         if runner.log_buffer.ready:
             self.log(runner)
