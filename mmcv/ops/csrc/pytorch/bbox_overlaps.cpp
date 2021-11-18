aeb18bbcabf67a3546493846d2d24b47039c8c1e
zcyKTH <zcy19950525@gmail.com>
2021-11-18 17:39:38 +0800

    [Feature] Support BBoxOverlaps with cambricon MLU backend

--- mmcv/ops/csrc/pytorch/bbox_overlaps.cpp
+++ mmcv/ops/csrc/pytorch/bbox_overlaps.cpp
@@ -12,6 +12,17 @@ void bbox_overlaps_cuda(const Tensor bboxes1, const Tensor bboxes2, Tensor ious,
 }
 #endif
 
+#ifdef MMCV_WITH_MLU
+void BBoxOverlapsMLUKernelLauncher(const Tensor bboxes1, const Tensor bboxes2,
+                                   Tensor ious, const int mode,
+                                   const bool aligned, const int offset);
+
+void bbox_overlaps_mlu(const Tensor bboxes1, const Tensor bboxes2, Tensor ious,
+                       const int mode, const bool aligned, const int offset) {
+  BBoxOverlapsMLUKernelLauncher(bboxes1, bboxes2, ious, mode, aligned, offset);
+}
+#endif
+
 void bbox_overlaps(const Tensor bboxes1, const Tensor bboxes2, Tensor ious,
                    const int mode, const bool aligned, const int offset) {
   if (bboxes1.device().is_cuda()) {
@@ -23,6 +34,14 @@ void bbox_overlaps(const Tensor bboxes1, const Tensor bboxes2, Tensor ious,
     bbox_overlaps_cuda(bboxes1, bboxes2, ious, mode, aligned, offset);
 #else
     AT_ERROR("bbox_overlaps is not compiled with GPU support");
+#endif
+#ifdef MMCV_WITH_MLU
+  } else if (bboxes1.device().type() == at::kMLU) {
+    CHECK_MLU_INPUT(bboxes1);
+    CHECK_MLU_INPUT(bboxes2);
+    CHECK_MLU_INPUT(ious);
+
+    bbox_overlaps_mlu(bboxes1, bboxes2, ious, mode, aligned, offset);
 #endif
   } else {
     AT_ERROR("bbox_overlaps is not implemented on CPU");
