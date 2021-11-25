1535eedb4367151a3485e74a8f21588089acf598
Wangjiazhen <841713301@qq.com>
2021-11-25 14:29:27 +0800

    [Fix] impl all function in setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ from pathlib import Path
 from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import find_packages, setup
 
-from mmcv.utils import get_git_hash
-
 EXT_TYPE = ''
 try:
     import torch
@@ -144,6 +142,47 @@ def add_version_info():
             return gcc_version.group()
         else:
             return None
+
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    def get_git_hash(fallback='unknown', digits=None):
+        """Get the git hash of the current repo.
+
+        Args:
+            fallback (str, optional): The fallback string when git hash is
+                unavailable. Defaults to 'unknown'.
+            digits (int, optional): kept digits of the hash. Defaults to None,
+                meaning all digits are kept.
+
+        Returns:
+            str: Git commit hash.
+        """
+
+        if digits is not None and not isinstance(digits, int):
+            raise TypeError('digits must be None or an integer')
+
+        try:
+            out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+            sha = out.strip().decode('ascii')
+            if digits is not None:
+                sha = sha[:digits]
+        except OSError:
+            sha = fallback
+
+        return sha
 
     mmcv_root = Path(__file__).parent
     version_path = mmcv_root / 'mmcv' / 'version.py'
