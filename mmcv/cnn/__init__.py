ffbf42c969369d7e15acbba89f19e3e9b867087c
wangluting <wangluting@sensetime.com>
2021-11-30 20:25:27 +0800

    lint

--- mmcv/cnn/__init__.py
+++ mmcv/cnn/__init__.py
@@ -1,14 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .alexnet import AlexNet
 # yapf: disable
-from .bricks import (
-    ACTIVATION_LAYERS, CONV_LAYERS, NORM_LAYERS, PADDING_LAYERS, PLUGIN_LAYERS,
-    UPSAMPLE_LAYERS, ContextBlock, Conv2d, Conv3d, ConvAWS2d, ConvModule,
-    ConvTranspose2d, ConvTranspose3d, ConvWS2d, DepthwiseSeparableConvModule,
-    GeneralizedAttention, HSigmoid, HSwish, Linear, MaxPool2d, MaxPool3d,
-    NonLocal1d, NonLocal2d, NonLocal3d, Scale, Swish, build_activation_layer,
-    build_conv_layer, build_norm_layer, build_padding_layer,
-    build_plugin_layer, build_upsample_layer, conv_ws_2d, is_norm)
+from .bricks import (ACTIVATION_LAYERS, CONV_LAYERS, NORM_LAYERS,
+                     PADDING_LAYERS, PLUGIN_LAYERS, UPSAMPLE_LAYERS,
+                     ContextBlock, Conv2d, Conv3d, ConvAWS2d, ConvModule,
+                     ConvTranspose2d, ConvTranspose3d, ConvWS2d,
+                     DepthwiseSeparableConvModule, GeneralizedAttention,
+                     HSigmoid, HSwish, Linear, MaxPool2d, MaxPool3d,
+                     NonLocal1d, NonLocal2d, NonLocal3d, Scale, Swish,
+                     build_activation_layer, build_conv_layer,
+                     build_norm_layer, build_padding_layer, build_plugin_layer,
+                     build_upsample_layer, conv_ws_2d, is_norm)
 from .builder import MODELS, build_model_from_cfg
 # yapf: enable
 from .resnet import ResNet, make_res_layer
