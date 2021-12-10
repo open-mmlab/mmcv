8c9125b3a3d3eb2048bb40f2368bb1598c9add6a
zhouyue <592267829@qq.com>
2021-12-10 18:53:24 +0800

    Update feature_refine.cpp

--- mmcv/ops/csrc/pytorch/feature_refine.cpp
+++ mmcv/ops/csrc/pytorch/feature_refine.cpp
@@ -3,31 +3,23 @@
 // https://github.com/SJTU-Thinklab-Det/r3det-on-mmdetection/blob/master/mmdet/ops/fr/src/feature_refine_cuda.cpp
 
 #include "pytorch_cpp_helper.hpp"
+#include "pytorch_device_registry.hpp"
 
-#ifdef MMCV_WITH_CUDA
-int FRForwardLauncher(const Tensor features, const Tensor best_bboxes,
-                      const float spatial_scale, const int points,
-                      Tensor output);
-int feature_refine_forward_cuda(const Tensor features, const Tensor best_bboxes,
-                                const float spatial_scale, const int points,
-                                Tensor output) {
-  int FRForwardLauncher(const Tensor features, const Tensor best_bboxes,
-                        const float spatial_scale, const int points,
-                        Tensor output);
-};
-
-int FRBackwardLauncher(const Tensor top_grad, const Tensor best_bboxes,
-                       const float spatial_scale, const int points,
-                       Tensor bottom_grad);
-int feature_refine_backward_cuda(const Tensor top_grad,
+void feature_refine_forward_impl(const Tensor features,
                                  const Tensor best_bboxes,
                                  const float spatial_scale, const int points,
-                                 Tensor bottom_grad) {
-  int FRBackwardLauncher(const Tensor top_grad, const Tensor best_bboxes,
-                         const float spatial_scale, const int points,
-                         Tensor bottom_grad);
-};
-#endif
+                                 Tensor output) {
+  DISPATCH_DEVICE_IMPL(feature_refine_forward_impl, features, best_bboxes,
+                       spatial_scale, points, output);
+}
+
+void feature_refine_backward_impl(const Tensor top_grad,
+                                  const Tensor best_bboxes,
+                                  const float spatial_scale, const int points,
+                                  Tensor bottom_grad) {
+  DISPATCH_DEVICE_IMPL(feature_refine_backward_impl, top_grad, best_bboxes,
+                       spatial_scale, points, bottom_grad);
+}
 
 int feature_refine_forward(const Tensor features, const Tensor best_bboxes,
                            const float spatial_scale, const int points,
@@ -38,7 +30,7 @@ int feature_refine_forward(const Tensor features, const Tensor best_bboxes,
     CHECK_CUDA_INPUT(best_bboxes);
     CHECK_CUDA_INPUT(output);
 
-    return feature_refine_forward_cuda(features, best_bboxes, spatial_scale,
+    return feature_refine_forward_impl(features, best_bboxes, spatial_scale,
                                        points, output);
 #else
     AT_ERROR("feature_refine is not compiled with GPU support");
@@ -57,7 +49,7 @@ int feature_refine_backward(const Tensor top_grad, const Tensor best_bboxes,
     CHECK_CUDA_INPUT(best_bboxes);
     CHECK_CUDA_INPUT(bottom_grad);
 
-    return feature_refine_backward_cuda(top_grad, best_bboxes, spatial_scale,
+    return feature_refine_backward_impl(top_grad, best_bboxes, spatial_scale,
                                         points, bottom_grad);
 #else
     AT_ERROR("feature_refine is not compiled with GPU support");
