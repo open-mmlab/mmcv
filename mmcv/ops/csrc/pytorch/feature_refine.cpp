733dc686425141a4dc34224ea846833bb6d575b7
zhouyue <592267829@qq.com>
2021-12-10 21:23:07 +0800

    change int to void

--- mmcv/ops/csrc/pytorch/feature_refine.cpp
+++ mmcv/ops/csrc/pytorch/feature_refine.cpp
@@ -21,9 +21,9 @@ void feature_refine_backward_impl(const Tensor top_grad,
                        spatial_scale, points, bottom_grad);
 }
 
-int feature_refine_forward(const Tensor features, const Tensor best_bboxes,
-                           const float spatial_scale, const int points,
-                           Tensor output) {
+void feature_refine_forward(const Tensor features, const Tensor best_bboxes,
+                            const float spatial_scale, const int points,
+                            Tensor output) {
   if (features.device().is_cuda()) {
 #ifdef MMCV_WITH_CUDA
     CHECK_CUDA_INPUT(features);
@@ -40,9 +40,9 @@ int feature_refine_forward(const Tensor features, const Tensor best_bboxes,
   }
 }
 
-int feature_refine_backward(const Tensor top_grad, const Tensor best_bboxes,
-                            const float spatial_scale, const int points,
-                            Tensor bottom_grad) {
+void feature_refine_backward(const Tensor top_grad, const Tensor best_bboxes,
+                             const float spatial_scale, const int points,
+                             Tensor bottom_grad) {
   if (top_grad.device().is_cuda()) {
 #ifdef MMCV_WITH_CUDA
     CHECK_CUDA_INPUT(top_grad);
