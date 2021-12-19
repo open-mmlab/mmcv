1606aae2aab71f57799a31d98c938ee68443f19c
erenzhou <592267829@qq.com>
2021-12-19 19:13:41 +0800

    Update points_in_polygons.cpp

--- mmcv/ops/csrc/pytorch/points_in_polygons.cpp
+++ mmcv/ops/csrc/pytorch/points_in_polygons.cpp
@@ -11,5 +11,5 @@ void points_in_polygons_forward_impl(const Tensor points, const Tensor polygons,
 void points_in_polygons_forward(Tensor points, Tensor polygons, Tensor output) {
   int rows = points.size(0);
   int cols = polygons.size(0);
-  points_in_polygons_all_forward_impl(points, polygons, output, rows, cols);
+  points_in_polygons_forward_impl(points, polygons, output, rows, cols);
 }
