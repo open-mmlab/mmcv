0f4827432b1962ebee153ea1b97e303f69e3ad36
erenzhou <592267829@qq.com>
2021-12-19 20:46:47 +0800

    Update test_points_in_polygons.py

--- tests/test_ops/test_points_in_polygons.py
+++ tests/test_ops/test_points_in_polygons.py
@@ -4,22 +4,19 @@ import torch
 
 from mmcv.ops import points_in_polygons
 
-points = np.array([[300., 300.], [400., 400.], [100., 100], [300, 250],
-                   [100, 0]])
-polygons = np.array([[200., 200., 400., 400., 500., 200., 400., 100.],
-                     [400., 400., 500., 500., 600., 300., 500., 200.],
-                     [300., 300., 600., 700., 700., 700., 700., 100.]])
-
-expert_output = np.array([[0., 0., 0.], [0., 0., 1.], [0., 0., 0.],
-                          [1., 0., 0.], [0., 0., 0.]])
-
-points = torch.from_numpy(points).cuda().float()
-polygons = torch.from_numpy(polygons).cuda().float()
-expert_output = torch.from_numpy(expert_output).cuda().float()
-
 
 @pytest.mark.skipif(
     not torch.cuda.is_available(), reason='requires CUDA support')
 def test_points_in_polygons():
+    points = np.array([[300., 300.], [400., 400.], [100., 100], [300, 250],
+                       [100, 0]])
+    polygons = np.array([[200., 200., 400., 400., 500., 200., 400., 100.],
+                         [400., 400., 500., 500., 600., 300., 500., 200.],
+                         [300., 300., 600., 700., 700., 700., 700., 100.]])
+    expert_output = np.array([[0., 0., 0.], [0., 0., 1.], [0., 0., 0.],
+                              [1., 0., 0.], [0., 0., 0.]])
+    points = torch.from_numpy(points).cuda().float()
+    polygons = torch.from_numpy(polygons).cuda().float()
+    expert_output = torch.from_numpy(expert_output).cuda().float()
     assert torch.allclose(
         points_in_polygons(points, polygons), expert_output, 1e-3)
