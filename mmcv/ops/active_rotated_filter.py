be72bd741dc6d6ca254bd670e56ce15741f77cf6
erenzhou <592267829@qq.com>
2021-12-19 16:24:50 +0800

    add  active_rotated_filter

---
+++ mmcv/ops/active_rotated_filter.py
@@ -0,0 +1,60 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from torch.autograd import Function
+from torch.autograd.function import once_differentiable
+
+from ..utils import ext_loader
+
+ext_module = ext_loader.load_ext(
+    '_ext',
+    ['active_rotated_filter_forward', 'active_rotated_filter_backward'])
+
+
+class ActiveRotatedFilterFunction(Function):
+    """Encoding the orientation information and then produces orientation-
+    sensitive and orientation-invariant features to alleviate the inconsistency
+    between classification score and localization accuracy.
+
+    The details are described in the paper `Align Deep Features for Oriented
+    Object Detection  <https://arxiv.org/abs/2008.09397>`.
+    """
+
+    @staticmethod
+    def forward(ctx, input, indices):
+        """
+        Args:
+            input (torch.Tensor): Input features with shape
+                [nOutputPlane, nInputPlane, nOrientatio, H, W].
+            indices (torch.Tensor): Indices with shape
+                [nOrientation, H, W, nRotation].
+        Returns:
+            torch.Tensor: Refined features with shape
+                [nOutputPlane * nRotation, nInputPlane * nOrientation, H, W].
+        """
+        ctx.save_for_backward(input, indices)
+        op, ip, o, h, w = input.size()
+        o, h, w, r = indices.size()
+        output = input.new_zeros((op * r, ip * o, h, w))
+        ext_module.active_rotated_filter_forward(input, indices, output)
+
+        return output
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_out):
+        """
+        Args:
+            grad_output (torch.Tensor): The gradiant of output features
+                with shape [nOutputPlane * nRotation,
+                nInputPlane * nOrientation, H, W].
+        Returns:
+            torch.Tensor: The gradiant of input features with shape
+                [nOutputPlane, nInputPlane, nOrientatio, H, W].
+        """
+        input, indices = ctx.saved_tensors
+        grad_in = torch.zeros_like(input)
+        ext_module.active_rotated_filter_backward(grad_out, indices, grad_in)
+        return grad_in, None
+
+
+active_rotated_filter = ActiveRotatedFilterFunction.apply
