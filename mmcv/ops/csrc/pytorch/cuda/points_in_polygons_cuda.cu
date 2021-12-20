31059519abbd78775111dfb9200ca6eb64935c48
erenzhou <592267829@qq.com>
2021-12-20 17:24:50 +0800

    add CUDAGuard and stream

--- mmcv/ops/csrc/pytorch/cuda/points_in_polygons_cuda.cu
+++ mmcv/ops/csrc/pytorch/cuda/points_in_polygons_cuda.cu
@@ -12,6 +12,8 @@ void PointsInPolygonsForwardCUDAKernelLauncher(const at::Tensor points,
                                                const int rows, const int cols,
                                                at::Tensor output) {
   const int output_size = rows * cols;
+  at::cuda::CUDAGuard device_guard(points.device());
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
       points.scalar_type(), "PointsInPolygonsLaucher", ([&] {
         const scalar_t *vertex1 = points.data_ptr<scalar_t>();
@@ -19,7 +21,7 @@ void PointsInPolygonsForwardCUDAKernelLauncher(const at::Tensor points,
         scalar_t *inside_flag = output.data_ptr<scalar_t>();
 
         points_in_polygons_forward_cuda_kernel<scalar_t>
-            <<<GET_BLOCKS(output_size), THREADS_PER_BLOCK>>>(
+            <<<GET_BLOCKS(output_size), THREADS_PER_BLOCK, 0, stream>>>(
                 output_size, vertex1, vertex2, rows, cols, inside_flag);
       }));
   AT_CUDA_CHECK(cudaGetLastError());
