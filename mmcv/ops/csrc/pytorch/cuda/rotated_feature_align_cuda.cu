80239f18ee0ff22dc79105506e070dd91c251b81
erenzhou <592267829@qq.com>
2021-12-21 17:03:18 +0800

    replace all the FR with RotatedFeatureAlign

--- mmcv/ops/csrc/pytorch/cuda/rotated_feature_align_cuda.cu
+++ mmcv/ops/csrc/pytorch/cuda/rotated_feature_align_cuda.cu
@@ -4,14 +4,14 @@
 #include "rotated_feature_align_cuda_kernel.cuh"
 #include "pytorch_cuda_helper.hpp"
 
-void FRForwardLauncher(const Tensor features, const Tensor best_bboxes,
+void RotatedFeatureAlignForwardLauncher(const Tensor features, const Tensor best_bboxes,
                        const float spatial_scale, const int points,
                        Tensor output) {
   at::cuda::CUDAGuard device_guard(features.device());
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   const int output_size = features.numel();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
-      features.scalar_type(), "FRForwardLaucherFun", ([&] {
+      features.scalar_type(), "RotatedFeatureAlignForwardLaucherFun", ([&] {
         const scalar_t* bottom_data = features.data_ptr<scalar_t>();
         const scalar_t* bboxes_data = best_bboxes.data_ptr<scalar_t>();
         scalar_t* top_data = output.data_ptr<scalar_t>();
@@ -25,14 +25,14 @@ void FRForwardLauncher(const Tensor features, const Tensor best_bboxes,
   AT_CUDA_CHECK(cudaGetLastError());
 }
 
-void FRBackwardLauncher(const Tensor top_grad, const Tensor best_bboxes,
+void RotatedFeatureAlignBackwardLauncher(const Tensor top_grad, const Tensor best_bboxes,
                         const float spatial_scale, const int points,
                         Tensor bottom_grad) {
   at::cuda::CUDAGuard device_guard(top_grad.device());
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   const int output_size = top_grad.numel();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
-      top_grad.scalar_type(), "FRBackwardLaucherFun", ([&] {
+      top_grad.scalar_type(), "RotatedFeatureAlignBackwardLaucherFun", ([&] {
         const scalar_t* top_diff = top_grad.data_ptr<scalar_t>();
         const scalar_t* bboxes_data = best_bboxes.data_ptr<scalar_t>();
         scalar_t* bottom_diff = bottom_grad.data_ptr<scalar_t>();
