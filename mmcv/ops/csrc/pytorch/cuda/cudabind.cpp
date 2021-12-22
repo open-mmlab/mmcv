42c98a1fcf8e384da3148aa9d8bb127275f7317f
zhouyue <592267829@qq.com>
2021-12-22 15:14:09 +0800

    Update mmcv/ops/csrc/pytorch/cuda/cudabind.cpp
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmcv/ops/csrc/pytorch/cuda/cudabind.cpp
+++ mmcv/ops/csrc/pytorch/cuda/cudabind.cpp
@@ -1364,7 +1364,7 @@ REGISTER_DEVICE_IMPL(hard_voxelize_forward_impl, CUDA,
 REGISTER_DEVICE_IMPL(dynamic_voxelize_forward_impl, CUDA,
                      dynamic_voxelize_forward_cuda);
 
-void FRForwardLauncher(const Tensor features, const Tensor best_bboxes,
+void RotatedFeatureAlignForwardLauncher(const Tensor features, const Tensor best_bboxes,
                        const float spatial_scale, const int points,
                        Tensor output);
 
