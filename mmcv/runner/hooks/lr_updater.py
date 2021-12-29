073cd00a3475f8531a7dc749dbd4102112f7e97f
Sharpiless <1691608003@qq.com>
2021-12-29 12:03:05 +0800

    update support for linearly learning rate decay

--- mmcv/runner/hooks/lr_updater.py
+++ mmcv/runner/hooks/lr_updater.py
@@ -644,6 +644,34 @@ class OneCycleLrUpdaterHook(LrUpdaterHook):
         return lr
 
 
+@HOOKS.register_module()
+class LinearlyDecayLrUpdaterHook(LrUpdaterHook):
+
+    def __init__(self, min_lr=None, min_lr_ratio=None, **kwargs):
+        assert (min_lr is None) ^ (min_lr_ratio is None)
+        self.min_lr = min_lr
+        self.min_lr_ratio = min_lr_ratio
+        super(LinearlyDecayLrUpdaterHook, self).__init__(**kwargs)
+
+    def get_lr(self, runner, base_lr):
+        if self.by_epoch:
+            warmup_epochs = self.warmup_epochs
+            progress = runner.epoch
+            max_progress = runner.max_epochs
+            ratio = (progress-warmup_epochs) / (max_progress-warmup_epochs)
+        else:
+            warmup_iters = self.warmup_iters
+            progress = runner.iter
+            max_progress = runner.max_iters
+            ratio = (progress-warmup_iters) / (max_progress-warmup_iters)
+
+        if self.min_lr_ratio is not None:
+            target_lr = base_lr * self.min_lr_ratio
+        else:
+            target_lr = self.min_lr
+        return annealing_linear(base_lr, target_lr, ratio)
+
+
 def annealing_cos(start, end, factor, weight=1):
     """Calculate annealing cos learning rate.
 
