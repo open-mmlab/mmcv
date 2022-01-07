043f3cb1b4076715fb39fba41143271f5c39bff7
crazysboy <45088252+crazysboy@users.noreply.github.com>
2022-01-07 11:44:44 +0800

    Add files via upload

--- mmcv/ops/csrc/pytorch/pybind.cpp
+++ mmcv/ops/csrc/pytorch/pybind.cpp
@@ -54,6 +54,30 @@ void deform_conv_backward_parameters(Tensor input, Tensor offset,
                                      int deformable_group, float scale,
                                      int im2col_step);
 
+
+void pyramid_deform_conv_forward(Tensor input, Tensor weight, Tensor offset,
+                         Tensor output, Tensor columns, Tensor ones, int kW,
+                         int kH, int dW, int dH, int padW, int padH,
+                         int dilationW, int dilationH, float scaleW, float scaleH,int group,
+                         int deformable_group, int im2col_step);
+
+void pyramid_deform_conv_backward_input(Tensor input, Tensor offset, Tensor gradOutput,
+                                Tensor gradInput, Tensor gradOffset,
+                                Tensor weight, Tensor columns, int kW, int kH,
+                                int dW, int dH, int padW, int padH,
+                                int dilationW, int dilationH, float scaleW, float scaleH,int group,
+                                int deformable_group, int im2col_step);
+
+void pyramid_deform_conv_backward_parameters(Tensor input, Tensor offset,
+                                     Tensor gradOutput, Tensor gradWeight,
+                                     Tensor columns, Tensor ones, int kW,
+                                     int kH, int dW, int dH, int padW, int padH,
+                                     int dilationW, int dilationH,float scaleW, float scaleH, int group,
+                                     int deformable_group, float scale,
+                                     int im2col_step);
+
+
+
 void deform_roi_pool_forward(Tensor input, Tensor rois, Tensor offset,
                              Tensor output, int pooled_height, int pooled_width,
                              float spatial_scale, int sampling_ratio,
@@ -434,6 +458,27 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
         py::arg("ones"), py::arg("kW"), py::arg("kH"), py::arg("dW"),
         py::arg("dH"), py::arg("padH"), py::arg("padW"), py::arg("dilationW"),
         py::arg("dilationH"), py::arg("group"), py::arg("deformable_group"),
+        py::arg("scale"), py::arg("im2col_step"));
+
+  m.def("pyramid_deform_conv_forward", &pyramid_deform_conv_forward, "pyramid_deform_conv_forward",
+        py::arg("input"), py::arg("weight"), py::arg("offset"),
+        py::arg("output"), py::arg("columns"), py::arg("ones"), py::arg("kW"),
+        py::arg("kH"), py::arg("dW"), py::arg("dH"), py::arg("padH"),
+        py::arg("padW"), py::arg("dilationW"), py::arg("dilationH"),py::arg("scaleW"), py::arg("scaleH"),
+        py::arg("group"), py::arg("deformable_group"), py::arg("im2col_step"));
+  m.def("pyramid_deform_conv_backward_input", &pyramid_deform_conv_backward_input,
+        "pyramid_deform_conv_backward_input", py::arg("input"), py::arg("offset"),
+        py::arg("gradOutput"), py::arg("gradInput"), py::arg("gradOffset"),
+        py::arg("weight"), py::arg("columns"), py::arg("kW"), py::arg("kH"),
+        py::arg("dW"), py::arg("dH"), py::arg("padH"), py::arg("padW"),
+        py::arg("dilationW"), py::arg("dilationH"), py::arg("scaleW"), py::arg("scaleH"),py::arg("group"),
+        py::arg("deformable_group"), py::arg("im2col_step"));
+  m.def("pyramid_deform_conv_backward_parameters", &pyramid_deform_conv_backward_parameters,
+        "pyramid_deform_conv_backward_parameters", py::arg("input"), py::arg("offset"),
+        py::arg("gradOutput"), py::arg("gradWeight"), py::arg("columns"),
+        py::arg("ones"), py::arg("kW"), py::arg("kH"), py::arg("dW"),
+        py::arg("dH"), py::arg("padH"), py::arg("padW"), py::arg("dilationW"),
+        py::arg("dilationH"),py::arg("scaleW"), py::arg("scaleH"), py::arg("group"), py::arg("deformable_group"),
         py::arg("scale"), py::arg("im2col_step"));
   m.def("deform_roi_pool_forward", &deform_roi_pool_forward,
         "deform roi pool forward", py::arg("input"), py::arg("rois"),
