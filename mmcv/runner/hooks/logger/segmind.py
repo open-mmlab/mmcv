1c468ff75025d62eedb8fed1c54b4744825ff4f9
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-01-09 16:55:43 +0000

    update linting for segmind.py

--- mmcv/runner/hooks/logger/segmind.py
+++ mmcv/runner/hooks/logger/segmind.py
@@ -11,7 +11,8 @@ class SegmindLoggerHook(LoggerHook):
     def __init__(
         self, init_kwargs=None, interval=10, ignore_last=True, reset_flag=True
     ):
-        super(SegmindLoggerHook, self).__init__(interval, ignore_last, reset_flag)
+        super(SegmindLoggerHook, self).__init__(
+            interval, ignore_last, reset_flag)
         self.import_segmind()
 
     def import_segmind(self):
@@ -20,8 +21,11 @@ class SegmindLoggerHook(LoggerHook):
             from segmind.tracking.fluent import log_metrics
             from segmind.utils.logging_utils import try_mlflow_log
         except ImportError:
-            raise ImportError('Please run "pip install segmind" to install segmind')
+            raise ImportError("Please run 'pip install segmind' to install \
+                segmind")
         self.segmind = segmind
+        self.log_metrics = log_metrics
+        self.try_mlflow_log = try_mlflow_log
 
     @master_only
     def before_run(self, runner):
@@ -45,7 +49,12 @@ class SegmindLoggerHook(LoggerHook):
         metrics["momentum"] = runner.current_momentum()[0]
 
         # logging metrics to segmind
-        try_mlflow_log(log_metrics, metrics, step=runner.epoch, epoch=runner.epoch)
+        self.try_mlflow_log(
+            self.log_metrics,
+            metrics,
+            step=runner.epoch,
+            epoch=runner.epoch
+        )
 
     @master_only
     def after_run(self, runner):
