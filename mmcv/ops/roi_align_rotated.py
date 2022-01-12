0134aac44f5f4b89413079565e5b47aeaf10cbe2
nijkah <nijkah@gmail.com>
2022-01-13 04:49:33 +0900

    fix interface for RoIAlignRotated

--- mmcv/ops/roi_align_rotated.py
+++ mmcv/ops/roi_align_rotated.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch.nn as nn
 from torch.autograd import Function
+from torch.nn.modules.utils import _pair
 
-from ..utils import ext_loader
+from ..utils import deprecated_api_warning, ext_loader
 
 ext_module = ext_loader.load_ext(
     '_ext', ['roi_align_rotated_forward', 'roi_align_rotated_backward'])
@@ -11,27 +12,27 @@ ext_module = ext_loader.load_ext(
 class RoIAlignRotatedFunction(Function):
 
     @staticmethod
-    def symbolic(g, features, rois, out_size, spatial_scale, sample_num,
+    def symbolic(g, input, rois, output_size, spatial_scale, sampling_ratio,
                  aligned, clockwise):
-        if isinstance(out_size, int):
-            out_h = out_size
-            out_w = out_size
-        elif isinstance(out_size, tuple):
-            assert len(out_size) == 2
-            assert isinstance(out_size[0], int)
-            assert isinstance(out_size[1], int)
-            out_h, out_w = out_size
+        if isinstance(output_size, int):
+            out_h = output_size
+            out_w = output_size
+        elif isinstance(output_size, tuple):
+            assert len(output_size) == 2
+            assert isinstance(output_size[0], int)
+            assert isinstance(output_size[1], int)
+            out_h, out_w = output_size
         else:
             raise TypeError(
-                '"out_size" must be an integer or tuple of integers')
+                '"output_size" must be an integer or tuple of integers')
         return g.op(
             'mmcv::MMCVRoIAlignRotated',
-            features,
+            input,
             rois,
             output_height_i=out_h,
             output_width_i=out_h,
             spatial_scale_f=spatial_scale,
-            sampling_ratio_i=sample_num,
+            sampling_ratio_i=sampling_ratio,
             aligned_i=aligned,
             clockwise_i=clockwise)
 
@@ -156,22 +157,37 @@ class RoIAlignRotated(nn.Module):
         performance if ROIAlign is used together with conv layers.
     """
 
+    @deprecated_api_warning(
+        {
+            'out_size': 'output_size',
+            'sample_num': 'sampling_ratio'
+        },
+        cls_name='RoIAlignRotated')
     def __init__(self,
-                 out_size,
+                 output_size,
                  spatial_scale,
-                 sample_num=0,
+                 sampling_ratio=0,
                  aligned=True,
                  clockwise=False):
         super(RoIAlignRotated, self).__init__()
 
-        self.out_size = out_size
+        self.output_size = _pair(output_size)
         self.spatial_scale = float(spatial_scale)
-        self.sample_num = int(sample_num)
+        self.sampling_ratio = int(sampling_ratio)
         self.aligned = aligned
         self.clockwise = clockwise
 
-    def forward(self, features, rois):
-        return RoIAlignRotatedFunction.apply(features, rois, self.out_size,
+    def forward(self, input, rois):
+        return RoIAlignRotatedFunction.apply(input, rois, self.output_size,
                                              self.spatial_scale,
-                                             self.sample_num, self.aligned,
+                                             self.sampling_ratio, self.aligned,
                                              self.clockwise)
+
+    def __repr__(self):
+        s = self.__class__.__name__
+        s += f'(output_size={self.output_size}, '
+        s += f'spatial_scale={self.spatial_scale}, '
+        s += f'sampling_ratio={self.sampling_ratio}, '
+        s += f'aligned={self.aligned}, '
+        s += f'clockwise={self.clockwise})'
+        return s
