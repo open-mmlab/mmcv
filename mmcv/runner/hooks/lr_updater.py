3c8376abfaed6d7207291b93e9f817f77e78ed42
HAOCHENYE <21724054@zju.edu.cn>
2022-01-20 15:43:05 +0800

    Fix LinearAnnealingLrUpdaterHook, update LinearAnnealingMomentumUpdaterHook, add unit test
    add docstring
    
    add docstring
    
    update linear lr momentum schedule test
    
    fix ci
    
    Fix CI

--- mmcv/runner/hooks/lr_updater.py
+++ mmcv/runner/hooks/lr_updater.py
@@ -256,6 +256,14 @@ class InvLrUpdaterHook(LrUpdaterHook):
 
 @HOOKS.register_module()
 class CosineAnnealingLrUpdaterHook(LrUpdaterHook):
+    """CosineAnnealing LR scheduler.
+
+    Args:
+        min_lr (float, optional): The minimum lr. Default: None.
+        min_lr_ratio (float, optional): The ratio of minimum lr to the base lr.
+            Either `min_lr` or `min_lr_ratio` should be specified.
+            Default: None.
+    """
 
     def __init__(self, min_lr=None, min_lr_ratio=None, **kwargs):
         assert (min_lr is None) ^ (min_lr_ratio is None)
@@ -645,31 +653,35 @@ class OneCycleLrUpdaterHook(LrUpdaterHook):
 
 
 @HOOKS.register_module()
-class LinearlyDecayLrUpdaterHook(LrUpdaterHook):
+class LinearAnnealingLrUpdaterHook(LrUpdaterHook):
+    """Linear annealing LR Scheduler decays the learning rate of each parameter
+    group linearly.
+
+    Args:
+        min_lr (float, optional): The minimum lr. Default: None.
+        min_lr_ratio (float, optional): The ratio of minimum lr to the base lr.
+            Either `min_lr` or `min_lr_ratio` should be specified.
+            Default: None.
+    """
 
     def __init__(self, min_lr=None, min_lr_ratio=None, **kwargs):
         assert (min_lr is None) ^ (min_lr_ratio is None)
         self.min_lr = min_lr
         self.min_lr_ratio = min_lr_ratio
-        super(LinearlyDecayLrUpdaterHook, self).__init__(**kwargs)
+        super(LinearAnnealingLrUpdaterHook, self).__init__(**kwargs)
 
     def get_lr(self, runner, base_lr):
         if self.by_epoch:
-            warmup_epochs = self.warmup_epochs
             progress = runner.epoch
             max_progress = runner.max_epochs
-            ratio = (progress-warmup_epochs) / (max_progress-warmup_epochs)
         else:
-            warmup_iters = self.warmup_iters
             progress = runner.iter
             max_progress = runner.max_iters
-            ratio = (progress-warmup_iters) / (max_progress-warmup_iters)
-
         if self.min_lr_ratio is not None:
             target_lr = base_lr * self.min_lr_ratio
         else:
             target_lr = self.min_lr
-        return annealing_linear(base_lr, target_lr, ratio)
+        return annealing_linear(base_lr, target_lr, progress / max_progress)
 
 
 def annealing_cos(start, end, factor, weight=1):
