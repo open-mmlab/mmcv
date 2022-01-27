b8ab4fd0e0b87b92a03c05b59ced0f166b87cf4f
budefei <budefei@cambricon.com>
2022-01-27 16:01:15 +0800

    [Feature] Support tin_shift with cambricon MLU backend

--- mmcv/ops/csrc/pytorch/tin_shift.cpp
+++ mmcv/ops/csrc/pytorch/tin_shift.cpp
@@ -19,6 +19,24 @@ void tin_shift_backward_cuda(Tensor grad_output, Tensor shift,
 
 #endif
 
+#ifdef MMCV_WITH_MLU
+void TINShiftForwardMLUKernelLauncher(Tensor input, Tensor shift,
+                                      Tensor output);
+
+void TINShiftBackwardMLUKernelLauncher(Tensor grad_output, Tensor shift,
+                                       Tensor grad_input);
+
+void tin_shift_forward_mlu(Tensor input, Tensor shift, Tensor output) {
+  TINShiftForwardMLUKernelLauncher(input, shift, output);
+}
+
+void tin_shift_backward_mlu(Tensor grad_output, Tensor shift,
+                            Tensor grad_input) {
+  TINShiftBackwardMLUKernelLauncher(grad_output, shift, grad_input);
+}
+
+#endif
+
 void tin_shift_forward(Tensor input, Tensor shift, Tensor output) {
   if (input.device().is_cuda()) {
 #ifdef MMCV_WITH_CUDA
@@ -29,6 +47,14 @@ void tin_shift_forward(Tensor input, Tensor shift, Tensor output) {
     tin_shift_forward_cuda(input, shift, output);
 #else
     AT_ERROR("TINShift is not compiled with GPU support");
+#endif
+#ifdef MMCV_WITH_MLU
+  } else if (input.device().type() == at::kMLU) {
+    CHECK_MLU_INPUT(input);
+    CHECK_MLU_INPUT(shift);
+    CHECK_MLU_INPUT(output);
+
+    tin_shift_forward_mlu(input, shift, output);
 #endif
   } else {
     AT_ERROR("TINShift is not implemented on CPU");
@@ -45,6 +71,14 @@ void tin_shift_backward(Tensor grad_output, Tensor shift, Tensor grad_input) {
     tin_shift_backward_cuda(grad_output, shift, grad_input);
 #else
     AT_ERROR("TINShift is not compiled with GPU support");
+#endif
+#ifdef MMCV_WITH_MLU
+  } else if (grad_output.device().type() == at::kMLU) {
+    CHECK_MLU_INPUT(grad_output);
+    CHECK_MLU_INPUT(shift);
+    CHECK_MLU_INPUT(grad_input);
+
+    tin_shift_backward_mlu(grad_output, shift, grad_input);
 #endif
   } else {
     AT_ERROR("TINShift is not implemented on CPU");
