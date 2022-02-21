2d846fb9daee0e00adfb1b2b8502f51cf2c7f383
Yining Li <liyining0712@gmail.com>
2022-02-22 00:28:42 +0800

    [Feature] Add base transform interface (#1538)
    * Support deepcopy for Config (#1658)
    
    * Support deepcopy for Config
    
    * Iterate the `__dict__` of Config directly.
    
    * Use __new__ to avoid unnecessary initialization.
    
    * Improve according to comments
    
    * [Feature] Add spconv ops from mmdet3d (#1581)
    
    * add ops (spconv) of mmdet3d
    
    * fix typo
    
    * refactor code
    
    * resolve comments in #1452
    
    * fix compile error
    
    * fix bugs
    
    * fix bug
    
    * transform from 'types.h' to 'extension.h'
    
    * fix bug
    
    * transform from 'types.h' to 'extension.h' in parrots
    
    * add extension.h in pybind.cpp
    
    * add unittest
    
    * Recover code
    
    * (1) Remove prettyprint.h
    (2) Switch `T` to `scalar_t`
    (3) Remove useless lines
    (4) Refine example in docstring of sparse_modules.py
    
    * (1) rename from `cu.h` to `cuh`
    (2) remove useless files
    (3) move cpu files to `pytorch/cpu`
    
    * reorganize files
    
    * Add docstring for sparse_functional.py
    
    * use dispatcher
    
    * remove template
    
    * use dispatch in cuda ops
    
    * resolve Segmentation fault
    
    * remove useless files
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix unittest in test_build_layers.py
    
    * add tensorview into include_dirs when compiling
    
    * recover all deleted files
    
    * fix lint and comments
    
    * recover setup.py
    
    * replace tv::GPU as tv::TorchGPU & support device guard
    
    * fix lint
    
    Co-authored-by: hdc <hudingchang.vendor@sensetime.com>
    Co-authored-by: grimoire <yaoqian@sensetime.com>
    
    * Imporve the docstring of imfrombytes and fix a deprecation-warning (#1731)
    
    * [Refactor] Refactor the interface for RoIAlignRotated (#1662)
    
    * fix interface for RoIAlignRotated
    
    * Add a unit test for RoIAlignRotated
    
    * Make a unit test for RoIAlignRotated concise
    
    * fix interface for RoIAlignRotated
    
    * Refactor ext_module.nms_rotated
    
    * Lint cpp files
    
    * add transforms
    
    * add invoking time check for cacheable methods
    
    * fix lint
    
    * add unittest
    
    * fix bug in non-strict input mapping
    
    * fix ci
    
    * fix ci
    
    * fix compatibility with python<3.9
    
    * fix typing compatibility
    
    * fix import
    
    * fix typing
    
    * add alternative for nullcontext
    
    * fix import
    
    * fix import
    
    * add docstrings
    
    * add docstrings
    
    * fix callable check
    
    * resolve comments
    
    * fix lint
    
    * enrich unittest cases
    
    * fix lint
    
    * fix unittest
    
    Co-authored-by: Ma Zerun <mzr1996@163.com>
    Co-authored-by: Wenhao Wu <79644370+wHao-Wu@users.noreply.github.com>
    Co-authored-by: hdc <hudingchang.vendor@sensetime.com>
    Co-authored-by: grimoire <yaoqian@sensetime.com>
    Co-authored-by: Jiazhen Wang <47851024+teamwong111@users.noreply.github.com>
    Co-authored-by: Hakjin Lee <nijkah@gmail.com>

---
+++ mmcv/transform/base.py
@@ -0,0 +1,27 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from abc import ABCMeta, abstractmethod
+from typing import Dict
+
+
+class BaseTransform(metaclass=ABCMeta):
+
+    def __call__(self, results: Dict) -> Dict:
+
+        return self.transform(results)
+
+    @abstractmethod
+    def transform(self, results: Dict) -> Dict:
+        """The transform function. All subclass of BaseTransform should
+        override this method.
+
+        This function takes the result dict as the input, and can add new
+        items to the dict or modify existing items in the dict. And the result
+        dict will be returned in the end, which allows to concate multiple
+        transforms into a pipeline.
+
+        Args:
+            results (dict): The result dict.
+
+        Returns:
+            dict: The result dict.
+        """
