f173083c27a65db96eaf3cea3416e6fc9b90627b
Yining Li <liyining0712@gmail.com>
2022-02-22 00:28:42 +0800

    [Feature] Add base transform interface (#1538)
    * Support deepcopy for Config (#1658)
    
    * Support deepcopy for Config
    
    * Iterate the `__dict__` of Config directly.
    
    * Use __new__ to avoid unnecessary initialization.
    
    * Improve according to comments
    
    * [Feature] Add spconv ops from mmdet3d (#1581)
    
    * add ops (spconv) of mmdet3d
    
    * fix typo
    
    * refactor code
    
    * resolve comments in #1452
    
    * fix compile error
    
    * fix bugs
    
    * fix bug
    
    * transform from 'types.h' to 'extension.h'
    
    * fix bug
    
    * transform from 'types.h' to 'extension.h' in parrots
    
    * add extension.h in pybind.cpp
    
    * add unittest
    
    * Recover code
    
    * (1) Remove prettyprint.h
    (2) Switch `T` to `scalar_t`
    (3) Remove useless lines
    (4) Refine example in docstring of sparse_modules.py
    
    * (1) rename from `cu.h` to `cuh`
    (2) remove useless files
    (3) move cpu files to `pytorch/cpu`
    
    * reorganize files
    
    * Add docstring for sparse_functional.py
    
    * use dispatcher
    
    * remove template
    
    * use dispatch in cuda ops
    
    * resolve Segmentation fault
    
    * remove useless files
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix unittest in test_build_layers.py
    
    * add tensorview into include_dirs when compiling
    
    * recover all deleted files
    
    * fix lint and comments
    
    * recover setup.py
    
    * replace tv::GPU as tv::TorchGPU & support device guard
    
    * fix lint
    
    Co-authored-by: hdc <hudingchang.vendor@sensetime.com>
    Co-authored-by: grimoire <yaoqian@sensetime.com>
    
    * Imporve the docstring of imfrombytes and fix a deprecation-warning (#1731)
    
    * [Refactor] Refactor the interface for RoIAlignRotated (#1662)
    
    * fix interface for RoIAlignRotated
    
    * Add a unit test for RoIAlignRotated
    
    * Make a unit test for RoIAlignRotated concise
    
    * fix interface for RoIAlignRotated
    
    * Refactor ext_module.nms_rotated
    
    * Lint cpp files
    
    * add transforms
    
    * add invoking time check for cacheable methods
    
    * fix lint
    
    * add unittest
    
    * fix bug in non-strict input mapping
    
    * fix ci
    
    * fix ci
    
    * fix compatibility with python<3.9
    
    * fix typing compatibility
    
    * fix import
    
    * fix typing
    
    * add alternative for nullcontext
    
    * fix import
    
    * fix import
    
    * add docstrings
    
    * add docstrings
    
    * fix callable check
    
    * resolve comments
    
    * fix lint
    
    * enrich unittest cases
    
    * fix lint
    
    * fix unittest
    
    Co-authored-by: Ma Zerun <mzr1996@163.com>
    Co-authored-by: Wenhao Wu <79644370+wHao-Wu@users.noreply.github.com>
    Co-authored-by: hdc <hudingchang.vendor@sensetime.com>
    Co-authored-by: grimoire <yaoqian@sensetime.com>
    Co-authored-by: Jiazhen Wang <47851024+teamwong111@users.noreply.github.com>
    Co-authored-by: Hakjin Lee <nijkah@gmail.com>

---
+++ mmcv/transform/utils.py
@@ -0,0 +1,162 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+import functools
+import inspect
+import weakref
+from collections import defaultdict
+from collections.abc import Iterable
+from contextlib import contextmanager
+from typing import Callable, Union
+
+from .base import BaseTransform
+
+
+class cacheable_method:
+    """Decorator that marks a method of a transform class as a cacheable
+    method.
+
+    This decorator is usually used together with the context-manager
+    :func`:cache_random_params`. In this context, a cacheable method will
+    cache its return value(s) at the first time of being invoked, and always
+    return the cached values when being invoked again.
+
+    .. note::
+        Only a instance method can be decorated as a cacheable_method.
+    """
+
+    def __init__(self, func):
+
+        # Check `func` is to be bound as an instance method
+        if not inspect.isfunction(func):
+            raise TypeError('Unsupport callable to decorate with'
+                            '@cacheable_method.')
+        func_args = inspect.getfullargspec(func).args
+        if len(func_args) == 0 or func_args[0] != 'self':
+            raise TypeError(
+                '@cacheable_method should only be used to decorate '
+                'instance methods (the first argument is `self`).')
+
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.instance_ref = None
+
+    def __set_name__(self, owner, name):
+        # Maintain a record of decorated methods in the class
+        if not hasattr(owner, '_cacheable_methods'):
+            setattr(owner, '_cacheable_methods', [])
+        owner._cacheable_methods.append(self.__name__)
+
+    def __call__(self, *args, **kwargs):
+        # Get the transform instance whose method is decorated
+        # by cacheable_method
+        instance = self.instance_ref()
+        name = self.__name__
+
+        # Check the flag `self._cache_enabled`, which should be
+        # set by the contextmanagers like `cache_random_parameters`
+        cache_enabled = getattr(instance, '_cache_enabled', False)
+
+        if cache_enabled:
+            # Initialize the cache of the transform instances. The flag
+            # `cache_enabled` is set by contextmanagers like
+            # `cache_random_params`.
+            if not hasattr(instance, '_cache'):
+                setattr(instance, '_cache', {})
+
+            if name not in instance._cache:
+                instance._cache[name] = self.func(instance, *args, **kwargs)
+            # Return the cached value
+            return instance._cache[name]
+        else:
+            # Clear cache
+            if hasattr(instance, '_cache'):
+                del instance._cache
+            # Return function output
+            return self.func(instance, *args, **kwargs)
+
+    def __get__(self, obj, cls):
+        self.instance_ref = weakref.ref(obj)
+        return self
+
+
+@contextmanager
+def cache_random_params(transforms: Union[BaseTransform, Iterable]):
+    """Context-manager that enables the cache of cacheable methods in
+    transforms.
+
+    In this mode, cacheable methods will cache their return values on the
+    first invoking, and always return the cached value afterward. This allow
+    to apply random transforms in a deterministic way. For example, apply same
+    transforms on multiple examples. See `cacheable_method` for more
+    information.
+
+    Args:
+        transforms (BaseTransform|list[BaseTransform]): The transforms to
+            enable cache.
+    """
+
+    # key2method stores the original methods that are replaced by the wrapped
+    # ones. These methods will be restituted when exiting the context.
+    key2method = dict()
+
+    # key2counter stores the usage number of each cacheable_method. This is
+    # used to check that any cacheable_method is invoked once during processing
+    # on data sample.
+    key2counter = defaultdict(int)
+
+    def _add_counter(obj, method_name):
+        method = getattr(obj, method_name)
+        key = f'{id(obj)}.{method_name}'
+        key2method[key] = method
+
+        @functools.wraps(method)
+        def wrapped(*args, **kwargs):
+            key2counter[key] += 1
+            return method(*args, **kwargs)
+
+        return wrapped
+
+    def _start_cache(t: BaseTransform):
+        # Set cache enabled flag
+        setattr(t, '_cache_enabled', True)
+
+        # Store the original method and init the counter
+        if hasattr(t, '_cacheable_methods'):
+            setattr(t, 'transform', _add_counter(t, 'transform'))
+            for name in t._cacheable_methods:
+                setattr(t, name, _add_counter(t, name))
+
+    def _end_cache(t: BaseTransform):
+        # Remove cache enabled flag
+        del t._cache_enabled
+        if hasattr(t, '_cache'):
+            del t._cache
+
+        # Restore the original method
+        if hasattr(t, '_cacheable_methods'):
+            key_transform = f'{id(t)}.transform'
+            for name in t._cacheable_methods:
+                key = f'{id(t)}.{name}'
+                if key2counter[key] != key2counter[key_transform]:
+                    raise RuntimeError(
+                        'The cacheable method should be called once and only'
+                        f'once during processing one data sample. {t} got'
+                        f'unmatched number of {key2counter[key]} ({name}) vs'
+                        f'{key2counter[key_transform]} (data samples)')
+                setattr(t, name, key2method[key])
+            setattr(t, 'transform', key2method[key_transform])
+
+    def _apply(t: Union[BaseTransform, Iterable],
+               func: Callable[[BaseTransform], None]):
+        if isinstance(t, BaseTransform):
+            if hasattr(t, '_cacheable_methods'):
+                func(t)
+        if isinstance(t, Iterable):
+            for _t in t:
+                _apply(_t, func)
+
+    try:
+        _apply(transforms, _start_cache)
+        yield
+    finally:
+        _apply(transforms, _end_cache)
---
+++ tests/test_transform/test_transform_wrapper.py
@@ -0,0 +1,370 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import warnings
+
+import numpy as np
+import pytest
+
+from mmcv.transform.base import BaseTransform
+from mmcv.transform.builder import TRANSFORMS
+from mmcv.transform.utils import cache_random_params, cacheable_method
+from mmcv.transform.wrappers import (ApplyToMultiple, Compose, RandomChoice,
+                                     Remap)
+
+
+@TRANSFORMS.register_module()
+class AddToValue(BaseTransform):
+    """Dummy transform to test transform wrappers."""
+
+    def __init__(self, constant_addend=0, use_random_addend=False) -> None:
+        super().__init__()
+        self.constant_addend = constant_addend
+        self.use_random_addend = use_random_addend
+
+    @cacheable_method
+    def get_random_addend(self):
+        return np.random.rand()
+
+    def transform(self, results):
+        augend = results['value']
+
+        if isinstance(augend, list):
+            warnings.warn('value is a list', UserWarning)
+        if isinstance(augend, dict):
+            warnings.warn('value is a dict', UserWarning)
+
+        def _add_to_value(augend, addend):
+            if isinstance(augend, list):
+                return [_add_to_value(v, addend) for v in augend]
+            if isinstance(augend, dict):
+                return {k: _add_to_value(v, addend) for k, v in augend.items()}
+            return augend + addend
+
+        if self.use_random_addend:
+            addend = self.get_random_addend()
+        else:
+            addend = self.constant_addend
+
+        results['value'] = _add_to_value(results['value'], addend)
+        return results
+
+
+@TRANSFORMS.register_module()
+class SumTwoValues(BaseTransform):
+    """Dummy transform to test transform wrappers."""
+
+    def transform(self, results):
+        if 'num_1' in results and 'num_2' in results:
+            results['sum'] = results['num_1'] + results['num_2']
+        else:
+            results['sum'] = np.nan
+        return results
+
+
+def test_compose():
+
+    # Case 1: build from cfg
+    pipeline = [dict(type='AddToValue')]
+    pipeline = Compose(pipeline)
+    _ = str(pipeline)
+
+    # Case 2: build from transform list
+    pipeline = [AddToValue()]
+    pipeline = Compose(pipeline)
+
+    # Case 3: invalid build arguments
+    pipeline = [[dict(type='AddToValue')]]
+    with pytest.raises(TypeError):
+        pipeline = Compose(pipeline)
+
+    # Case 4: contain transform with None output
+    class DummyTransform(BaseTransform):
+
+        def transform(self, results):
+            return None
+
+    pipeline = Compose([DummyTransform()])
+    results = pipeline({})
+    assert results is None
+
+
+def test_cache_random_parameters():
+
+    transform = AddToValue(use_random_addend=True)
+
+    # Case 1: cache random parameters
+    assert hasattr(AddToValue, '_cacheable_methods')
+    assert 'get_random_addend' in AddToValue._cacheable_methods
+
+    with cache_random_params(transform):
+        results_1 = transform(dict(value=0))
+        results_2 = transform(dict(value=0))
+        np.testing.assert_equal(results_1['value'], results_2['value'])
+
+    # Case 2: do not cache random parameters
+    results_1 = transform(dict(value=0))
+    results_2 = transform(dict(value=0))
+    with pytest.raises(AssertionError):
+        np.testing.assert_equal(results_1['value'], results_2['value'])
+
+    # Case 3: invalid use of cacheable methods
+    with pytest.raises(RuntimeError):
+        with cache_random_params(transform):
+            _ = transform.get_random_addend()
+
+    # Case 4: apply on nested transforms
+    transform = Compose([AddToValue(use_random_addend=True)])
+    with cache_random_params(transform):
+        results_1 = transform(dict(value=0))
+        results_2 = transform(dict(value=0))
+        np.testing.assert_equal(results_1['value'], results_2['value'])
+
+
+def test_remap():
+
+    # Case 1: simple remap
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=1)],
+        input_mapping=dict(value='v_in'),
+        output_mapping=dict(value='v_out'))
+
+    results = dict(value=0, v_in=1)
+    results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)  # should be unchanged
+    np.testing.assert_equal(results['v_in'], 1)
+    np.testing.assert_equal(results['v_out'], 2)
+
+    # Case 2: collecting list
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=2)],
+        input_mapping=dict(value=['v_in_1', 'v_in_2']),
+        output_mapping=dict(value=['v_out_1', 'v_out_2']))
+    results = dict(value=0, v_in_1=1, v_in_2=2)
+
+    with pytest.warns(UserWarning, match='value is a list'):
+        results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)  # should be unchanged
+    np.testing.assert_equal(results['v_in_1'], 1)
+    np.testing.assert_equal(results['v_in_2'], 2)
+    np.testing.assert_equal(results['v_out_1'], 3)
+    np.testing.assert_equal(results['v_out_2'], 4)
+
+    # Case 3: collecting dict
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=2)],
+        input_mapping=dict(value=dict(v1='v_in_1', v2='v_in_2')),
+        output_mapping=dict(value=dict(v1='v_out_1', v2='v_out_2')))
+    results = dict(value=0, v_in_1=1, v_in_2=2)
+
+    with pytest.warns(UserWarning, match='value is a dict'):
+        results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)  # should be unchanged
+    np.testing.assert_equal(results['v_in_1'], 1)
+    np.testing.assert_equal(results['v_in_2'], 2)
+    np.testing.assert_equal(results['v_out_1'], 3)
+    np.testing.assert_equal(results['v_out_2'], 4)
+
+    # Case 4: collecting list with inplace mode
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=2)],
+        input_mapping=dict(value=['v_in_1', 'v_in_2']),
+        inplace=True)
+    results = dict(value=0, v_in_1=1, v_in_2=2)
+
+    with pytest.warns(UserWarning, match='value is a list'):
+        results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)
+    np.testing.assert_equal(results['v_in_1'], 3)
+    np.testing.assert_equal(results['v_in_2'], 4)
+
+    # Case 5: collecting dict with inplace mode
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=2)],
+        input_mapping=dict(value=dict(v1='v_in_1', v2='v_in_2')),
+        inplace=True)
+    results = dict(value=0, v_in_1=1, v_in_2=2)
+
+    with pytest.warns(UserWarning, match='value is a dict'):
+        results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)
+    np.testing.assert_equal(results['v_in_1'], 3)
+    np.testing.assert_equal(results['v_in_2'], 4)
+
+    # Case 6: nested collection with inplace mode
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=2)],
+        input_mapping=dict(value=['v1', dict(v2=['v21', 'v22'], v3='v3')]),
+        inplace=True)
+    results = dict(value=0, v1=1, v21=2, v22=3, v3=4)
+
+    with pytest.warns(UserWarning, match='value is a list'):
+        results = pipeline(results)
+
+    np.testing.assert_equal(results['value'], 0)
+    np.testing.assert_equal(results['v1'], 3)
+    np.testing.assert_equal(results['v21'], 4)
+    np.testing.assert_equal(results['v22'], 5)
+    np.testing.assert_equal(results['v3'], 6)
+
+    # Case 7: `strict` must be True if `inplace` is set True
+    with pytest.raises(ValueError):
+        pipeline = Remap(
+            transforms=[AddToValue(constant_addend=2)],
+            input_mapping=dict(value=['v_in_1', 'v_in_2']),
+            inplace=True,
+            strict=False)
+
+    # Case 8: output_map must be None if `inplace` is set True
+    with pytest.raises(ValueError):
+        pipeline = Remap(
+            transforms=[AddToValue(constant_addend=1)],
+            input_mapping=dict(value='v_in'),
+            output_mapping=dict(value='v_out'),
+            inplace=True)
+
+    # Case 9: non-strict input mapping
+    pipeline = Remap(
+        transforms=[SumTwoValues()],
+        input_mapping=dict(num_1='a', num_2='b'),
+        strict=False)
+
+    results = pipeline(dict(a=1, b=2))
+    np.testing.assert_equal(results['sum'], 3)
+
+    results = pipeline(dict(a=1))
+    assert np.isnan(results['sum'])
+
+    # Test basic functions
+    pipeline = Remap(
+        transforms=[AddToValue(constant_addend=1)],
+        input_mapping=dict(value='v_in'),
+        output_mapping=dict(value='v_out'))
+
+    # __iter__
+    for _ in pipeline:
+        pass
+
+    # __repr__
+    _ = str(pipeline)
+
+
+def test_apply_to_multiple():
+
+    # Case 1: apply to list in results
+    pipeline = ApplyToMultiple(
+        transforms=[AddToValue(constant_addend=1)],
+        input_mapping=dict(value='values'),
+        inplace=True)
+    results = dict(values=[1, 2])
+
+    results = pipeline(results)
+
+    np.testing.assert_equal(results['values'], [2, 3])
+
+    # Case 2: apply to multiple keys
+    pipeline = ApplyToMultiple(
+        transforms=[AddToValue(constant_addend=1)],
+        input_mapping=dict(value=['v_1', 'v_2']),
+        inplace=True)
+    results = dict(v_1=1, v_2=2)
+
+    results = pipeline(results)
+
+    np.testing.assert_equal(results['v_1'], 2)
+    np.testing.assert_equal(results['v_2'], 3)
+
+    # Case 3: apply to multiple groups of keys
+    pipeline = ApplyToMultiple(
+        transforms=[SumTwoValues()],
+        input_mapping=dict(num_1=['a_1', 'b_1'], num_2=['a_2', 'b_2']),
+        output_mapping=dict(sum=['a', 'b']))
+
+    results = dict(a_1=1, a_2=2, b_1=3, b_2=4)
+    results = pipeline(results)
+
+    np.testing.assert_equal(results['a'], 3)
+    np.testing.assert_equal(results['b'], 7)
+
+    # Case 4: inconsistent sequence length
+    with pytest.raises(ValueError):
+        pipeline = ApplyToMultiple(
+            transforms=[SumTwoValues()],
+            input_mapping=dict(num_1='list_1', num_2='list_2'))
+
+        results = dict(list_1=[1, 2], list_2=[1, 2, 3])
+        _ = pipeline(results)
+
+    # Case 5: share random parameter
+    pipeline = ApplyToMultiple(
+        transforms=[AddToValue(use_random_addend=True)],
+        input_mapping=dict(value='values'),
+        inplace=True,
+        share_random_params=True,
+    )
+
+    results = dict(values=[0, 0])
+    results = pipeline(results)
+
+    np.testing.assert_equal(results['values'][0], results['values'][1])
+
+    # Test repr
+    _ = str(pipeline)
+
+
+def test_randomchoice():
+
+    # Case 1: given probability
+    pipeline = RandomChoice(
+        pipelines=[[AddToValue(constant_addend=1.0)],
+                   [AddToValue(constant_addend=2.0)]],
+        pipeline_probs=[1.0, 0.0])
+
+    results = pipeline(dict(value=1))
+    np.testing.assert_equal(results['value'], 2.0)
+
+    # Case 1: default probability
+    pipeline = RandomChoice(pipelines=[[AddToValue(
+        constant_addend=1.0)], [AddToValue(constant_addend=2.0)]])
+
+    _ = pipeline(dict(value=1))
+
+
+def test_utils():
+    # Test cacheable_method: normal case
+    class DummyTransform(BaseTransform):
+
+        @cacheable_method
+        def func(self):
+            return np.random.rand()
+
+        def transform(self, results):
+            _ = self.func()
+            return results
+
+    transform = DummyTransform()
+    _ = transform({})
+    with cache_random_params(transform):
+        _ = transform({})
+
+    # Test cacheable_method: invalid function type
+    with pytest.raises(TypeError):
+
+        class DummyTransform():
+
+            @cacheable_method
+            @staticmethod
+            def func():
+                return np.random.rand()
+
+    # Test cacheable_method: invalid function argument list
+    with pytest.raises(TypeError):
+
+        class DummyTransform():
+
+            @cacheable_method
+            def func(cls):
+                return np.random.rand()
