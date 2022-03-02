4e636811ef23e98adfc6e1656d3c08c731c04b7d
Wangjiazhen <841713301@qq.com>
2022-03-02 17:11:44 +0800

    cannel buildextension wrapper

--- setup.py
+++ setup.py
@@ -15,18 +15,7 @@ try:
     else:
         from torch.utils.cpp_extension import BuildExtension
         EXT_TYPE = 'pytorch'
-    if platform.system() != 'Windows':
-
-        class MMCVBuildExtension(BuildExtension):
-
-            def build_extensions(self):
-                if '-Wstrict-prototypes' in self.compiler.compiler_so:
-                    self.compiler.compiler_so.remove('-Wstrict-prototypes')
-                super().build_extensions()
-
-        cmd_class = {'build_ext': MMCVBuildExtension}
-    else:
-        cmd_class = {'build_ext': BuildExtension}
+    cmd_class = {'build_ext': BuildExtension}
 except ModuleNotFoundError:
     cmd_class = {}
     print('Skip building ext ops due to the absence of torch.')
