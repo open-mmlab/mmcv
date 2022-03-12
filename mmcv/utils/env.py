72666bbb5cec1d0409f9892f04464bb1979b8147
苏坦 <sutan@insta360.com>
2022-03-12 16:16:20 +0800

    fix: code style

--- mmcv/utils/env.py
+++ mmcv/utils/env.py
@@ -76,7 +76,8 @@ def collect_env():
             from setuptools._distutils.ccompiler import new_compiler
             ccompiler = new_compiler()
             ccompiler.initialize()
-            cc = subprocess.check_output(f'{ccompiler.cc}', stderr=subprocess.STDOUT, shell=True)
+            cc = subprocess.check_output(
+                f'{ccompiler.cc}', stderr=subprocess.STDOUT, shell=True)
             env_info['CC'] = cc.decode('utf-8').partition('\n')[0].strip()
             del ccompiler
     except subprocess.CalledProcessError:
