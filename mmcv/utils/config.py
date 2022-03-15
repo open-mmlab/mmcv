45db24c396f3970069c4c244b6a66b9af6deab89
苏坦 <sutan@insta360.com>
2022-03-15 12:55:41 +0800

    [fix] config ignore modules and functions

--- mmcv/utils/config.py
+++ mmcv/utils/config.py
@@ -7,6 +7,7 @@ import platform
 import shutil
 import sys
 import tempfile
+import types
 import uuid
 import warnings
 from argparse import Action, ArgumentParser
@@ -209,6 +210,8 @@ class Config:
                     name: value
                     for name, value in mod.__dict__.items()
                     if not name.startswith('__')
+                    and not isinstance(value, types.ModuleType)
+                    and not isinstance(value, types.FunctionType)
                 }
                 # delete imported module
                 del sys.modules[temp_module_name]
