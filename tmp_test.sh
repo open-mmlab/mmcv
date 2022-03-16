b8bfc063f034ab89191dd604817f026f3400c085
Hu Di <476658825@qq.com>
2022-03-16 10:42:11 +0800

    rm tmp_test.sh

--- tmp_test.sh
+++
@@ -1,7 +0,0 @@
-coverage run -p --source mmcv/runner  tests/test_ipu/test_complexdatamanager.py
-coverage run -p --source mmcv/runner tests/test_ipu/test_hooks.py
-coverage run -p --source mmcv/runner tests/test_ipu/test_ipu_model.py
-coverage run -p --source mmcv/runner tests/test_ipu/test_ipu_runner.py
-coverage run -p --source mmcv/runner tests/test_ipu/test_utils.py
-coverage combine
-coverage html && coverage report
