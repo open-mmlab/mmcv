248aa7192ee2c174356b961be6c3f3fd59c3bfc5
Hu Di <476658825@qq.com>
2022-03-23 10:03:38 +0800

    Update mmcv/runner/ipu/__init__.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmcv/runner/ipu/__init__.py
+++ mmcv/runner/ipu/__init__.py
@@ -3,9 +3,9 @@ from mmcv.utils.ipu_wrapper import IPU_MODE
 
 
 if IPU_MODE:
-    from .util import parse_ipu_options, ipu_model_wrapper,\
-        build_from_cfg_with_wrapper, model_sharding,\
-        recomputation_checkpoint
+    from .util import (parse_ipu_options, ipu_model_wrapper,
+        build_from_cfg_with_wrapper, model_sharding,
+        recomputation_checkpoint)
     from .hooks import wrap_optimizer_hook, IPUFp16OptimizerHook,\
         wrap_lr_update_hook
     from .dataloder import IPUDataloader
