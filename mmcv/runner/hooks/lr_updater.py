969e2af866045417dccbc3980422c80d9736d970
Sharpiless <50802787+Sharpiless@users.noreply.github.com>
2022-03-31 09:39:40 +0800

    [Feature] Support for linearly learning rate decay (#1627)
    * update support for linearly learning rate decay
    
    * Fix LinearAnnealingLrUpdaterHook, update LinearAnnealingMomentumUpdaterHook, add unit test
    
    add docstring
    
    add docstring
    
    update linear lr momentum schedule test
    
    fix ci
    
    Fix CI
    
    * add lr and momentum hook to runner and hooks package
    
    add lr and momentum hook to runner and hooks package
    
    * replace multi_optimziers with multi_optimizers
    
    Co-authored-by: HAOCHENYE <21724054@zju.edu.cn>
    Co-authored-by: Mashiro <57566630+HAOCHENYE@users.noreply.github.com>

--- mmcv/runner/hooks/lr_updater.py
+++ mmcv/runner/hooks/lr_updater.py
@@ -256,6 +256,14 @@ class InvLrUpdaterHook(LrUpdaterHook):
 
 @HOOKS.register_module()
 class CosineAnnealingLrUpdaterHook(LrUpdaterHook):
+    """CosineAnnealing LR scheduler.
+
+    Args:
+        min_lr (float, optional): The minimum lr. Default: None.
+        min_lr_ratio (float, optional): The ratio of minimum lr to the base lr.
+            Either `min_lr` or `min_lr_ratio` should be specified.
+            Default: None.
+    """
 
     def __init__(self, min_lr=None, min_lr_ratio=None, **kwargs):
         assert (min_lr is None) ^ (min_lr_ratio is None)
@@ -642,6 +650,38 @@ class OneCycleLrUpdaterHook(LrUpdaterHook):
                 break
             start_iter = end_iter
         return lr
+
+
+@HOOKS.register_module()
+class LinearAnnealingLrUpdaterHook(LrUpdaterHook):
+    """Linear annealing LR Scheduler decays the learning rate of each parameter
+    group linearly.
+
+    Args:
+        min_lr (float, optional): The minimum lr. Default: None.
+        min_lr_ratio (float, optional): The ratio of minimum lr to the base lr.
+            Either `min_lr` or `min_lr_ratio` should be specified.
+            Default: None.
+    """
+
+    def __init__(self, min_lr=None, min_lr_ratio=None, **kwargs):
+        assert (min_lr is None) ^ (min_lr_ratio is None)
+        self.min_lr = min_lr
+        self.min_lr_ratio = min_lr_ratio
+        super(LinearAnnealingLrUpdaterHook, self).__init__(**kwargs)
+
+    def get_lr(self, runner, base_lr):
+        if self.by_epoch:
+            progress = runner.epoch
+            max_progress = runner.max_epochs
+        else:
+            progress = runner.iter
+            max_progress = runner.max_iters
+        if self.min_lr_ratio is not None:
+            target_lr = base_lr * self.min_lr_ratio
+        else:
+            target_lr = self.min_lr
+        return annealing_linear(base_lr, target_lr, progress / max_progress)
 
 
 def annealing_cos(start, end, factor, weight=1):
