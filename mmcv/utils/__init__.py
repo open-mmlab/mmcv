70770a02acea18135d735e55ad762503b9b30000
jzwang <841713301@qq.com>
2022-04-07 12:31:01 +0800

    fix mmcv.utils.__init__

--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -50,7 +50,6 @@ else:
                                   is_rocm_pytorch)
     # yapf: enable
     from .registry import Registry, build_from_cfg
-    from .seed import worker_init_fn
     from .trace import is_jit_tracing
     __all__ = [
         'Config', 'ConfigDict', 'DictAction', 'collect_env', 'get_logger',
@@ -71,5 +70,5 @@ else:
         'assert_dict_has_keys', 'assert_keys_equal', 'assert_is_norm_layer',
         'assert_params_all_zeros', 'check_python_script',
         'is_method_overridden', 'is_jit_tracing', 'is_rocm_pytorch',
-        '_get_cuda_home', 'load_url', 'has_method', 'worker_init_fn'
+        '_get_cuda_home', 'load_url', 'has_method'
     ]
