6c6856c0b7131c02bf7c383903b7c214749b626a
zhouzaida <zhouzaida@163.com>
2022-04-16 15:59:20 +0800

    bump version to v1.5.0

--- setup.py
+++ setup.py
@@ -405,6 +405,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Utilities',
     ],
     url='https://github.com/open-mmlab/mmcv',
