b49e58389169406169c4d2576dcd028ac8e4e234
caoweihan <caoweihan@sensetime.com>
2022-04-20 17:32:57 +0800

    fix is_module_wrapper

--- mmcv/parallel/utils.py
+++ mmcv/parallel/utils.py
@@ -8,7 +8,8 @@ def is_module_wrapper(module):
     The following 3 modules in MMCV (and their subclasses) are regarded as
     module wrappers: DataParallel, DistributedDataParallel,
     MMDistributedDataParallel (the deprecated version). You may add you own
-    module wrapper by registering it to mmcv.parallel.MODULE_WRAPPERS.
+    module wrapper by registering it to mmcv.parallel.MODULE_WRAPPERS or
+    its child registry.
 
     Args:
         module (nn.Module): The module to be checked.
@@ -16,5 +17,14 @@ def is_module_wrapper(module):
     Returns:
         bool: True if the input module is a module wrapper.
     """
-    module_wrappers = tuple(MODULE_WRAPPERS.module_dict.values())
-    return isinstance(module, module_wrappers)
+
+    def dfs(MODULE_WRAPPER):
+        module_wrappers = tuple(MODULE_WRAPPER.module_dict.values())
+        if isinstance(module, module_wrappers):
+            return True
+        for CHILD_MODULE_WRAPPER in MODULE_WRAPPER.children.values():
+            if dfs(CHILD_MODULE_WRAPPER):
+                return True
+        return False
+
+    return dfs(MODULE_WRAPPERS)
