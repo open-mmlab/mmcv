f9a5ee2241ae1c047c05f96d3a909632f1cc4f00
akravchuk97 <a.kravchuk97@yandex.ru>
2022-04-22 16:31:45 +0700

    added clearml logger support

--- mmcv/runner/hooks/logger/__init__.py
+++ mmcv/runner/hooks/logger/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import LoggerHook
+from .clearml import ClearMLLoggerHook
 from .dvclive import DvcliveLoggerHook
 from .mlflow import MlflowLoggerHook
 from .neptune import NeptuneLoggerHook
@@ -10,7 +11,14 @@ from .text import TextLoggerHook
 from .wandb import WandbLoggerHook
 
 __all__ = [
-    'LoggerHook', 'MlflowLoggerHook', 'PaviLoggerHook',
-    'TensorboardLoggerHook', 'TextLoggerHook', 'WandbLoggerHook',
-    'NeptuneLoggerHook', 'DvcliveLoggerHook', 'SegmindLoggerHook'
+    'LoggerHook',
+    'MlflowLoggerHook',
+    'PaviLoggerHook',
+    'TensorboardLoggerHook',
+    'TextLoggerHook',
+    'WandbLoggerHook',
+    'NeptuneLoggerHook',
+    'DvcliveLoggerHook',
+    'SegmindLoggerHook',
+    'ClearMLLoggerHook',
 ]
