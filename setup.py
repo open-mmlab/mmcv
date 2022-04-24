97a665173fd65dfea1197f62441da6edf606f883
rolfma <rolfma@163.com>
2022-04-24 13:37:52 +0800

    fix the lint check

--- setup.py
+++ setup.py
@@ -389,24 +389,27 @@ def get_extensions():
 
     return extensions
 
+
 def create_compile_commands(path='build'):
     path = os.path.realpath(path)
     os.makedirs(path, exist_ok=True)
     ninja_files = []
     for root, dirs, files in os.walk('./'):
         for file in files:
-            if file == "build.ninja":
+            if file == 'build.ninja':
                 ninja_files.append(os.path.join(root, file))
-    if(len(ninja_files)==1):
+    if (len(ninja_files) == 1):
         real_path = os.path.realpath(ninja_files[0])
 
-        output_path = os.path.join(path, "compile_commands.json")
-        result = os.popen(f"ninja -f {real_path} -t compdb")
+        output_path = os.path.join(path, 'compile_commands.json')
+        result = os.popen(f'ninja -f {real_path} -t compdb')
         with open(output_path, 'w') as f:
             f.write(result.read())
-        print(f"Saved compile_commands.json at {os.path.relpath(output_path)}")
+        print(f'Saved compile_commands.json at {os.path.relpath(output_path)}')
     else:
-        print("Failed to create compile_commands.json, the number of 'build.ninja' file must be 1.")
+        print('Failed to create compile_commands.json, '
+              'the number of `build.ninja` file must be 1.')
+
 
 setup(
     name='mmcv' if os.getenv('MMCV_WITH_OPS', '0') == '0' else 'mmcv-full',
