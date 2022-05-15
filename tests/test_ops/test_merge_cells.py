e05e79323cc7b844b2455013c197cfe08ee63eb4
JarvisKevin <39183050+JarvisKevin@users.noreply.github.com>
2022-05-15 11:08:26 +0800

    Update test_merge_cells.py
    Mainly test the downsampling resize in BaseMergeCell. The smaller target size is set to (14, 7), the classical feature map's size in the last few stages of the backbone, which will product different downsampling scales in different dims.

--- tests/test_ops/test_merge_cells.py
+++ tests/test_ops/test_merge_cells.py
@@ -1,4 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 """
 CommandLine:
     pytest tests/test_merge_cells.py
@@ -11,7 +10,7 @@ from mmcv.ops.merge_cells import (BaseMergeCell, ConcatCell, GlobalPoolingCell,
 
 
 def test_sum_cell():
-    inputs_x = torch.randn([2, 256, 32, 32])
+    inputs_x = torch.randn([2, 256, 14, 7])
     inputs_y = torch.randn([2, 256, 16, 16])
     sum_cell = SumCell(256, 256)
     output = sum_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
@@ -19,11 +18,11 @@ def test_sum_cell():
     output = sum_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
     assert output.size() == inputs_y.size()
     output = sum_cell(inputs_x, inputs_y)
-    assert output.size() == inputs_x.size()
+    assert output.size() == inputs_y.size()
 
 
 def test_concat_cell():
-    inputs_x = torch.randn([2, 256, 32, 32])
+    inputs_x = torch.randn([2, 256, 14, 7])
     inputs_y = torch.randn([2, 256, 16, 16])
     concat_cell = ConcatCell(256, 256)
     output = concat_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
@@ -31,11 +30,11 @@ def test_concat_cell():
     output = concat_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
     assert output.size() == inputs_y.size()
     output = concat_cell(inputs_x, inputs_y)
-    assert output.size() == inputs_x.size()
+    assert output.size() == inputs_y.size()
 
 
 def test_global_pool_cell():
-    inputs_x = torch.randn([2, 256, 32, 32])
+    inputs_x = torch.randn([2, 256, 14, 7])
     inputs_y = torch.randn([2, 256, 32, 32])
     gp_cell = GlobalPoolingCell(with_out_conv=False)
     gp_cell_out = gp_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
@@ -56,11 +55,28 @@ def test_resize_methods():
             merge_cell_out = merge_cell._resize(inputs_x, target_size)
             gt_out = F.interpolate(inputs_x, size=target_size, mode=method)
             assert merge_cell_out.equal(gt_out)
-
-    target_size = (64, 64)  # resize to a smaller size
+    """resize to a smaller size by which the inputs_x can't be divisible.
+    And There're different downsample scale on dim H & dim W.
+    """
+    target_size = (14, 7)
     merge_cell = BaseMergeCell()
     merge_cell_out = merge_cell._resize(inputs_x, target_size)
-    kernel_size = inputs_x.shape[-1] // target_size[-1]
+
+    if inputs_x.shape[-2] % target_size[-2] != 0 or inputs_x.shape[
+            -1] % target_size[-1] != 0:
+        h, w = inputs_x.shape[-2:]
+        h_t, w_t = target_size
+        padding_h = (h // h_t + 1) * h_t - h
+        padding_w = (w // w_t + 1) * w_t - w
+        padding_left = padding_w // 2
+        padding_right = padding_w - padding_left
+        padding_top = padding_h // 2
+        padding_bottom = padding_h - padding_top
+        pad = (padding_left, padding_right, padding_top, padding_bottom)
+        inputs_x = F.pad(inputs_x, pad, mode='constant', value=0.0)
+    kernel_size = (inputs_x.shape[-2] // target_size[-2],
+                   inputs_x.shape[-1] // target_size[-1])
     gt_out = F.max_pool2d(
         inputs_x, kernel_size=kernel_size, stride=kernel_size)
     assert (merge_cell_out == gt_out).all()
+    assert merge_cell_out.shape[2:] == target_size
