d6959e87a8992383cc231d9ee6bef4a0f9510906
JarvisKevin <39183050+JarvisKevin@users.noreply.github.com>
2022-05-16 20:37:37 +0800

    Update test_merge_cells.py
    Testing divisible and indivisible situations simultaneously

--- tests/test_ops/test_merge_cells.py
+++ tests/test_ops/test_merge_cells.py
@@ -10,74 +10,87 @@ from mmcv.ops.merge_cells import (BaseMergeCell, ConcatCell, GlobalPoolingCell,
                                   SumCell)
 
 
+# All size (14, 7) below is to test the situation that
+# the input size can't be divisible by the target size.
 def test_sum_cell():
-    inputs_x = torch.randn([2, 256, 14, 7])
-    inputs_y = torch.randn([2, 256, 16, 16])
-    sum_cell = SumCell(256, 256)
-    output = sum_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
-    assert output.size() == inputs_x.size()
-    output = sum_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
-    assert output.size() == inputs_y.size()
-    output = sum_cell(inputs_x, inputs_y)
-    assert output.size() == inputs_y.size()
+    target_resize_sizes = [
+        torch.randn([2, 256, 16, 16]),
+        torch.randn([2, 256, 14, 7])
+    ]
+    for inputs_x in target_resize_sizes:
+        inputs_y = torch.randn([2, 256, 32, 32])
+        sum_cell = SumCell(256, 256)
+        output = sum_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
+        assert output.size() == inputs_x.size()
+        output = sum_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
+        assert output.size() == inputs_y.size()
+        output = sum_cell(inputs_x, inputs_y)
+        assert output.size() == inputs_y.size()
 
 
 def test_concat_cell():
-    inputs_x = torch.randn([2, 256, 14, 7])
-    inputs_y = torch.randn([2, 256, 16, 16])
-    concat_cell = ConcatCell(256, 256)
-    output = concat_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
-    assert output.size() == inputs_x.size()
-    output = concat_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
-    assert output.size() == inputs_y.size()
-    output = concat_cell(inputs_x, inputs_y)
-    assert output.size() == inputs_y.size()
+    target_resize_sizes = [
+        torch.randn([2, 256, 16, 16]),
+        torch.randn([2, 256, 14, 7])
+    ]
+    for inputs_x in target_resize_sizes:
+        inputs_y = torch.randn([2, 256, 32, 32])
+        concat_cell = ConcatCell(256, 256)
+        output = concat_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
+        assert output.size() == inputs_x.size()
+        output = concat_cell(inputs_x, inputs_y, out_size=inputs_y.shape[-2:])
+        assert output.size() == inputs_y.size()
+        output = concat_cell(inputs_x, inputs_y)
+        assert output.size() == inputs_y.size()
 
 
 def test_global_pool_cell():
-    inputs_x = torch.randn([2, 256, 14, 7])
-    inputs_y = torch.randn([2, 256, 32, 32])
-    gp_cell = GlobalPoolingCell(with_out_conv=False)
-    gp_cell_out = gp_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
-    assert (gp_cell_out.size() == inputs_x.size())
-    gp_cell = GlobalPoolingCell(256, 256)
-    gp_cell_out = gp_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
-    assert (gp_cell_out.size() == inputs_x.size())
+    target_resize_sizes = [
+        torch.randn([2, 256, 16, 16]),
+        torch.randn([2, 256, 14, 7])
+    ]
+    for inputs_x in target_resize_sizes:
+        inputs_y = torch.randn([2, 256, 32, 32])
+        gp_cell = GlobalPoolingCell(with_out_conv=False)
+        gp_cell_out = gp_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
+        assert (gp_cell_out.size() == inputs_x.size())
+        gp_cell = GlobalPoolingCell(256, 256)
+        gp_cell_out = gp_cell(inputs_x, inputs_y, out_size=inputs_x.shape[-2:])
+        assert (gp_cell_out.size() == inputs_x.size())
 
 
 def test_resize_methods():
     inputs_x = torch.randn([2, 256, 128, 128])
-    target_resize_sizes = [(128, 128), (256, 256)]
+    target_upsample_sizes = [(128, 128), (256, 256)]
     resize_methods_list = ['nearest', 'bilinear']
 
     for method in resize_methods_list:
         merge_cell = BaseMergeCell(upsample_mode=method)
-        for target_size in target_resize_sizes:
+        for target_size in target_upsample_sizes:
             merge_cell_out = merge_cell._resize(inputs_x, target_size)
             gt_out = F.interpolate(inputs_x, size=target_size, mode=method)
             assert merge_cell_out.equal(gt_out)
-    """resize to a smaller size by which the inputs_x can't be divisible.
-    And There're different downsample scale on dim H & dim W.
-    """
-    target_size = (14, 7)
-    merge_cell = BaseMergeCell()
-    merge_cell_out = merge_cell._resize(inputs_x, target_size)
 
-    if inputs_x.shape[-2] % target_size[-2] != 0 or inputs_x.shape[
-            -1] % target_size[-1] != 0:
-        h, w = inputs_x.shape[-2:]
-        h_t, w_t = target_size
-        padding_h = (h // h_t + 1) * h_t - h
-        padding_w = (w // w_t + 1) * w_t - w
-        padding_left = padding_w // 2
-        padding_right = padding_w - padding_left
-        padding_top = padding_h // 2
-        padding_bottom = padding_h - padding_top
-        pad = (padding_left, padding_right, padding_top, padding_bottom)
-        inputs_x = F.pad(inputs_x, pad, mode='constant', value=0.0)
-    kernel_size = (inputs_x.shape[-2] // target_size[-2],
-                   inputs_x.shape[-1] // target_size[-1])
-    gt_out = F.max_pool2d(
-        inputs_x, kernel_size=kernel_size, stride=kernel_size)
-    assert (merge_cell_out == gt_out).all()
-    assert merge_cell_out.shape[2:] == target_size
+    target_domnsample_sizes = [(64, 64), (14, 7)]  # resize to a smaller size
+    for target_size in target_domnsample_sizes:
+        merge_cell = BaseMergeCell()
+        merge_cell_out = merge_cell._resize(inputs_x, target_size)
+
+        if inputs_x.shape[-2] % target_size[-2] != 0 or inputs_x.shape[
+                -1] % target_size[-1] != 0:
+            h, w = inputs_x.shape[-2:]
+            target_h, target_w = target_size
+            pad_h = (h // target_h + 1) * target_h - h
+            pad_w = (w // target_w + 1) * target_w - w
+            pad_l = pad_w // 2
+            pad_r = pad_w - pad_l
+            pad_t = pad_h // 2
+            pad_b = pad_h - pad_t
+            pad = (pad_l, pad_r, pad_t, pad_b)
+            inputs_x = F.pad(inputs_x, pad, mode='constant', value=0.0)
+        kernel_size = (inputs_x.shape[-2] // target_size[-2],
+                       inputs_x.shape[-1] // target_size[-1])
+        gt_out = F.max_pool2d(
+            inputs_x, kernel_size=kernel_size, stride=kernel_size)
+        assert (merge_cell_out == gt_out).all()
+        assert merge_cell_out.shape[2:] == target_size
