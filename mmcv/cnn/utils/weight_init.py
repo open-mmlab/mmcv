616ed36da40ff00d57e96b294b501b5c75b99795
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-06-01 16:14:50 +0800

    Update mmcv/cnn/utils/weight_init.py

--- mmcv/cnn/utils/weight_init.py
+++ mmcv/cnn/utils/weight_init.py
@@ -54,8 +54,8 @@ def constant_init(module: nn.Module, val: float, bias: float = 0) -> None:
 
 
 def xavier_init(module: nn.Module,
-                gain: float = 1,
-                bias: float = 0,
+                gain: float = 1.,
+                bias: float = 0.,
                 distribution: str = 'normal') -> None:
     assert distribution in ['uniform', 'normal']
     if hasattr(module, 'weight') and module.weight is not None:
