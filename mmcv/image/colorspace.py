88ae2a4e59783d1ec95b11295157dfda8925f20a
tripleMu <92794867+triple-Mu@users.noreply.github.com>
2022-06-03 07:14:51 +0800

    Add type hints in several files (#1999)
    * Add typehint in such files
    
    * Update mmcv/video/processing.py
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
    
    * Fix np.ndarray str
    
    * Fix
    
    * Update mmcv/video/processing.py
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
    
    * Fix
    
    * Fix
    
    * Update mmcv/video/processing.py
    
    Co-authored-by: Jiazhen Wang <47851024+teamwong111@users.noreply.github.com>
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
    Co-authored-by: Jiazhen Wang <47851024+teamwong111@users.noreply.github.com>

--- mmcv/image/colorspace.py
+++ mmcv/image/colorspace.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Callable, Union
+
 import cv2
 import numpy as np
 
 
-def imconvert(img, src, dst):
+def imconvert(img: np.ndarray, src: str, dst: str) -> np.ndarray:
     """Convert an image from the src colorspace to dst colorspace.
 
     Args:
@@ -19,7 +21,7 @@ def imconvert(img, src, dst):
     return out_img
 
 
-def bgr2gray(img, keepdim=False):
+def bgr2gray(img: np.ndarray, keepdim: bool = False) -> np.ndarray:
     """Convert a BGR image to grayscale image.
 
     Args:
@@ -36,7 +38,7 @@ def bgr2gray(img, keepdim=False):
     return out_img
 
 
-def rgb2gray(img, keepdim=False):
+def rgb2gray(img: np.ndarray, keepdim: bool = False) -> np.ndarray:
     """Convert a RGB image to grayscale image.
 
     Args:
@@ -53,7 +55,7 @@ def rgb2gray(img, keepdim=False):
     return out_img
 
 
-def gray2bgr(img):
+def gray2bgr(img: np.ndarray) -> np.ndarray:
     """Convert a grayscale image to BGR image.
 
     Args:
@@ -67,7 +69,7 @@ def gray2bgr(img):
     return out_img
 
 
-def gray2rgb(img):
+def gray2rgb(img: np.ndarray) -> np.ndarray:
     """Convert a grayscale image to RGB image.
 
     Args:
@@ -81,7 +83,7 @@ def gray2rgb(img):
     return out_img
 
 
-def _convert_input_type_range(img):
+def _convert_input_type_range(img: np.ndarray) -> np.ndarray:
     """Convert the type and range of the input image.
 
     It converts the input image to np.float32 type and range of [0, 1].
@@ -109,7 +111,8 @@ def _convert_input_type_range(img):
     return img
 
 
-def _convert_output_type_range(img, dst_type):
+def _convert_output_type_range(
+        img: np.ndarray, dst_type: Union[np.uint8, np.float32]) -> np.ndarray:
     """Convert the type and range of the image according to dst_type.
 
     It converts the image to desired type and range. If `dst_type` is np.uint8,
@@ -140,7 +143,7 @@ def _convert_output_type_range(img, dst_type):
     return img.astype(dst_type)
 
 
-def rgb2ycbcr(img, y_only=False):
+def rgb2ycbcr(img: np.ndarray, y_only: bool = False) -> np.ndarray:
     """Convert a RGB image to YCbCr image.
 
     This function produces the same results as Matlab's `rgb2ycbcr` function.
@@ -174,7 +177,7 @@ def rgb2ycbcr(img, y_only=False):
     return out_img
 
 
-def bgr2ycbcr(img, y_only=False):
+def bgr2ycbcr(img: np.ndarray, y_only: bool = False) -> np.ndarray:
     """Convert a BGR image to YCbCr image.
 
     The bgr version of rgb2ycbcr.
@@ -208,7 +211,7 @@ def bgr2ycbcr(img, y_only=False):
     return out_img
 
 
-def ycbcr2rgb(img):
+def ycbcr2rgb(img: np.ndarray) -> np.ndarray:
     """Convert a YCbCr image to RGB image.
 
     This function produces the same results as Matlab's ycbcr2rgb function.
@@ -240,7 +243,7 @@ def ycbcr2rgb(img):
     return out_img
 
 
-def ycbcr2bgr(img):
+def ycbcr2bgr(img: np.ndarray) -> np.ndarray:
     """Convert a YCbCr image to BGR image.
 
     The bgr version of ycbcr2rgb.
@@ -272,11 +275,11 @@ def ycbcr2bgr(img):
     return out_img
 
 
-def convert_color_factory(src, dst):
+def convert_color_factory(src: str, dst: str) -> Callable:
 
     code = getattr(cv2, f'COLOR_{src.upper()}2{dst.upper()}')
 
-    def convert_color(img):
+    def convert_color(img: np.ndarray) -> np.ndarray:
         out_img = cv2.cvtColor(img, code)
         return out_img
 
