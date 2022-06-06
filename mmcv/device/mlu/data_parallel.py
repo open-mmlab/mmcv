8ffca2d512ed81a93b72403ee3fb24d01056c87b
luoshuxiao <1069103287@qq.com>
2022-06-06 16:40:58 +0800

    update_hierarchical_data resource code fix

--- mmcv/device/mlu/data_parallel.py
+++ mmcv/device/mlu/data_parallel.py
@@ -8,25 +8,19 @@ from .scatter_gather import scatter_kwargs
 
 class MLUDataParallel(MMDataParallel):
     """The MLUDataParallel module that supports DataContainer.
-
     MLUDataParallel is a class inherited from MMDataParall, which supports
     MLU training and inference only.
-
     The main differences with MMDataParallel:
-
     - It only supports single-card of MLU, and only use first card to
       run training and inference.
-
     - It uses direct host-to-device copy instead of stream-background
       scatter.
-
     .. warning::
         MLUDataParallel only supports single MLU training, if you need to
         train with multiple MLUs, please use MLUDistributedDataParallel
         instead. If you have multiple MLUs, you can set the environment
         variable ``MLU_VISIBLE_DEVICES=0`` (or any other card number(s))
         to specify the running device.
-
     Args:
         module (:class:`nn.Module`): Module to be encapsulated.
         dim (int): Dimension used to scatter the data. Defaults to 0.
