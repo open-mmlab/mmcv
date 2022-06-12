ec67e3719ee3f988c568045e6e304e33511b3fd9
tripleMu <92794867+triple-Mu@users.noreply.github.com>
2022-06-12 22:52:23 +0800

    Update mmcv/parallel/_functions.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmcv/parallel/_functions.py
+++ mmcv/parallel/_functions.py
@@ -66,7 +66,7 @@ def get_input_device(input: Union[List, torch.Tensor]) -> int:
 class Scatter:
 
     @staticmethod
-    def forward(target_gpus: List, input: List) -> tuple:
+    def forward(target_gpus: List[int], input: Union[List, torch.Tensor]) -> tuple:
         input_device = get_input_device(input)
         streams = None
         if input_device == -1 and target_gpus != [-1]:
