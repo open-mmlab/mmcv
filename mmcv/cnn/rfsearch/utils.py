dae649bebc1f8eacbc20455b099d1e61f1b39aa2
shgao <shgao@live.com>
2022-06-15 13:09:45 +0800

    format

--- mmcv/cnn/rfsearch/utils.py
+++ mmcv/cnn/rfsearch/utils.py
@@ -1,24 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import json
 
-# def init_config():
-#     config = {}
-#     config["search"] = {}
-#     config["model"] = {}
-#     config["search"]["step"] = 0
-#     config["search"]["max_step"] = 11
-#     config["search"]["search_interval"] = 1
-#     config["search"]["exp_rate"] = 0.5
-#     config["search"]["init_alphas"] = 0.01
-#     config["search"]["normlize"] = "absavg"
-#     config["search"]["mmin"] = 1
-#     config["search"]["mmax"] = 24
-#     config["search"]["finetune"] = False
-#     return config
-
 
 def load_structure(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         config = json.load(f)
         return config['model']
 
