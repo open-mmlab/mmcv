66074af16a500bdc395aa9c75c7fd70cf2842db4
ChaimZhu <zhuchenming@pjlab.org.cn>
2022-08-29 16:23:58 +0800

    remove points_xyz is None

--- mmcv/ops/points_sampler.py
+++ mmcv/ops/points_sampler.py
@@ -100,7 +100,7 @@ class PointsSampler(nn.Module):
         Returns:
             torch.Tensor: (B, npoint, sample_num) Indices of sampled points.
         """
-        if points_xyz and points_xyz.dtype == torch.half:
+        if points_xyz.dtype == torch.half:
             points_xyz = points_xyz.to(torch.float32)
         if features is not None and features.dtype == torch.half:
             features = features.to(torch.float32)
