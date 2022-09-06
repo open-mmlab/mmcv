677583232780fc8f6c74559b6f3ec0d6cca13958
ckirchhoff <chenxiang143@huawei.com>
2022-09-06 17:06:37 +0800

    modify focallossbackward adapter code

--- mmcv/ops/csrc/pytorch/npu/focal_loss_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/focal_loss_npu.cpp
@@ -28,19 +28,29 @@ void sigmoid_focal_loss_forward_impl(Tensor input, Tensor target, Tensor weight,
 
 void sigmoid_focal_loss_backward_npu(Tensor input, Tensor target, Tensor weight, 
                                      Tensor grad_input, float gamma, float alpha) {
-    at::Scalar s = 1;
     OpPreparation::CheckOut(
         {input, target, weight},
         grad_input,
         input);
 
     at::Tensor target_y = at_npu::native::NPUNativeFunctions::npu_reshape(target, input.sizes(), true);
+    at::Tensor dout = at::ones_like(input);
+    int64_t weight_size = weight.size(0);
+    at::Tensor weight_y = at::ones_like(input);
+    if(weight_size > 0) {
+        weight_y = aat_npu::native::NPUNativeFunctions::npu_broadcast(weight, input.sizes());
+    }
+
     OpCommand cmd;
-    cmd.Name("AxpyV2")
+    cmd.Name("SigmoidFocalLossGrad")
         .Input(input)
         .Input(target_y)
-        .Input(s, input.scalar_type())
+        .Input(dout)
+        .Input(weight_y)
         .Output(grad_input)
+        .Attr("gamma", gamma)
+        .Attr("alpha", alpha)
+        .Attr("reduction", "none")
         .Run();
 }
 
@@ -73,7 +83,6 @@ void softmax_focal_loss_forward_impl(Tensor input, Tensor target, Tensor weight,
 
 void softmax_focal_loss_backward_npu(Tensor input, Tensor target, Tensor weight, Tensor buff,
                                      Tensor grad_input, float gamma, float alpha) {
-    at::Scalar s = 1;
     OpPreparation::CheckOut(
         {input, target, weight},
         grad_input,
@@ -82,13 +91,23 @@ void softmax_focal_loss_backward_npu(Tensor input, Tensor target, Tensor weight,
     int64_t  n_class = input.size(1);
     at::Tensor target_y = at_npu::native::NPUNativeFunctions::one_hot(target, n_class);
     target_y = at_npu::native::NPUNativeFunctions::npu_dtype_cast(target_y, input.scalar_type());
-
+    at::Tensor dout = at::ones_like(input);
+    int64_t weight_size = weight.size(0);
+    at::Tensor weight_y = at::ones_like(input);
+    if(weight_size > 0) {
+        weight_y = aat_npu::native::NPUNativeFunctions::npu_broadcast(weight, input.sizes());
+    }
+    
     OpCommand cmd;
-    cmd.Name("AxpyV2")
+    cmd.Name("SoftmaxFocalLossGrad")
         .Input(input)
         .Input(target_y)
-        .Input(s, input.scalar_type())
+        .Input(dout)
+        .Input(weight_y)
         .Output(grad_input)
+        .Attr("gamma", gamma)
+        .Attr("alpha", alpha)
+        .Attr("reduction", "none")
         .Run();
 }
 
