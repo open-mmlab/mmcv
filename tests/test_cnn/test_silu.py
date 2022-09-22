18abae613b3d015668b60add43a48c39f364a7bd
Rist115 <to78314910@gmail.com>
2022-09-22 17:57:29 +0900

    fix test

--- tests/test_cnn/test_silu.py
+++ tests/test_cnn/test_silu.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
-import torch.nn.functional as F
 
 from mmcv.cnn.bricks import build_activation_layer
 
@@ -8,9 +7,11 @@ from mmcv.cnn.bricks import build_activation_layer
 def test_silu():
     act = build_activation_layer(dict(type='SiLU'))
     input = torch.randn(1, 3, 64, 64)
-    expected_output = F.silu(input)
     output = act(input)
+    expected_output = input * torch.sigmoid(input)
     # test output shape
     assert output.shape == expected_output.shape
     # test output value
-    assert torch.equal(output, expected_output)
+    print(output)
+    print(expected_output)
+    assert torch.allclose(output, expected_output)
