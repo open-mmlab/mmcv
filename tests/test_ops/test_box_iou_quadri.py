dd759bc57488b44894689398f80dd31ca94004dd
zytx121 <592267829@qq.com>
2022-10-13 17:02:25 +0800

    fix ut

--- tests/test_ops/test_box_iou_quadri.py
+++ tests/test_ops/test_box_iou_quadri.py
@@ -8,34 +8,8 @@ from mmcv.utils import IS_CUDA_AVAILABLE
 
 class TestBoxIoUQuadri:
 
-    def test_box_iou_quadri_cpu(self):
-        from mmcv.ops import box_iou_quadri
-        np_boxes1 = np.asarray([[1.0, 1.0, 3.0, 4.0, 4.0, 4.0, 4.0, 1.0],
-                                [2.0, 2.0, 3.0, 4.0, 4.0, 2.0, 3.0, 1.0],
-                                [7.0, 7.0, 8.0, 8.0, 9.0, 7.0, 8.0, 6.0]],
-                               dtype=np.float32)
-        np_boxes2 = np.asarray([[0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0, 0.0],
-                                [2.0, 1.0, 2.0, 4.0, 4.0, 4.0, 4.0, 1.0],
-                                [7.0, 6.0, 7.0, 8.0, 9.0, 8.0, 9.0, 6.0]],
-                               dtype=np.float32)
-        np_expect_ious = np.asarray(
-            [[0.0714, 1.0000, 0.0000], [0.0000, 0.5000, 0.0000],
-             [0.0000, 0.0000, 0.5000]],
-            dtype=np.float32)
-        np_expect_ious_aligned = np.asarray([0.0714, 0.5000, 0.5000],
-                                            dtype=np.float32)
-
-        boxes1 = torch.from_numpy(np_boxes1)
-        boxes2 = torch.from_numpy(np_boxes2)
-
-        ious = box_iou_quadri(boxes1, boxes2)
-        assert np.allclose(ious.cpu().numpy(), np_expect_ious, atol=1e-4)
-
-        ious = box_iou_quadri(boxes1, boxes2, aligned=True)
-        assert np.allclose(
-            ious.cpu().numpy(), np_expect_ious_aligned, atol=1e-4)
-
     @pytest.mark.parametrize('device', [
+        'cpu',
         pytest.param(
             'cuda',
             marks=pytest.mark.skipif(
@@ -68,33 +42,8 @@ class TestBoxIoUQuadri:
         assert np.allclose(
             ious.cpu().numpy(), np_expect_ious_aligned, atol=1e-4)
 
-    def test_box_iou_quadri_iof_cpu(self):
-        from mmcv.ops import box_iou_quadri
-        np_boxes1 = np.asarray([[1.0, 1.0, 3.0, 4.0, 4.0, 4.0, 4.0, 1.0],
-                                [2.0, 2.0, 3.0, 4.0, 4.0, 2.0, 3.0, 1.0],
-                                [7.0, 7.0, 8.0, 8.0, 9.0, 7.0, 8.0, 6.0]],
-                               dtype=np.float32)
-        np_boxes2 = np.asarray([[0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0, 0.0],
-                                [2.0, 1.0, 2.0, 4.0, 4.0, 4.0, 4.0, 1.0],
-                                [7.0, 6.0, 7.0, 8.0, 9.0, 8.0, 9.0, 6.0]],
-                               dtype=np.float32)
-        np_expect_ious = np.asarray(
-            [[0.1111, 1.0000, 0.0000], [0.0000, 1.0000, 0.0000],
-             [0.0000, 0.0000, 1.0000]],
-            dtype=np.float32)
-        np_expect_ious_aligned = np.asarray([0.1111, 1.0000, 1.0000],
-                                            dtype=np.float32)
-
-        boxes1 = torch.from_numpy(np_boxes1)
-        boxes2 = torch.from_numpy(np_boxes2)
-
-        ious = box_iou_quadri(boxes1, boxes2, mode='iof')
-        assert np.allclose(ious.cpu().numpy(), np_expect_ious, atol=1e-4)
-        ious = box_iou_quadri(boxes1, boxes2, mode='iof', aligned=True)
-        assert np.allclose(
-            ious.cpu().numpy(), np_expect_ious_aligned, atol=1e-4)
-
     @pytest.mark.parametrize('device', [
+        'cpu',
         pytest.param(
             'cuda',
             marks=pytest.mark.skipif(
--- tests/test_ops/test_nms_quadri.py
+++ tests/test_ops/test_nms_quadri.py
@@ -8,30 +8,8 @@ from mmcv.utils import IS_CUDA_AVAILABLE
 
 class TestNMSQuadri:
 
-    def test_ml_nms_quadri_cpu(self):
-        from mmcv.ops import nms_quadri
-        np_boxes = np.array([[1.0, 1.0, 3.0, 4.0, 4.0, 4.0, 4.0, 1.0, 0.7],
-                             [2.0, 2.0, 3.0, 4.0, 4.0, 2.0, 3.0, 1.0, 0.8],
-                             [7.0, 7.0, 8.0, 8.0, 9.0, 7.0, 8.0, 6.0, 0.5],
-                             [0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0, 0.0, 0.9]],
-                            dtype=np.float32)
-        np_labels = np.array([1, 0, 1, 0], dtype=np.float32)
-
-        np_expect_dets = np.array([[0., 0., 0., 2., 2., 2., 2., 0.],
-                                   [2., 2., 3., 4., 4., 2., 3., 1.],
-                                   [7., 7., 8., 8., 9., 7., 8., 6.]],
-                                  dtype=np.float32)
-        np_expect_keep_inds = np.array([3, 1, 2], dtype=np.int64)
-
-        boxes = torch.from_numpy(np_boxes)
-        labels = torch.from_numpy(np_labels)
-
-        dets, keep_inds = nms_quadri(boxes[:, :8], boxes[:, -1], 0.3, labels)
-
-        assert np.allclose(dets.numpy()[:, :8], np_expect_dets)
-        assert np.allclose(keep_inds.numpy(), np_expect_keep_inds)
-
     @pytest.mark.parametrize('device', [
+        'cpu',
         pytest.param(
             'cuda',
             marks=pytest.mark.skipif(
@@ -60,27 +38,8 @@ class TestNMSQuadri:
         assert np.allclose(dets.cpu().numpy()[:, :8], np_expect_dets)
         assert np.allclose(keep_inds.cpu().numpy(), np_expect_keep_inds)
 
-    def test_nms_quadri_cpu(self):
-        from mmcv.ops import nms_quadri
-        np_boxes = np.array([[1.0, 1.0, 3.0, 4.0, 4.0, 4.0, 4.0, 1.0, 0.7],
-                             [2.0, 2.0, 3.0, 4.0, 4.0, 2.0, 3.0, 1.0, 0.8],
-                             [7.0, 7.0, 8.0, 8.0, 9.0, 7.0, 8.0, 6.0, 0.5],
-                             [0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0, 0.0, 0.9]],
-                            dtype=np.float32)
-
-        np_expect_dets = np.array([[0., 0., 0., 2., 2., 2., 2., 0.],
-                                   [2., 2., 3., 4., 4., 2., 3., 1.],
-                                   [7., 7., 8., 8., 9., 7., 8., 6.]],
-                                  dtype=np.float32)
-        np_expect_keep_inds = np.array([3, 1, 2], dtype=np.int64)
-
-        boxes = torch.from_numpy(np_boxes)
-
-        dets, keep_inds = nms_quadri(boxes[:, :8], boxes[:, -1], 0.3)
-        assert np.allclose(dets.numpy()[:, :8], np_expect_dets)
-        assert np.allclose(keep_inds.numpy(), np_expect_keep_inds)
-
     @pytest.mark.parametrize('device', [
+        'cpu',
         pytest.param(
             'cuda',
             marks=pytest.mark.skipif(
@@ -106,53 +65,8 @@ class TestNMSQuadri:
         assert np.allclose(dets.cpu().numpy()[:, :8], np_expect_dets)
         assert np.allclose(keep_inds.cpu().numpy(), np_expect_keep_inds)
 
-    def test_batched_nms_cpu(self):
-        # test batched_nms with nms_quadri
-        from mmcv.ops import batched_nms
-
-        np_boxes = np.array([[1.0, 1.0, 3.0, 4.0, 4.0, 4.0, 4.0, 1.0, 0.7],
-                             [2.0, 2.0, 3.0, 4.0, 4.0, 2.0, 3.0, 1.0, 0.8],
-                             [7.0, 7.0, 8.0, 8.0, 9.0, 7.0, 8.0, 6.0, 0.5],
-                             [0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0, 0.0, 0.9]],
-                            dtype=np.float32)
-        np_labels = np.array([1, 0, 1, 0], dtype=np.float32)
-
-        np_expect_agnostic_dets = np.array([[0., 0., 0., 2., 2., 2., 2., 0.],
-                                            [2., 2., 3., 4., 4., 2., 3., 1.],
-                                            [7., 7., 8., 8., 9., 7., 8., 6.]],
-                                           dtype=np.float32)
-        np_expect_agnostic_keep_inds = np.array([3, 1, 2], dtype=np.int64)
-
-        np_expect_dets = np.array([[0., 0., 0., 2., 2., 2., 2., 0.],
-                                   [2., 2., 3., 4., 4., 2., 3., 1.],
-                                   [1., 1., 3., 4., 4., 4., 4., 1.],
-                                   [7., 7., 8., 8., 9., 7., 8., 6.]],
-                                  dtype=np.float32)
-        np_expect_keep_inds = np.array([3, 1, 0, 2], dtype=np.int64)
-
-        nms_cfg = dict(type='nms_quadri', iou_threshold=0.3)
-
-        # test class_agnostic is True
-        boxes, keep = batched_nms(
-            torch.from_numpy(np_boxes[:, :8]),
-            torch.from_numpy(np_boxes[:, -1]),
-            torch.from_numpy(np_labels),
-            nms_cfg,
-            class_agnostic=True)
-        assert np.allclose(boxes.numpy()[:, :8], np_expect_agnostic_dets)
-        assert np.allclose(keep.numpy(), np_expect_agnostic_keep_inds)
-
-        # test class_agnostic is False
-        boxes, keep = batched_nms(
-            torch.from_numpy(np_boxes[:, :8]),
-            torch.from_numpy(np_boxes[:, -1]),
-            torch.from_numpy(np_labels),
-            nms_cfg,
-            class_agnostic=False)
-        assert np.allclose(boxes.numpy()[:, :8], np_expect_dets)
-        assert np.allclose(keep.numpy(), np_expect_keep_inds)
-
     @pytest.mark.parametrize('device', [
+        'cpu',
         pytest.param(
             'cuda',
             marks=pytest.mark.skipif(
