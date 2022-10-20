071eabaa4d5c25f7900039e77a249d4bbf710e1a
budefei <budefei@cambricon.com>
2022-10-20 09:40:48 +0800

    refactor code for lint

--- mmcv/ops/csrc/common/pytorch_mlu_helper.hpp
+++ mmcv/ops/csrc/common/pytorch_mlu_helper.hpp
@@ -27,8 +27,7 @@
 
 inline int32_t getJobLimitCapability() {
   CNcontext drv_ctx;
-  TORCH_CHECK(CN_SUCCESS == cnCtxGetCurrent(&drv_ctx),
-      "cnCtxGetCurrent fails");
+  TORCH_CHECK(CN_SUCCESS == cnCtxGetCurrent(&drv_ctx), "cnCtxGetCurrent fails");
   CNctxConfigParam ctx_conf_param;
   TORCH_CHECK(
       CN_SUCCESS == cnGetCtxConfigParam(drv_ctx, CN_CTX_CONFIG_UNION_LIMIT,
