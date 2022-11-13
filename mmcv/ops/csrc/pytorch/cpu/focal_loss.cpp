3ea152002679f7bb79bebb12fa5d3794ab35f501
HAOCHENYE <21724054@zju.edu.cn>
2022-11-14 03:58:58 +0800

    add focal loss

---
+++ mmcv/ops/csrc/pytorch/cpu/focal_loss.cpp
@@ -0,0 +1,57 @@
+#include "pytorch_cpp_helper.hpp"
+#include "pytorch_device_registry.hpp"
+
+void sigmoid_focal_loss_forward_cpu(Tensor input, Tensor target,
+                                               Tensor weight, Tensor output,
+                                               const float gamma,
+                                               const float alpha)
+{
+  std::cout << "please implement sigmoid_focal_loss_forward_cpu" << std::endl;
+}
+
+void sigmoid_focal_loss_backward_cpu(Tensor input, Tensor target,
+                                      Tensor weight, Tensor grad_input,
+                                      float gamma, float alpha)
+{
+  std::cout << "please implement sigmoid_focal_loss_backward_cpu" << std::endl;
+}
+
+
+void softmax_focal_loss_forward_cpu(Tensor input, Tensor target, Tensor weight,
+                                     Tensor output, float gamma, float alpha)
+{
+  std::cout << "please implement  softmax_focal_loss_forward_cpu" << std::endl;
+}
+
+void softmax_focal_loss_backward_cpu(Tensor input, Tensor target,
+                                      Tensor weight, Tensor buff,
+                                      Tensor grad_input, float gamma,
+                                      float alpha)
+{
+  std::cout << "please implement softmax_focal_loss_backward_cpu" << std::endl;
+}
+
+
+void sigmoid_focal_loss_forward_impl(Tensor input, Tensor target, Tensor weight,
+                                     Tensor output, float gamma, float alpha);
+void sigmoid_focal_loss_backward_impl(Tensor input, Tensor target,
+                                      Tensor weight, Tensor grad_input,
+                                      float gamma, float alpha);
+void softmax_focal_loss_forward_impl(Tensor input, Tensor target, Tensor weight,
+                                     Tensor output, float gamma, float alpha);
+void softmax_focal_loss_backward_impl(Tensor input, Tensor target,
+                                      Tensor weight, Tensor buff,
+                                      Tensor grad_input, float gamma,
+                                      float alpha);
+
+REGISTER_DEVICE_IMPL(softmax_focal_loss_forward_impl, CPU,
+                     softmax_focal_loss_forward_cpu);
+
+REGISTER_DEVICE_IMPL(softmax_focal_loss_backward_impl, CPU,
+                     softmax_focal_loss_backward_cpu);
+
+REGISTER_DEVICE_IMPL(sigmoid_focal_loss_forward_impl, CPU,
+                     sigmoid_focal_loss_forward_cpu);
+                     
+REGISTER_DEVICE_IMPL(sigmoid_focal_loss_backward_impl, CPU,
+                     sigmoid_focal_loss_backward_cpu);
