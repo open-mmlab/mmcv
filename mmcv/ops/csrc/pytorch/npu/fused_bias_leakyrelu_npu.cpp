5c59540f5bfc6b9fe172a099e96c436553e1c40e
jayggh <35617559+jayggh@users.noreply.github.com>
2022-12-07 12:53:33 +0800

    Update fused_bias_leakyrelu_npu.cpp

--- mmcv/ops/csrc/pytorch/npu/fused_bias_leakyrelu_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/fused_bias_leakyrelu_npu.cpp
@@ -3,51 +3,52 @@
 using namespace NPU_NAME_SPACE;
 using namespace std;
 
-Tensor fused_bias_leakyrelu_op_impl(Tensor& input, Tensor& bias,
-                                    Tensor& refer, int act, int grad, 
-                                    float alpha, float scale) {
-}
+Tensor fused_bias_leakyrelu_op_impl(Tensor& input, Tensor& bias, 
+                                    Tensor& refer, int act, 
+                                    int grad, float alpha, float scale);
 
-Tensor fused_bias_leakyrelu_npu(Tensor& input,Tensor& bias,
-                                Tensor& refer, int act, int grad, 
-                                float alpha, float scale) {
-  at::tensor y = at::tempty_like(input);
-  // forward
+Tensor fused_bias_leakyrelu_npu(Tensor& input, Tensor& bias, 
+                                Tensor& refer, int act, 
+                                int grad, float alpha, float scale){
+  at::Tensor py = at::empty_like(input);
+  //forward
   if (grad == 1){
-    auto input_size = input.size();
+    auto input_size = input.sizes();
     int input_length = input_size.size();
+    c10::SmallVector<int64_t, SIZE> input_size_tmp;
+    input_size_tmp = array_to_small_vector(input_size);
     if (input_length > 1){
         for (int i = 0; i < input_length; i++){
             if (i != 1){
-                input_size[i] = 1;
-            }  
+              input_size_tmp[i] = 1;
+            }
         }
     }
-    at::Tensor bias_ = at::reshape(bias, input_size);
-    at::Tensor bias_tmp = NPUNativeFunctions::npu_broadcast(bias_, input.size());
+    at::Tensor bias_tmp = at::reshape(bias, input_size_tmp);
+    at::Tensor bias_ = at_npu::native::NPUNativeFunctions::npu_broadcast(bias_tmp, input.sizes());
     OpCommand cmd;
     cmd.Name("FusedBiasLeakyRelu")
-        .Input(input);
-        .Input(bias);
-        .Output(y);
-        .Attr("sacle",sacle);
-        .Attr("negative_slope", alpha);
+        .Input(input)
+        .Input(bias_)
+        .Output(py)
+        .Attr("scale", scale)
+        .Attr("negative_slope", alpha)
         .Run();
   }
 
-  // backward
+  //backward
   if (grad == 2){
     OpCommand cmd;
     cmd.Name("FusedBiasLeakyReluGrad")
-        .Input(input);
-        .Input(ref);
-        .Output(y);
-        .Attr("sacle",sacle);
-        .Attr("negative_slope", alpha);
+        .Input(input)
+        .Input(refer)
+        .Output(py)
+        .Attr("scale", scale)
+        .Attr("negative_slope", alpha)
         .Run();
   }
-  
-  return y;
+  return py;
+
 }
 
 REGISTER_NPU_IMPL(fused_bias_leakyrelu_op_impl,
