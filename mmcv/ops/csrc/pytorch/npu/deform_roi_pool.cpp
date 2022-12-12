5f4fd22498167bf671c12815549aa01c96a9bdf7
jayggh <35617559+jayggh@users.noreply.github.com>
2022-12-12 19:56:02 +0800

    Update deform_roi_pool.cpp

--- mmcv/ops/csrc/pytorch/npu/deform_roi_pool.cpp
+++ mmcv/ops/csrc/pytorch/npu/deform_roi_pool.cpp
@@ -19,20 +19,20 @@ void deform_roi_pool_forward_npu(Tensor input, Tensor rois, Tensor offset,
                                  Tensor output, int pooled_height,
                                  int pooled_width, float spatial_scale,
 -                                int sampling_ratio, float gamma) {
-    c10::SmallVector<int64_t, 2> output_sizes = {pooled_height, pooled_width};
-    at::IntArrayRef output_size = at::IntArrayRef(output_sizes);
-    int64_t sampling_ratio_ = (int64_t)sampling_ratio;
-    OpCommand cmd;
-    cmd.Name("DeformRoiPool")
-        .Input(input)
-        .Input(rois)
-        .Input(offset)
-        .Output(output)
-        .Attr("spatial_scale", spatial_scale)
-        .Attr("output_size", output_size)
-        .Attr("sampling_ratio", sampling_ratio_)
-        .Attr("gamma", gamma)
-        .Run();
+  c10::SmallVector<int64_t, 2> output_sizes = {pooled_height, pooled_width};
+  at::IntArrayRef output_size = at::IntArrayRef(output_sizes);
+  int64_t sampling_ratio_ = (int64_t)sampling_ratio;
+  OpCommand cmd;
+  cmd.Name("DeformRoiPool")
+      .Input(input)
+      .Input(rois)
+      .Input(offset)
+      .Output(output)
+      .Attr("spatial_scale", spatial_scale)
+      .Attr("output_size", output_size)
+      .Attr("sampling_ratio", sampling_ratio_)
+      .Attr("gamma", gamma)
+      .Run();
 }
 
 void deform_roi_pool_backward_npu(Tensor grad_output, Tensor input, Tensor rois,
@@ -40,22 +40,22 @@ void deform_roi_pool_backward_npu(Tensor grad_output, Tensor input, Tensor rois,
 +                                  Tensor grad_offset, int pooled_height,
 +                                  int pooled_width, float spatial_scale,
 +                                  int sampling_ratio, float gamma) {
-    c10::SmallVector<int64_t, 2> output_sizes = {pooled_height, pooled_width};
-    at::IntArrayRef output_size = at::IntArrayRef(output_sizes);
-    int64_t sampling_ratio_ = (int64_t)sampling_ratio;
-    OpCommand cmd;
-    cmd.Name("DeformRoiPoolGrad")
-        .Input(grad_input)
-        .Input(input)
-        .Input(rois)
-        .Input(offset)
-        .Output(grad_output)
-        .Output(grad_offset)
-        .Attr("output_size", output_size)
-        .Attr("spatial_scale", spatial_scale)
-        .Attr("sample_ratio", sampling_ratio_)
-        .Attr("gamma", gamma)
-        .Run();
+  c10::SmallVector<int64_t, 2> output_sizes = {pooled_height, pooled_width};
+  at::IntArrayRef output_size = at::IntArrayRef(output_sizes);
+  int64_t sampling_ratio_ = (int64_t)sampling_ratio;
+  OpCommand cmd;
+  cmd.Name("DeformRoiPoolGrad")
+      .Input(grad_input)
+      .Input(input)
+      .Input(rois)
+      .Input(offset)
+      .Output(grad_output)
+      .Output(grad_offset)
+      .Attr("output_size", output_size)
+      .Attr("spatial_scale", spatial_scale)
+      .Attr("sample_ratio", sampling_ratio_)
+      .Attr("gamma", gamma)
+      .Run();
 }
 
 REGISTER_NPU_IMPL(deform_roi_pool_forward_impl, deform_roi_pool_forward_npu);
