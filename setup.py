f41eff28c486a46c7380740186c1e5e2c86342f0
xiabo123 <40659418+xiabo123@users.noreply.github.com>
2022-12-28 15:04:35 +0800

    Change the block size to 1024 during compilation. (#2460)
    * The default block size of rocm is 256.The designed kenel has a block size of 512.Change the block size to 1024 during compilation.
    
    * The default block size of rocm is 256.The designed kenel has a block size of 512.Change the block size to 1024 during compilation.
    
    * The default block size of rocm is 256.The designed kenel has a block size of 512.Change the block size to 1024 during compilation.
    
    * The default block size of rocm is 256.The designed kenel has a block size of 512.Change the block size to 1024 during compilation.

--- setup.py
+++ setup.py
@@ -284,6 +284,9 @@ def get_extensions():
             define_macros += [('MMCV_WITH_CUDA', None)]
             cuda_args = os.getenv('MMCV_CUDA_ARGS')
             extra_compile_args['nvcc'] = [cuda_args] if cuda_args else []
+            if is_rocm_pytorch and platform.system() != 'Windows':
+                extra_compile_args['nvcc'] += \
+                    ['--gpu-max-threads-per-block=1024']
             op_files = glob.glob('./mmcv/ops/csrc/pytorch/*.cpp') + \
                 glob.glob('./mmcv/ops/csrc/pytorch/cpu/*.cpp') + \
                 glob.glob('./mmcv/ops/csrc/pytorch/cuda/*.cu') + \
