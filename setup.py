dae5e15af53e0abf0d99067276e96d8456197ad9
cokedong <408244909@qq.com>
2023-01-10 15:07:41 +0800

    Fix error while rebasing

--- setup.py
+++ setup.py
@@ -210,11 +210,13 @@ def get_extensions():
         include_dirs = []
         op_files = glob.glob('./mmcv/ops/csrc/pytorch/cuda/*.cu') +\
             glob.glob('./mmcv/ops/csrc/pytorch/cpu/*.cpp') +\
-            glob.glob('./mmcv/ops/csrc/parrots/*.cpp')
+            glob.glob('./mmcv/ops/csrc/parrots/*.cpp') +\
+            glob.glob('./mmcv/ops/csrc/diopi_impl/torch/*.cpp')
         op_files.remove('./mmcv/ops/csrc/pytorch/cuda/iou3d_cuda.cu')
 
         include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common'))
         include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common/cuda'))
+        include_dirs.append(os.path.abspath('./DIOPI/include'))
         cuda_args = os.getenv('MMCV_CUDA_ARGS')
         extra_compile_args = {
             'nvcc': [cuda_args, '-std=c++14'] if cuda_args else ['-std=c++14'],
