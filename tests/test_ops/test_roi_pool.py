2810718a99045bb762d24f465a69b7add34f3123
sherie <963372609@qq.com>
2023-01-12 11:52:28 +0800

    [Feature] Add Ascend support for RoIPool op (#2483)
    Co-authored-by: wangxiaoxin_sherie <wangxiaoxin7@huawei.com>

--- tests/test_ops/test_roi_pool.py
+++ tests/test_ops/test_roi_pool.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 import torch
 
-from mmcv.utils import IS_CUDA_AVAILABLE, IS_MLU_AVAILABLE
+from mmcv.utils import IS_CUDA_AVAILABLE, IS_MLU_AVAILABLE, IS_NPU_AVAILABLE
 
 _USING_PARROTS = True
 try:
@@ -69,14 +69,20 @@ class TestRoiPool:
             np_output = np.array(output[0])
             np_grad = np.array(output[1])
 
-            x = torch.tensor(
-                np_input, dtype=dtype, device=device, requires_grad=True)
-            rois = torch.tensor(np_rois, dtype=dtype, device=device)
-
-            output = roi_pool(x, rois, (pool_h, pool_w), spatial_scale)
-            output.backward(torch.ones_like(output))
-            assert np.allclose(output.data.cpu().numpy(), np_output, 1e-3)
-            assert np.allclose(x.grad.data.cpu().numpy(), np_grad, 1e-3)
+            if device == 'npu':
+                import torch_npu  # noqa: F401
+                x = torch.tensor(np_input, dtype=dtype).npu()
+                rois = torch.tensor(np_rois, dtype=dtype).npu()
+                output = roi_pool(x, rois, (pool_h, pool_w), spatial_scale)
+                assert np.allclose(output.data.cpu().numpy(), np_output, 1e-3)
+            else:
+                x = torch.tensor(
+                    np_input, dtype=dtype, device=device, requires_grad=True)
+                rois = torch.tensor(np_rois, dtype=dtype, device=device)
+                output = roi_pool(x, rois, (pool_h, pool_w), spatial_scale)
+                output.backward(torch.ones_like(output))
+                assert np.allclose(output.data.cpu().numpy(), np_output, 1e-3)
+                assert np.allclose(x.grad.data.cpu().numpy(), np_grad, 1e-3)
 
     @pytest.mark.parametrize('device', [
         pytest.param(
@@ -86,7 +92,11 @@ class TestRoiPool:
         pytest.param(
             'mlu',
             marks=pytest.mark.skipif(
-                not IS_MLU_AVAILABLE, reason='requires MLU support'))
+                not IS_MLU_AVAILABLE, reason='requires MLU support')),
+        pytest.param(
+            'npu',
+            marks=pytest.mark.skipif(
+                not IS_NPU_AVAILABLE, reason='requires NPU support'))
     ])
     @pytest.mark.parametrize('dtype', [
         torch.float,
