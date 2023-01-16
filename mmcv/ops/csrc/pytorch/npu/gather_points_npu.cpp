8e7c16ae0f751aeb5564b2c3f420f2e3fe8e1460
zcc9707 <zhaochenchen9@huawei.com>
2023-01-16 14:24:46 +0800

    更改格式内容

--- mmcv/ops/csrc/pytorch/npu/gather_points_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/gather_points_npu.cpp
@@ -4,23 +4,23 @@ using namespace NPU_NAME_SPACE;
 using namespace std;
 
 void gather_points_forward_npu(int b, int c, int n, int npoints,
-                               const Tensor points, const Tensor idx, Tensor out)
-{
-    c10::SmallVector<int64_t, N> axis = {2};
-    int64_t batch_dims = 1;
+                               const Tensor points, const Tensor idx,
+                               Tensor out) {
+  c10::SmallVector<int64_t, N> axis = {2};
+  int64_t batch_dims = 1;
 
-    OpCommand cmd;
-    cmd.Name("GatherV2")
-        .Input(points)
-        .Input(idx)
-        .Input(axis)
-        .Output(out)
-        .Attr("batch_dims", batch_dims)
-        .Run();
+  OpCommand cmd;
+  cmd.Name("GatherV2")
+      .Input(points)
+      .Input(idx)
+      .Input(axis)
+      .Output(out)
+      .Attr("batch_dims", batch_dims)
+      .Run();
 }
 
 void gather_points_forward_impl(int b, int c, int n, int npoints,
-                                const Tensor points, const Tensor idx, Tensor out);
+                                const Tensor points, const Tensor idx,
+                                Tensor out);
 
-REGISTER_NPU_IMPL(gather_points_forward_impl,
-                  gather_points_forward_npu);
+REGISTER_NPU_IMPL(gather_points_forward_impl, gather_points_forward_npu);
