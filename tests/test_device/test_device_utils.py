6ccd9a63fd86a9f3fe8d4459935e654dab5e4d69
Wenyi Tang <wenyi.tang@intel.com>
2023-03-10 19:47:18 +0800

    unittest error fix

--- tests/test_device/test_device_utils.py
+++ tests/test_device/test_device_utils.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.device import get_device
 from mmcv.utils import (IS_CUDA_AVAILABLE, IS_MLU_AVAILABLE, IS_MPS_AVAILABLE,
-                        IS_NPU_AVAILABLE, IS_XPU_AVAIABLE)
+                        IS_NPU_AVAILABLE, IS_XPU_AVAILABLE)
 
 
 def test_get_device():
@@ -14,7 +14,7 @@ def test_get_device():
         assert current_device == 'mlu'
     elif IS_MPS_AVAILABLE:
         assert current_device == 'mps'
-    elif IS_XPU_AVAIABLE:
+    elif IS_XPU_AVAILABLE:
         assert current_device == 'xpu'
     else:
         assert current_device == 'cpu'
