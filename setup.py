9be08c7bd923d9eb3f8377dba5c47df7b512d9cb
cokedong <408244909@qq.com>
2023-03-15 15:43:26 +0800

    Replace with diopi_impl

--- setup.py
+++ setup.py
@@ -308,10 +308,10 @@ def get_extensions():
                 glob.glob('./mmcv/ops/csrc/pytorch/cuda/*.cu') + \
                 glob.glob('./mmcv/ops/csrc/pytorch/cuda/*.cpp')
             if os.getenv('MMCV_WITH_DIOPI', '0') == '1':
-                op_files += glob.glob('./mmcv/ops/csrc/diopi_impl/cuda/*.cpp')
-                # op_files += glob.glob('./mmcv/ops/csrc/DIOPI-IMPL/cuda/*.cu')
                 # op_files += glob.glob(
-                #     './mmcv/ops/csrc/diopi_impl/cuda_native/*.cu')
+                #     './mmcv/ops/csrc/diopi_impl/cuda/*.cpp')
+                op_files += glob.glob(
+                    './mmcv/ops/csrc/DIOPI-IMPL/cuda/functions/*.cu')
                 op_files += glob.glob('./mmcv/ops/csrc/diopi_rt/torch/*.cpp')
             extension = CUDAExtension
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/pytorch'))
