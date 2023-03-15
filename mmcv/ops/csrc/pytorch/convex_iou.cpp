daf58877f5b8a47a79e57b665b2ca63a459ef309
cokedong <408244909@qq.com>
2023-03-15 19:33:52 +0800

    adapt convex_iou

--- mmcv/ops/csrc/pytorch/convex_iou.cpp
+++ mmcv/ops/csrc/pytorch/convex_iou.cpp
@@ -3,6 +3,11 @@
 // https://github.com/SDL-GuoZonghao/BeyondBoundingBox/tree/main/mmdet/ops/iou/src
 #include "pytorch_cpp_helper.hpp"
 #include "pytorch_device_registry.hpp"
+#ifdef MMCV_WITH_DIOPI
+#include <diopi/diopirt.h>
+#include <diopi/functions.h>
+#include "diopi.hpp"
+#endif
 
 void convex_iou_impl(const Tensor pointsets, const Tensor polygons,
                      Tensor ious) {
@@ -10,7 +15,22 @@ void convex_iou_impl(const Tensor pointsets, const Tensor polygons,
 }
 
 void convex_iou(const Tensor pointsets, const Tensor polygons, Tensor ious) {
+#ifdef MMCV_WITH_DIOPI
+  auto pointsets_p = reinterpret_cast<diopiConstTensorHandle_t>(&pointsets);
+  diopiDevice_t device;
+  diopiGetTensorDevice(pointsets_p, &device);
+  if (device == diopi_host) {
+      convex_iou_impl(pointsets, polygons, ious);
+      return;
+  }
+  diopiContext ctx;
+  diopiContextHandle_t ch = &ctx;
+  auto polygons_p = reinterpret_cast<diopiConstTensorHandle_t>(&polygons);
+  auto ious_p = reinterpret_cast<diopiTensorHandle_t>(&ious);
+  diopiConvexIou(ch, pointsets_p, polygons_p, ious_p);
+#else
   convex_iou_impl(pointsets, polygons, ious);
+#endif
 }
 
 void convex_giou_impl(const Tensor pointsets, const Tensor polygons,
@@ -19,5 +39,20 @@ void convex_giou_impl(const Tensor pointsets, const Tensor polygons,
 }
 
 void convex_giou(const Tensor pointsets, const Tensor polygons, Tensor output) {
+#ifdef MMCV_WITH_DIOPI
+  auto pointsets_p = reinterpret_cast<diopiConstTensorHandle_t>(&pointsets);
+  diopiDevice_t device;
+  diopiGetTensorDevice(pointsets_p, &device);
+  if (device == diopi_host) {
+      convex_giou_impl(pointsets, polygons, output);
+      return;
+  }
+  diopiContext ctx;
+  diopiContextHandle_t ch = &ctx;
+  auto polygons_p = reinterpret_cast<diopiConstTensorHandle_t>(&polygons);
+  auto output_p = reinterpret_cast<diopiTensorHandle_t>(&output);
+  diopiConvexGiou(ch, pointsets_p, polygons_p, output_p);
+#else
   convex_giou_impl(pointsets, polygons, output);
+#endif
 }
