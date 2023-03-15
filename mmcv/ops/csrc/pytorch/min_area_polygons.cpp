34ac2eb4eafb3245eaaf38bffabb41292f51ddb8
cokedong <408244909@qq.com>
2023-03-15 19:59:37 +0800

    adapt min_area_polygons

--- mmcv/ops/csrc/pytorch/min_area_polygons.cpp
+++ mmcv/ops/csrc/pytorch/min_area_polygons.cpp
@@ -1,11 +1,30 @@
 // Copyright (c) OpenMMLab. All rights reserved
 #include "pytorch_cpp_helper.hpp"
 #include "pytorch_device_registry.hpp"
+#ifdef MMCV_WITH_DIOPI
+#include <diopi/diopirt.h>
+#include <diopi/functions.h>
+#include "diopi.hpp"
+#endif
 
 void min_area_polygons_impl(const Tensor pointsets, Tensor polygons) {
   DISPATCH_DEVICE_IMPL(min_area_polygons_impl, pointsets, polygons);
 }
 
 void min_area_polygons(const Tensor pointsets, Tensor polygons) {
+#ifdef MMCV_WITH_DIOPI
+  auto pointsets_p = reinterpret_cast<diopiConstTensorHandle_t>(&pointsets);
+  diopiDevice_t device;
+  diopiGetTensorDevice(pointsets_p, &device);
+  if (device == diopi_host) {
+      min_area_polygons_impl(pointsets, polygons);
+      return;
+  }
+  diopiContext ctx;
+  diopiContextHandle_t ch = &ctx;
+  auto polygons_p = reinterpret_cast<diopiTensorHandle_t>(&polygons);
+  diopiMinAreaPolygons(ch, pointsets_p, polygons_p);
+#else
   min_area_polygons_impl(pointsets, polygons);
+#endif
 }
