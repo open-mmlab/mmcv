aa366ca3e808d70d221ae5460d1b8557ffd57d1c
root <root@DESKTOP-C50HHUN.localdomain>
2023-03-16 16:44:42 +0800

    magic number

--- mmcv/ops/nms.py
+++ mmcv/ops/nms.py
@@ -460,8 +460,10 @@ def nms_rotated(dets: Tensor,
         input_labels = labels
     if dets.device.type == 'npu':
         order = scores.new_empty(0, dtype=torch.long)
+        # 180 / PI = 57.29578
+        coefficient = 57.29578
         for i in range(dets.size()[0]):
-            dets_cw[i][4] *= 57.29578  # radians to angle
+            dets_cw[i][4] *= coefficient  # radians to angle
         keep_inds = ext_module.nms_rotated(dets_cw, scores, order, dets_cw,
                                            input_labels, iou_threshold,
                                            multi_label)
