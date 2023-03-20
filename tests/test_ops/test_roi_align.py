b6834bd42857c101a43ee04054111dd31252da32
momo609 <963372609.qq.com>
2023-03-20 09:43:32 +0800

    add unit test

--- tests/test_ops/test_roi_align.py
+++ tests/test_ops/test_roi_align.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 import torch
 
-from mmcv.utils import IS_CUDA_AVAILABLE, IS_MLU_AVAILABLE
+from mmcv.utils import IS_CUDA_AVAILABLE, IS_MLU_AVAILABLE, IS_NPU_AVAILABLE
 
 _USING_PARROTS = True
 try:
@@ -102,7 +102,11 @@ def _test_roialign_allclose(device, dtype):
     pytest.param(
         'mlu',
         marks=pytest.mark.skipif(
-            not IS_MLU_AVAILABLE, reason='requires MLU support'))
+            not IS_MLU_AVAILABLE, reason='requires MLU support')),
+    pytest.param(
+        'npu',
+        marks=pytest.mark.skipif(
+            not IS_NPU_AVAILABLE, reason='requires NPU support'))
 ])
 @pytest.mark.parametrize('dtype', [
     torch.float,
@@ -111,6 +115,11 @@ def _test_roialign_allclose(device, dtype):
         marks=pytest.mark.skipif(
             IS_MLU_AVAILABLE,
             reason='MLU does not support for 64-bit floating point')),
+    pytest.param(
+        torch.double,
+        marks=pytest.mark.skipif(
+            IS_NPU_AVAILABLE,
+            reason='NPU does not support for 64-bit floating point')),
     torch.half
 ])
 def test_roialign(device, dtype):
