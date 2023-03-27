41699bbb01f0a1e7abc2a8f9dc7b5d61502d3b7c
zhouzaida <zhouzaida@163.com>
2023-03-27 11:51:05 +0800

    install cuda
    install cuda
    
    install cuda
    
    install cuda
    
    install cuda
    
    install cuda
    
    install cuda
    
    fix env
    
    fix env
    
    fix env
    
    fix env
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix circleci
    
    fix circleci
    
    remove cuda11.1 test from windows
    
    update ci
    
    update ci
    
    update ci
    
    update ci
    
    update ci
    
    update ci
    
    update ci
    
    update ci

--- setup.py
+++ setup.py
@@ -327,6 +327,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Utilities',
     ],
     url='https://github.com/open-mmlab/mmcv',
