602ce911dd5376e61d4a4f320728d91ac44c2bdf
wicky-zheng <wicky-zheng@outlook.com>
2023-04-03 16:02:23 +0800

    [Feature] ms_deform_attn performance optimization V3

--- mmcv/ops/csrc/pytorch/mlu/ms_deform_attn_mlu.cpp
+++ mmcv/ops/csrc/pytorch/mlu/ms_deform_attn_mlu.cpp
@@ -9,9 +9,9 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *************************************************************************/
+#include "mlu_common_helper.h"
 #include "pytorch_device_registry.hpp"
 #include "pytorch_mlu_helper.hpp"
-#include "mlu_common_helper.h"
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
