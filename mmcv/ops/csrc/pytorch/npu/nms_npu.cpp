5c6bd19afca425c0d46e3a82b4f87c7fe716819b
sherie <963372609@qq.com>
2023-04-03 08:59:49 +0800

    update error message
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmcv/ops/csrc/pytorch/npu/nms_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/nms_npu.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 Tensor nms_npu(Tensor boxes, Tensor scores, float iou_threshold, int offset) {
   TORCH_CHECK((boxes.scalar_type == at::ScalarType::Float),
-              "The boxes tensor in NMS op need float dtype, does not support "
-              "half dtype.");
+              "The type of boxes tensor passed in nms_npu should be float");
   int64_t offset_64 = offset;
   at::Tensor iou_threshold_y = at_npu::native::OpPreparation::ApplyTensor(
                                    {}, boxes.options().dtype(at::kFloat), boxes)
