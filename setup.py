531fc172b4c38f61fc399cd4d8895fff0eca2f16
zhouzaida <zhouzaida@163.com>
2023-04-04 11:17:36 +0800

    do not test python3.11

--- setup.py
+++ setup.py
@@ -327,7 +327,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Topic :: Utilities',
     ],
     url='https://github.com/open-mmlab/mmcv',
