eea7889a91421362f835eb3fca6113b9a155c130
lizhihao6 <lizhihao6@outlook.com>
2023-04-12 08:06:57 +0000

    edit test config

--- tests/test_ops/test_neighborhood_attention.py
+++ tests/test_ops/test_neighborhood_attention.py
@@ -12,38 +12,34 @@ def _priv_test_allclose_cuda(
     batch_size,
     height,
     width,
-    kernel_sizes=[3, 5, 7, 9],
-    dims=[4, 8],
-    heads=[1, 2, 3],
-    tol=1e-8,
+    kernel_size,
+    dim,
+    num_heads,
+    qkv_bias,
     device='cuda',
     dtype=torch.float32,
 ):
-    for kernel_size in kernel_sizes:
-        for dim in dims:
-            for num_heads in heads:
-                for qkv_bias in [True, False]:
-                    model_kwargs = {
-                        'dim': dim * num_heads,
-                        'kernel_size': kernel_size,
-                        'num_heads': num_heads,
-                        'qkv_bias': qkv_bias,
-                    }
-
-                    base_state_dict = NeighborhoodAttention(
-                        **model_kwargs).state_dict()
-
-                    x = torch.randn((batch_size, height, width,
-                                     dim * num_heads)).to(device)
-
-                    nat = NeighborhoodAttention(
-                        **model_kwargs).to(device).eval()
-                    nat.load_state_dict(base_state_dict, strict=False)
-
-                    y = nat(x)
-                    y.sum().backward()
+    model_kwargs = {
+        'dim': dim * num_heads,
+        'kernel_size': kernel_size,
+        'num_heads': num_heads,
+        'qkv_bias': qkv_bias,
+    }
+    base_state_dict = NeighborhoodAttention(**model_kwargs).state_dict()
+    x = torch.randn((batch_size, height, width, dim * num_heads),
+                    dtype=dtype).to(device)
+    nat = NeighborhoodAttention(**model_kwargs).to(device).eval()
+    if dtype == torch.half:
+        nat = nat.half()
+    nat.load_state_dict(base_state_dict, strict=False)
+    y = nat(x)
+    y.sum().backward()
 
 
+@pytest.mark.parametrize('kernel_size', [3, 5, 7, 9])
+@pytest.mark.parametrize('dim', [4, 8])
+@pytest.mark.parametrize('num_heads', [1, 2, 3])
+@pytest.mark.parametrize('qkv_bias', [True, False])
 @pytest.mark.parametrize('device', [
     pytest.param(
         'cuda',
@@ -51,6 +47,16 @@ def _priv_test_allclose_cuda(
             not torch.cuda.is_available(), reason='requires CUDA support'))
 ])
 @pytest.mark.parametrize('dtype', [torch.float, torch.half])
-def test_neighborhood_attention(device, dtype):
+def test_neighborhood_attention(kernel_size, dim, num_heads, qkv_bias, device,
+                                dtype):
     b, li, lj = 4, 14, 16
-    _priv_test_allclose_cuda(b, li, lj, device=device, dtype=dtype)
+    _priv_test_allclose_cuda(
+        b,
+        li,
+        lj,
+        kernel_size=kernel_size,
+        dim=dim,
+        num_heads=num_heads,
+        qkv_bias=qkv_bias,
+        device=device,
+        dtype=dtype)
