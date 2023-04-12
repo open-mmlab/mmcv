965dd102dc63684060989150f2984bd6e986b5a1
yangdian <yangdian@cambricon.com>
2023-04-12 16:42:40 +0800

    [WIP] Replace the implementation of roiAlign with mlu-ops

--- mmcv/ops/csrc/pytorch/mlu/roi_align_mlu.cpp
+++ mmcv/ops/csrc/pytorch/mlu/roi_align_mlu.cpp
@@ -26,14 +26,16 @@ void ROIAlignForwardMLUKernelLauncher(Tensor input, Tensor rois, Tensor output,
   int height = input.size(2);
   int width = input.size(3);
 
-  auto output_contiguous = at::empty({num_rois, channels, aligned_height, aligned_width},
-                           input.options(), memory_format);
+  auto output_contiguous =
+      at::empty({num_rois, channels, aligned_height, aligned_width},
+                input.options(), memory_format);
   // get tensor impl
   auto self_impl = torch_mlu::getMluTensorImpl(input_tensor);
   auto rois_impl = torch_mlu::getMluTensorImpl(rois);
   auto output_impl = torch_mlu::getMluTensorImpl(output_contiguous);
 
-  MluOpTensorDescriptor input_desc, rois_desc, argmax_y_desc, argmax_x_desc, output_desc;
+  MluOpTensorDescriptor input_desc, rois_desc, argmax_y_desc, argmax_x_desc,
+      output_desc;
   input_desc.set_with_layout(input_tensor, MLUOP_LAYOUT_NHWC);
   rois_desc.set_with_layout(rois, MLUOP_LAYOUT_ARRAY);
   output_desc.set_with_layout(output_contiguous, MLUOP_LAYOUT_NHWC);
@@ -45,43 +47,32 @@ void ROIAlignForwardMLUKernelLauncher(Tensor input, Tensor rois, Tensor output,
 
   mluOpRoiAlignForwardDescriptor_t roialign_desc;
   mluOpCreateRoiAlignForwardDescriptor(&roialign_desc);
-  mluOpSetRoiAlignForwardDescriptor_v2(
-    roialign_desc, aligned_height, aligned_width, sampling_ratio, spatial_scale,
-    pool_mode, aligned);
- 
+  mluOpSetRoiAlignForwardDescriptor_v2(roialign_desc, aligned_height,
+                                       aligned_width, sampling_ratio,
+                                       spatial_scale, pool_mode, aligned);
+
   auto handle = mluOpGetCurrentHandle();
   if (pool_mode == 0) {
     auto argmax_y_contiguous =
-      torch_mlu::cnnl::ops::cnnl_contiguous(argmax_y, memory_format);
+        torch_mlu::cnnl::ops::cnnl_contiguous(argmax_y, memory_format);
     auto argmax_x_contiguous =
-      torch_mlu::cnnl::ops::cnnl_contiguous(argmax_x, memory_format);
-    auto argmax_x_impl =
-      torch_mlu::getMluTensorImpl(argmax_x_contiguous);
-    auto argmax_y_impl =
-      torch_mlu::getMluTensorImpl(argmax_y_contiguous);
-    auto argmax_x_ptr =
-      argmax_x_impl->cnnlMalloc();
-    auto argmax_y_ptr =
-      argmax_y_impl->cnnlMalloc();
+        torch_mlu::cnnl::ops::cnnl_contiguous(argmax_x, memory_format);
+    auto argmax_x_impl = torch_mlu::getMluTensorImpl(argmax_x_contiguous);
+    auto argmax_y_impl = torch_mlu::getMluTensorImpl(argmax_y_contiguous);
+    auto argmax_x_ptr = argmax_x_impl->cnnlMalloc();
+    auto argmax_y_ptr = argmax_y_impl->cnnlMalloc();
     argmax_y_desc.set_with_layout(argmax_x_contiguous, MLUOP_LAYOUT_NHWC);
     argmax_x_desc.set_with_layout(argmax_x_contiguous, MLUOP_LAYOUT_NHWC);
-    mluOpRoiAlignForward_v2(
-        handle, roialign_desc,
-        input_desc.desc(), self_ptr,
-        rois_desc.desc(), rois_ptr,
-        output_desc.desc(), output_ptr,
-        argmax_x_desc.desc(), argmax_x_ptr,
-        argmax_y_desc.desc(), argmax_y_ptr);
+    mluOpRoiAlignForward_v2(handle, roialign_desc, input_desc.desc(), self_ptr,
+                            rois_desc.desc(), rois_ptr, output_desc.desc(),
+                            output_ptr, argmax_x_desc.desc(), argmax_x_ptr,
+                            argmax_y_desc.desc(), argmax_y_ptr);
     argmax_x.copy_(argmax_x_contiguous);
     argmax_y.copy_(argmax_y_contiguous);
   } else {
-    mluOpRoiAlignForward_v2(
-        handle, roialign_desc,
-        input_desc.desc(), self_ptr,
-        rois_desc.desc(), rois_ptr,
-        output_desc.desc(), output_ptr,
-        NULL, NULL,
-        NULL, NULL);
+    mluOpRoiAlignForward_v2(handle, roialign_desc, input_desc.desc(), self_ptr,
+                            rois_desc.desc(), rois_ptr, output_desc.desc(),
+                            output_ptr, NULL, NULL, NULL, NULL);
   }
   mluOpDestroyRoiAlignForwardDescriptor(roialign_desc);
   output.copy_(output_contiguous);
@@ -124,7 +115,8 @@ void ROIAlignBackwardMLUKernelLauncher(Tensor grad, Tensor rois,
   auto rois_ptr = rois_impl->cnnlMalloc();
   auto grad_input_ptr = grad_input_impl->cnnlMalloc();
 
-  MluOpTensorDescriptor grads_desc, rois_desc, argmax_y_desc, argmax_x_desc, grad_input_desc;
+  MluOpTensorDescriptor grads_desc, rois_desc, argmax_y_desc, argmax_x_desc,
+      grad_input_desc;
   grads_desc.set_with_layout(grad_, MLUOP_LAYOUT_NHWC);
   rois_desc.set_with_layout(rois, MLUOP_LAYOUT_ARRAY);
   grad_input_desc.set_with_layout(grad_input_, MLUOP_LAYOUT_NHWC);
@@ -132,38 +124,25 @@ void ROIAlignBackwardMLUKernelLauncher(Tensor grad, Tensor rois,
   auto handle = mluOpGetCurrentHandle();
   if (pool_mode == 0) {
     auto argmax_y_contiguous =
-      torch_mlu::cnnl::ops::cnnl_contiguous(argmax_y, memory_format);
+        torch_mlu::cnnl::ops::cnnl_contiguous(argmax_y, memory_format);
     auto argmax_x_contiguous =
-      torch_mlu::cnnl::ops::cnnl_contiguous(argmax_x, memory_format);
-    auto argmax_x_impl =
-      torch_mlu::getMluTensorImpl(argmax_x_contiguous);
-    auto argmax_y_impl =
-      torch_mlu::getMluTensorImpl(argmax_y_contiguous);
-    auto argmax_x_ptr =
-      argmax_x_impl->cnnlMalloc();
-    auto argmax_y_ptr =
-      argmax_y_impl->cnnlMalloc();
+        torch_mlu::cnnl::ops::cnnl_contiguous(argmax_x, memory_format);
+    auto argmax_x_impl = torch_mlu::getMluTensorImpl(argmax_x_contiguous);
+    auto argmax_y_impl = torch_mlu::getMluTensorImpl(argmax_y_contiguous);
+    auto argmax_x_ptr = argmax_x_impl->cnnlMalloc();
+    auto argmax_y_ptr = argmax_y_impl->cnnlMalloc();
     argmax_y_desc.set_with_layout(argmax_x_contiguous, MLUOP_LAYOUT_NHWC);
     argmax_x_desc.set_with_layout(argmax_x_contiguous, MLUOP_LAYOUT_NHWC);
-    mluOpRoiAlignBackward_v2(
-      handle, grads_desc.desc(),
-      grad_ptr, rois_desc.desc(),
-      rois_ptr, argmax_y_desc.desc(),
-      argmax_x_ptr, argmax_y_desc.desc(),
-      argmax_y_ptr, spatial_scale, sampling_ratio,
-      aligned, pool_mode,
-      grad_input_desc.desc(), grad_input_ptr);
-    argmax_x.copy_(argmax_x_contiguous);
-    argmax_y.copy_(argmax_y_contiguous);
+    mluOpRoiAlignBackward_v2(handle, grads_desc.desc(), grad_ptr,
+                             rois_desc.desc(), rois_ptr, argmax_y_desc.desc(),
+                             argmax_x_ptr, argmax_y_desc.desc(), argmax_y_ptr,
+                             spatial_scale, sampling_ratio, aligned, pool_mode,
+                             grad_input_desc.desc(), grad_input_ptr);
   } else {
-    mluOpRoiAlignBackward_v2(
-      handle, grads_desc.desc(),
-      grad_ptr, rois_desc.desc(),
-      rois_ptr, NULL,
-      NULL, NULL,
-      NULL, spatial_scale, sampling_ratio,
-      aligned, pool_mode,
-      grad_input_desc.desc(), grad_input_ptr);
+    mluOpRoiAlignBackward_v2(handle, grads_desc.desc(), grad_ptr,
+                             rois_desc.desc(), rois_ptr, NULL, NULL, NULL, NULL,
+                             spatial_scale, sampling_ratio, aligned, pool_mode,
+                             grad_input_desc.desc(), grad_input_ptr);
   }
   grad_input.copy_(grad_input_);
 }
