422ec554615f4970f0d3ee753b409aa0e4974916
萝卜菜 <ccw1996@outlook.com>
2023-04-18 22:31:44 +0800

    use async to accelerate sigmoid focal loss

--- mmcv/ops/csrc/pytorch/cpu/sigmoid_focal_loss.cpp
+++ mmcv/ops/csrc/pytorch/cpu/sigmoid_focal_loss.cpp
@@ -2,28 +2,29 @@
 #include "pytorch_device_registry.hpp"
 #include <cmath>
 #include <iostream>
+#include <vector>
 #include <cfloat>
+#include <future>
 
 template <typename T>
-void sigmoid_focal_loss_forward_cpu_kernel(int N, const T *input, const int64_t *target,
-                                           const T *weight, T *output,
-                                           const float gamma,
-                                           const float alpha, const int num_classes)
+void sigmoid_focal_loss_forward(int start, int end,const T *input, const int64_t *target,
+                                const T *weight, T *output,
+                                const float gamma,
+                                const float alpha, const int num_classes)
 {
-    for (int i = 0; i < N; i++)
+    for (int i = start; i < end; i++)
     {
         int64_t t = target[i / num_classes];
         int c = i % num_classes;
-        T p = (T)1. / ((T)1. + expf(-input[i]));
-        T p_t = p * target[i] + (1 - p) * (1 - target[i]);
+        T p = (T)1.0 / ((T)1.0 + expf(-input[i]));
         if (t == c)
         {
-            T term_p = pow(((T)1. - p), gamma) * log(std::max(p, (T)FLT_MIN));
+            T term_p = pow(((T)1. - p), gamma) * log(p);
             output[i] = -alpha * term_p;
         }
         else
         {
-            T term_n = pow(p, gamma) * log(std::max((T)1. - p, (T)FLT_MIN));
+            T term_n = pow(p, gamma) * log((T)1. - p);
             output[i] = -((T)1. - alpha) * term_n;
         }
         if (weight != NULL)
@@ -34,12 +35,36 @@ void sigmoid_focal_loss_forward_cpu_kernel(int N, const T *input, const int64_t 
 }
 
 template <typename T>
-void sigmoid_focal_loss_backward_cpu_kernel(const int N, const T *input, const int64_t *target,
-                                            const T *weight, T *grad_input,
-                                            const float gamma,
-                                            const float alpha, const int num_classes)
+void sigmoid_focal_loss_forward_cpu_kernel(int N, const T *input, const int64_t *target,
+                                           const T *weight, T *output,
+                                           const float gamma,
+                                           const float alpha, const int num_classes)
 {
-    for (int i = 0; i < N; i++)
+    if (N < 4)
+    {
+        sigmoid_focal_loss_forward(0, 3, input, target, weight, output, gamma, alpha, num_classes);
+    }
+    else
+    {
+        int num = N / 4;
+        std::vector<std::future<void>>ft(4);
+        for(int i=0;i<4;i++){
+            ft[i]=std::async(std::launch::async,&sigmoid_focal_loss_forward<T>,num*i,num*(i+1),input, target, weight, output, gamma, alpha, num_classes);
+        }
+        for(int i=0;i<4;i++){
+            ft[i].wait();
+        }
+        sigmoid_focal_loss_forward(num*4, N, input, target, weight, output, gamma, alpha, num_classes);
+    }
+}
+
+template <typename T>
+void sigmoid_focal_loss_backward(int start, int end,const T *input, const int64_t *target,
+                                const T *weight, T *grad_input,
+                                const float gamma,
+                                const float alpha, const int num_classes)
+{
+    for (int i = start; i < end; i++)
     {
         int64_t t = target[i / num_classes];
         int c = i % num_classes;
@@ -47,19 +72,43 @@ void sigmoid_focal_loss_backward_cpu_kernel(const int N, const T *input, const i
         if (t == c)
         {
             T term_p = pow(((T)1. - p), gamma) *
-                       ((T)1. - p - (gamma * p * log(std::max(p, (T)FLT_MIN))));
+                       ((T)1. - p - (gamma * p * log(p)));
             grad_input[i] = -alpha * term_p;
         }
         else
         {
             T term_n = pow(p, gamma) *
-                       (gamma * ((T)1. - p) * log(std::max((T)1. - p, (T)FLT_MIN)) - p);
+                       (gamma * ((T)1. - p) * log((T)1. - p) - p);
             grad_input[i] = -((T)1. - alpha) * term_n;
         }
         if (weight != NULL)
         {
             grad_input[i] *= weight[t];
         }
+    }
+}
+
+template <typename T>
+void sigmoid_focal_loss_backward_cpu_kernel(const int N, const T *input, const int64_t *target,
+                                            const T *weight, T *grad_input,
+                                            const float gamma,
+                                            const float alpha, const int num_classes)
+{
+    if (N < 4)
+    {
+        sigmoid_focal_loss_backward(0, 3, input, target, weight, grad_input, gamma, alpha, num_classes);
+    }
+    else
+    {
+        int num = N / 4;
+        std::vector<std::future<void>>ft(4);
+        for(int i=0;i<4;i++){
+            ft[i]=std::async(std::launch::async,&sigmoid_focal_loss_backward<T>,num*i,num*(i+1),input, target, weight, grad_input, gamma, alpha, num_classes);
+        }
+        for(int i=0;i<4;i++){
+            ft[i].wait();
+        }
+        sigmoid_focal_loss_backward(num*4, N, input, target, weight, grad_input, gamma, alpha, num_classes);
     }
 }
 
