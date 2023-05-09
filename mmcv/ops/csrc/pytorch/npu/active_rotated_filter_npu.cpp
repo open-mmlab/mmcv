67189b273404126f3c1b827799967de89bd682ed
dflhw <595869386@qq.com>
2023-05-09 11:18:53 +0800

    fix lint

--- mmcv/ops/csrc/pytorch/npu/active_rotated_filter_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/active_rotated_filter_npu.cpp
@@ -9,8 +9,8 @@ void active_rotated_filter_forward_impl(const Tensor input,
 void active_rotated_filter_backward_impl(const Tensor grad_out,
                                          const Tensor indices, Tensor grad_in);
 
-void active_rotated_filter_forward_npu(const Tensor input,
-                                       const Tensor indices, Tensor output) {
+void active_rotated_filter_forward_npu(const Tensor input, const Tensor indices,
+                                       Tensor output) {
   OpCommand cmd;
   cmd.Name("ActiveRotatedFilter")
       .Input(input)
