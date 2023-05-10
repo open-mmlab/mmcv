7fd3844ec2a261d6331a72ee40a49866db3fbac5
cokedong <408244909@qq.com>
2023-05-10 15:08:04 +0800

    Fix

--- tests/test_ops/test_bbox.py
+++ tests/test_ops/test_bbox.py
@@ -60,10 +60,9 @@ class TestBBox:
         pytest.param(
             'npu',
             marks=pytest.mark.skipif(
-                not IS_NPU_AVAILABLE, reason='requires NPU support')),
+                not IS_NPU_AVAILABLE, reason='requires NPU support'))
     ])
     def test_bbox_overlaps_float(self, device):
-        print("\ndevice " +device+"\n")
         self._test_bbox_overlaps(device, dtype=torch.float)
 
     @pytest.mark.parametrize('device', [
@@ -78,8 +77,7 @@ class TestBBox:
         pytest.param(
             'npu',
             marks=pytest.mark.skipif(
-                not IS_NPU_AVAILABLE, reason='requires NPU support')),
+                not IS_NPU_AVAILABLE, reason='requires NPU support'))
     ])
     def test_bbox_overlaps_half(self, device):
-        print("\ndevice " +device+"\n")
         self._test_bbox_overlaps(device, dtype=torch.half)
