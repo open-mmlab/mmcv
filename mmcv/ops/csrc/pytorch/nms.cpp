fc79564e3c9f5e17ebbd65a22c9e9f6f3d508603
cokedong <408244909@qq.com>
2023-05-24 15:41:44 +0800

    Fix nms for none output

--- mmcv/ops/csrc/pytorch/nms.cpp
+++ mmcv/ops/csrc/pytorch/nms.cpp
@@ -53,9 +53,7 @@ Tensor nms(Tensor boxes, Tensor scores, float iou_threshold, int offset) {
   LOG(WARNING) << "Fallback to cpu: mmcv ext op nms";
   auto boxes_cpu = boxes.cpu();
   auto scores_cpu = scores.cpu();
-  auto out_cpu = nms_impl(boxes_cpu, scores_cpu, iou_threshold, offset);
-  out.copy_(out_cpu);
-  return out;
+  return nms_impl(boxes_cpu, scores_cpu, iou_threshold, offset);;
 #else
   return nms_impl(boxes, scores, iou_threshold, offset);
 #endif
