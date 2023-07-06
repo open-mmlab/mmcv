43c5c76f9bfec8632dff024ce46bd28fa78d0c36
long11111111 <48040992+long11111111@users.noreply.github.com>
2023-07-06 15:34:51 +0800

    [Feature] Add support of points_in_polyogns for Ascend device (#2848)

--- mmcv/ops/points_in_polygons.py
+++ mmcv/ops/points_in_polygons.py
@@ -31,8 +31,11 @@ def points_in_polygons(points: Tensor, polygons: Tensor) -> Tensor:
     assert polygons.shape[1] == 8, \
         'polygons dimension should be 8, ' \
         f'but got unexpected shape {polygons.shape[1]}'
-    output = torch.full([points.shape[0], polygons.shape[0]],
-                        0.).cuda().float()
+    output = torch.zeros(
+        points.shape[0],
+        polygons.shape[0],
+        dtype=torch.float32,
+        device=points.device)
     ext_module.points_in_polygons_forward(points.contiguous(),
                                           polygons.contiguous(), output)
     return output
--- tests/test_ops/test_points_in_polygons.py
+++ tests/test_ops/test_points_in_polygons.py
@@ -4,20 +4,29 @@ import pytest
 import torch
 
 from mmcv.ops import points_in_polygons
+from mmcv.utils import IS_CUDA_AVAILABLE, IS_NPU_AVAILABLE
 
 
-@pytest.mark.skipif(
-    not torch.cuda.is_available(), reason='requires CUDA support')
-def test_points_in_polygons():
+@pytest.mark.parametrize('device', [
+    pytest.param(
+        'cuda',
+        marks=pytest.mark.skipif(
+            not IS_CUDA_AVAILABLE, reason='requires CUDA support')),
+    pytest.param(
+        'npu',
+        marks=pytest.mark.skipif(
+            not IS_NPU_AVAILABLE, reason='requires NPU support'))
+])
+def test_points_in_polygons(device):
     points = np.array([[300., 300.], [400., 400.], [100., 100], [300, 250],
                        [100, 0]])
     polygons = np.array([[200., 200., 400., 400., 500., 200., 400., 100.],
                          [400., 400., 500., 500., 600., 300., 500., 200.],
                          [300., 300., 600., 700., 700., 700., 700., 100.]])
     expected_output = np.array([[0., 0., 0.], [0., 0., 1.], [0., 0., 0.],
-                                [1., 0., 0.], [0., 0., 0.]])
-    points = torch.from_numpy(points).cuda().float()
-    polygons = torch.from_numpy(polygons).cuda().float()
-    expected_output = torch.from_numpy(expected_output).cuda().float()
-    assert torch.allclose(
-        points_in_polygons(points, polygons), expected_output, 1e-3)
+                                [1., 0., 0.], [0., 0., 0.]]).astype(np.float32)
+    points = torch.tensor(points, dtype=torch.float32, device=device)
+    polygons = torch.tensor(polygons, dtype=torch.float32, device=device)
+    assert np.allclose(
+        points_in_polygons(points, polygons).cpu().numpy(), expected_output,
+        1e-3)
