fix softmax focal loss algorithm
Signed-off-by: Qingpeng Li <qingpeng9802@gmail.com>

mmcv/ops/focal_loss.py
@@ -139,12 +139,15 @@ class SoftmaxFocalLossFunction(Function):
         channel_stats = input_softmax.sum(dim=1)
         input_softmax /= channel_stats.unsqueeze(1).expand_as(input)
 
-        output = input.new_zeros(input.size(0))
+        log_softmax_prob = input.new_zeros(input.size())
+        output = input.new_zeros(input.size())
+
         ext_module.softmax_focal_loss_forward(
-            input_softmax,
+            input,
             target,
             weight,
             output,
+            log_softmax_prob,
             gamma=ctx.gamma,
             alpha=ctx.alpha)
 
@@ -152,27 +155,27 @@ class SoftmaxFocalLossFunction(Function):
             output = output.sum() / input.size(0)
         elif ctx.reduction == ctx.reduction_dict['sum']:
             output = output.sum()
-        ctx.save_for_backward(input_softmax, target, weight)
+        ctx.save_for_backward(log_softmax_prob, target, weight)
         return output
 
     @staticmethod
+    @once_differentiable
     def backward(ctx, grad_output: torch.Tensor) -> tuple:
-        input_softmax, target, weight = ctx.saved_tensors
-        buff = input_softmax.new_zeros(input_softmax.size(0))
-        grad_input = input_softmax.new_zeros(input_softmax.size())
+        log_softmax_prob, target, weight = ctx.saved_tensors
+
+        grad_input = log_softmax_prob.new_zeros(log_softmax_prob.size())
 
         ext_module.softmax_focal_loss_backward(
-            input_softmax,
+            log_softmax_prob,
             target,
             weight,
-            buff,
             grad_input,
             gamma=ctx.gamma,
             alpha=ctx.alpha)
 
         grad_input *= grad_output
         if ctx.reduction == ctx.reduction_dict['mean']:
-            grad_input /= input_softmax.size(0)
+            grad_input /= log_softmax_prob.size(0)
         return grad_input, None, None, None, None, None
 
 