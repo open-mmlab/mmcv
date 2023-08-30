7fa67a1f38b12040de4f5a80039e6e8eec6052dd
Qingpeng Li <qingpeng9802@gmail.com>
2023-08-30 23:45:02 +0800

    revert `__ldg`
    Signed-off-by: Qingpeng Li <qingpeng9802@gmail.com>

--- mmcv/ops/csrc/common/cuda/softmax_focal_loss_cuda_kernel.cuh
+++ mmcv/ops/csrc/common/cuda/softmax_focal_loss_cuda_kernel.cuh
@@ -33,12 +33,12 @@ __global__ void softmax_focal_loss_forward_cuda_kernel(
     const T expsum = [&] {
       T expsum = 0;
       for(int c_idx = start; c_idx < end; ++c_idx) {
-        expsum += exp(input[c_idx] - __ldg(&max_val_along_class));
+        expsum += exp(input[c_idx] - max_val_along_class);
       }
       return expsum;
     }();
 
-    const T log_pred = input[index] - __ldg(&max_val_along_class) - log(expsum);
+    const T log_pred = input[index] - max_val_along_class - log(expsum);
     log_softmax_prob[index] = log_pred;
 
     // focal loss
