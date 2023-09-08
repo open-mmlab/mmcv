72084061b2572a61202e2e231e28d77ac1744c7c
Chris Jiang <43343691+ClowDragon@users.noreply.github.com>
2023-09-08 10:43:56 +0800

    [Fix] Update mlu-ops version (#2921)

--- mmcv/ops/csrc/pytorch/mlu/mlu_common_helper.h
+++ mmcv/ops/csrc/pytorch/mlu/mlu_common_helper.h
@@ -19,7 +19,7 @@
 
 #define MLUOP_MAJOR 0
 #define MLUOP_MINOR 8
-#define MLUOP_PATCHLEVEL 0
+#define MLUOP_PATCHLEVEL 1
 
 /*************************************************************************
  * This MACRO contains operations of simple tensor to mlu-tensor.
