1e0fc42570e65d654080cbeeef460ba31b84b4b9
Chai <673958639@qq.com>
2023-12-27 10:27:21 +0800

    fix lint

--- mmcv/ops/csrc/pytorch/npu/stack_group_points_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/stack_group_points_npu.cpp
@@ -11,13 +11,13 @@ void stacK_group_points_forward_npu(int b, int c, int n, int nsample,
                features_batch_cnt_tensor, idx_tensor, idx_batch_cnt_tensor,
                out_tensor);
 }
- 
+
 void stack_group_points_forward_impl(int b, int c, int n, int nsample,
                                      const Tensor features_tensor,
                                      const Tensor features_batch_cnt_tensor,
                                      const Tensor idx_tensor,
                                      const Tensor idx_batch_cnt_tensor,
                                      Tensor out_tensor);
- 
+
 REGISTER_NPU_IMPL(stack_group_points_forward_impl,
                   stack_group_points_forward_npu);
