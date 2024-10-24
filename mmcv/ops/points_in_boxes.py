650c3218f845d13e7819aceb34585b209266ff5d
张帮政 <zhangbangzheng@huawei.com>
2024-10-24 15:49:51 +0800

    points_in_boxes_all的mmcv适配

--- mmcv/ops/points_in_boxes.py
+++ mmcv/ops/points_in_boxes.py
@@ -130,9 +130,11 @@ def points_in_boxes_all(points: Tensor, boxes: Tensor) -> Tensor:
     points_device = points.get_device()
     assert points_device == boxes.get_device(), \
         'Points and boxes should be put on the same device'
-    if torch.cuda.current_device() != points_device:
-        torch.cuda.set_device(points_device)
-
+    if points.device.type != 'npu':
+        if torch.cuda.current_device() != points_device:
+            torch.cuda.set_device(points_device)
+    elif points.device.type == 'npu':
+        boxes[:, :, 2] += boxes[:, :, 5] / 2.0
     ext_module.points_in_boxes_all_forward(boxes.contiguous(),
                                            points.contiguous(),
                                            box_idxs_of_pts)
