a752d175c0406849003852f2d9c3bab5eb4ab163
Jing-Yi Li <lijingyi21@huawei.com>
2024-12-16 08:24:13 +0800

    modify internal calls of npu boxes_overlap_bev & box_iou_rotated

--- mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
@@ -10,16 +10,19 @@ void iou3d_boxes_overlap_bev_forward_impl(const int num_a, const Tensor boxes_a,
 void iou3d_boxes_overlap_bev_forward_npu(const int num_a, const Tensor boxes_a,
                                          const int num_b, const Tensor boxes_b,
                                          Tensor ans_overlap) {
+  TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
+  TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
 
-    TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
-    TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
+  auto format_flag = 3;
+  auto clockwise = true;
+  auto mode_flag = 0;
+  auto aligned = false;
+  auto margin = 1e-2;
 
-    auto trans = false;
-    auto is_clockwise = false;
-    auto aligned = false;
-    auto mode_flag = 2;
-    EXEC_NPU_CMD(aclnnBoxesOverlapBev, boxes_a, boxes_b, trans, is_clockwise, aligned, mode_flag, ans_overlap);
-    return;
+  EXEC_NPU_CMD(aclnnBoxesOverlapBev, boxes_a, boxes_b, format_flag, clockwise,
+               mode_flag, aligned, margin, ans_overlap);
+  return;
 }
 
-REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl, iou3d_boxes_overlap_bev_forward_npu);
+REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl,
+                  iou3d_boxes_overlap_bev_forward_npu);
