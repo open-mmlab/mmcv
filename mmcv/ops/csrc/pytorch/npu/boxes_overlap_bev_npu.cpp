32cfb9a345e5bb4bf60aed63530cfa9f33c29138
JYYCaN <32977726+JYYCaN@users.noreply.github.com>
2025-03-25 18:52:24 +0800

    Update boxes_overlap_bev_npu.cpp
    更换 boxes_overlap_bev_npu 的底层 aclnn 调用算子

--- mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
@@ -3,6 +3,11 @@
 using namespace NPU_NAME_SPACE;
 using namespace std;
 
+namespace {
+constexpr int32_t MODE_FLAG_OVERLAP = 0;
+constexpr int32_t FORMAT_FLAG_XYZWHDR = 3;
+};
+
 void iou3d_boxes_overlap_bev_forward_impl(const int num_a, const Tensor boxes_a,
                                           const int num_b, const Tensor boxes_b,
                                           Tensor ans_overlap);
@@ -10,19 +15,18 @@ void iou3d_boxes_overlap_bev_forward_impl(const int num_a, const Tensor boxes_a,
 void iou3d_boxes_overlap_bev_forward_npu(const int num_a, const Tensor boxes_a,
                                          const int num_b, const Tensor boxes_b,
                                          Tensor ans_overlap) {
-  TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
-  TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
 
-  auto format_flag = 3;
-  auto clockwise = true;
-  auto mode_flag = 0;
-  auto aligned = false;
-  auto margin = 1e-2;
+    TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
+    TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
 
-  EXEC_NPU_CMD(aclnnBoxesOverlapBev, boxes_a, boxes_b, format_flag, clockwise,
-               mode_flag, aligned, margin, ans_overlap);
-  return;
+    auto clockwise = true;
+    bool aligned = false;
+    double margin = 1e-5;
+    int32_t mode_flag = MODE_FLAG_OVERLAP;
+    int32_t format_flag = FORMAT_FLAG_XYZWHDR;
+
+    EXEC_NPU_CMD(aclnnBoxesOverlapBevV1, boxes_a, boxes_b, format_flag, clockwise, mode_flag, aligned, margin, ans_overlap);
+    return;
 }
 
-REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl,
-                  iou3d_boxes_overlap_bev_forward_npu);
+REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl, iou3d_boxes_overlap_bev_forward_npu);
