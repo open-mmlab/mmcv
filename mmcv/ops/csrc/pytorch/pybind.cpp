5cdb4929e71bf993b5d71d7f3bd0f8fdf6d950bd
黄源 <huangyuan64@huawei.com>
2025-03-25 16:14:52 +0800

    remove the roi align rotated v2 ops

--- mmcv/ops/csrc/pytorch/pybind.cpp
+++ mmcv/ops/csrc/pytorch/pybind.cpp
@@ -208,17 +208,6 @@ void roi_align_backward(Tensor grad_output, Tensor rois, Tensor argmax_y,
                         int aligned_width, float spatial_scale,
                         int sampling_ratio, int pool_mode, bool aligned);
 
-void roi_align_rotated_v2_forward(Tensor input, Tensor rois, Tensor output,
-                                  double spatial_scale, int sampling_ratio,
-                                  int aligned_height, int aligned_width,
-                                  bool aligned, bool clockwise);
-
-void roi_align_rotated_v2_backward(Tensor input, Tensor rois,
-                                   Tensor grad_output, Tensor grad_input,
-                                   int pooled_height, int pooled_width,
-                                   double spatial_scale, int sampling_ratio,
-                                   bool aligned, bool clockwise);
-
 void roi_pool_forward(Tensor input, Tensor rois, Tensor output, Tensor argmax,
                       int pooled_height, int pooled_width, float spatial_scale);
 
@@ -801,16 +790,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("roi_align_rotated_backward", &roi_align_rotated_backward,
         "roi_align_rotated backward", py::arg("rois"), py::arg("grad_input"),
         py::arg("grad_output"), py::arg("pooled_height"),
-        py::arg("pooled_width"), py::arg("spatial_scale"),
-        py::arg("sampling_ratio"), py::arg("aligned"), py::arg("clockwise"));
-  m.def("roi_align_rotated_v2_forward", &roi_align_rotated_v2_forward,
-        "roi_align_rotated_v2_forward", py::arg("input"), py::arg("rois"),
-        py::arg("output"), py::arg("spatial_scale"), py::arg("sampling_ratio"),
-        py::arg("pooled_height"), py::arg("pooled_width"), py::arg("aligned"),
-        py::arg("clockwise"));
-  m.def("roi_align_rotated_v2_backward", &roi_align_rotated_v2_backward,
-        "roi_align_rotated_v2_backward", py::arg("input"), py::arg("rois"),
-        py::arg("grad_output"), py::arg("grad_input"), py::arg("pooled_height"),
         py::arg("pooled_width"), py::arg("spatial_scale"),
         py::arg("sampling_ratio"), py::arg("aligned"), py::arg("clockwise"));
   m.def("dynamic_point_to_voxel_forward", &dynamic_point_to_voxel_forward,
