dc76d97a54c676df7b4ad60428fcf9c96a6cf95e
JYYCaN <32977726+JYYCaN@users.noreply.github.com>
2025-04-08 18:50:50 +0800

    Update boxes_overlap_bev_npu.cpp

--- mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
@@ -15,7 +15,6 @@ void iou3d_boxes_overlap_bev_forward_impl(const int num_a, const Tensor boxes_a,
 void iou3d_boxes_overlap_bev_forward_npu(const int num_a, const Tensor boxes_a,
                                          const int num_b, const Tensor boxes_b,
                                          Tensor ans_overlap) {
-
   TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
   TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
 
