eb44ff15d4d14d54576a497585f185ad0f8274a2
JYYCaN <32977726+JYYCaN@users.noreply.github.com>
2025-04-08 17:19:18 +0800

    Update boxes_overlap_bev_npu.cpp

--- mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/boxes_overlap_bev_npu.cpp
@@ -16,17 +16,19 @@ void iou3d_boxes_overlap_bev_forward_npu(const int num_a, const Tensor boxes_a,
                                          const int num_b, const Tensor boxes_b,
                                          Tensor ans_overlap) {
 
-    TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
-    TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
+  TORCH_CHECK(boxes_a.size(1) == 7, "boxes_a must be 2D tensor (N, 7)");
+  TORCH_CHECK(boxes_b.size(1) == 7, "boxes_b must be 2D tensor (N, 7)");
 
-    auto clockwise = true;
-    bool aligned = false;
-    double margin = 1e-5;
-    int32_t mode_flag = MODE_FLAG_OVERLAP;
-    int32_t format_flag = FORMAT_FLAG_XYZWHDR;
+  auto clockwise = true;
+  bool aligned = false;
+  double margin = 1e-5;
+  int32_t mode_flag = MODE_FLAG_OVERLAP;
+  int32_t format_flag = FORMAT_FLAG_XYZWHDR;
 
-    EXEC_NPU_CMD(aclnnBoxesOverlapBevV1, boxes_a, boxes_b, format_flag, clockwise, mode_flag, aligned, margin, ans_overlap);
-    return;
+  EXEC_NPU_CMD(aclnnBoxesOverlapBevV1, boxes_a, boxes_b, format_flag, clockwise,
+               mode_flag, aligned, margin, ans_overlap);
+  return;
 }
 
-REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl, iou3d_boxes_overlap_bev_forward_npu);
+REGISTER_NPU_IMPL(iou3d_boxes_overlap_bev_forward_impl,
+                  iou3d_boxes_overlap_bev_forward_npu);
